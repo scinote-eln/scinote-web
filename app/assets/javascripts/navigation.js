76bd21884a172bda06e4e08dab247c1c54152e9c
Zmago Devetak <zmago_devetak@yahoo.com>
2016-04-16 10:12:10 +0000

    fix a typo and cleanup commented code

--- app/assets/javascripts/navigation.js
+++ app/assets/javascripts/navigation.js
@@ -1,10 +1,9 @@
 /* Loading overlay for search */
-var base_url = window.location.protocol + "//" + window.location.host;
 $("#search-bar").submit(function (){
   animateSpinner(document.body);
 });
 
-/* Loading sugestions */
+/* Loading suggestions */
 $('#autocomplete').autocomplete({
     serviceUrl: '/search/search_options',
     dataType: 'json',
