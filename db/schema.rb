a6d39762db689c53928064d659d53f80b4e9be3a
scibb <gitbberic@gmail.com>
2016-08-09 10:04:10 +0200

    the migration to update canvas coordinates

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160704110900) do
+ActiveRecord::Schema.define(version: 20160809074757) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
