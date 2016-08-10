6b0b2b0df7cd883e78066a354dc08205dece1e17
scibb <gitbberic@gmail.com>
2016-08-10 08:36:58 +0200

    tabs replaced with 2-spaces

--- db/migrate/20160809074757_update_my_module_coordinate.rb
+++ db/migrate/20160809074757_update_my_module_coordinate.rb
@@ -1,12 +1,12 @@
 class UpdateMyModuleCoordinate < ActiveRecord::Migration
-  def change
-    MyModule.all.each do |my_module|
-	  x = my_module.x
-	  y = my_module.y
-	  x = x * 32
-	  y = y * 16
-	  my_module.update(x: x)
-	  my_module.update(y: y)
+	def change
+		MyModule.all.each do |my_module|
+			x = my_module.x
+			y = my_module.y
+			x *= 32
+			y *= 16
+			my_module.update(x: x)
+			my_module.update(y: y)
+		end
 	end
-  end
 end
