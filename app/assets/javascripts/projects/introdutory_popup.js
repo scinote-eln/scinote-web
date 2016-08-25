99976722a8a9185ac3803d79cecb5f89fb7a287f
zmagod <zmago_devetak@yahoo.com>
2016-08-25 18:49:49 +0200

    uncomment js file

--- app/assets/javascripts/projects/introdutory_popup.js
+++ app/assets/javascripts/projects/introdutory_popup.js
@@ -1,10 +1,10 @@
 (function(){
   // display introductory modal
-  // if( Cookies.get('popup-already-shown') !== 'yes'){
+  if( Cookies.get('popup-already-shown') !== 'yes'){
     $("#introductory-popup-modal").modal('show',{
       backdrop: true,
       keyboard: false,
     });
-  //  Cookies.set('popup-already-shown', 'yes');
-  // }
+    Cookies.set('popup-already-shown', 'yes');
+  }
 })();
