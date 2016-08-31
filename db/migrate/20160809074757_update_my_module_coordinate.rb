bd414c8b4fe8048b39133ed819ff066a0b3b4d9a
Luka Murn <murn.luka@gmail.com>
2016-08-31 11:46:27 +0200

    Update a problematic migration

--- db/migrate/20160809074757_update_my_module_coordinate.rb
+++ db/migrate/20160809074757_update_my_module_coordinate.rb
@@ -1,12 +1,11 @@
 class UpdateMyModuleCoordinate < ActiveRecord::Migration
   def change
-    MyModule.all.each do |my_module|
+    MyModule.find_each do |my_module|
       x = my_module.x
       y = my_module.y
       x *= 32
       y *= 16
-      my_module.update(x: x)
-      my_module.update(y: y)
+      my_module.update(x: x, y: y)
     end
   end
 end
