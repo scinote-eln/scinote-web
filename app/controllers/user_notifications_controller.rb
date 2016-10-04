246dae4f46be17ccccc76d7aaea11e16337ff7af
Oleksii Kriuchykhin <okriuchykhin@biosistemika.com>
2016-10-04 14:16:18 +0200

    Code style improvements according to hound [SCI-442]

--- app/controllers/user_notifications_controller.rb
+++ app/controllers/user_notifications_controller.rb
@@ -10,33 +10,34 @@ class UserNotificationsController < ApplicationController
                                           @last_notification_id,
                                           @per_page + 1)
 
-    @more_notifications_url = ""
+    @more_notifications_url = ''
 
     @overflown = @notifications.length > @per_page
 
-    @notifications = UserNotification
-      .last_notifications(@current_user, @last_notification_id, @per_page)
+    @notifications =
+      UserNotification.last_notifications(@current_user,
+                                          @last_notification_id,
+                                          @per_page)
 
     if @notifications.count > 0
       @more_notifications_url = url_for(
         controller: 'user_notifications',
         action: 'index',
         format: :json,
-        from: @notifications.last.id)
+        from: @notifications.last.id
+      )
     end
 
     respond_to do |format|
       format.html
-      format.json {
-        render :json => {
-          :per_page => @per_page,
-          :results_number => @notifications.length,
-          :more_notifications_url => @more_notifications_url,
-          :html => render_to_string({
-            :partial => 'list.html.erb'
-          })
+      format.json do
+        render json: {
+          per_page: @per_page,
+          results_number: @notifications.length,
+          more_notifications_url: @more_notifications_url,
+          html: render_to_string(partial: 'list.html.erb')
         }
-      }
+      end
     end
     mark_seen_notification @notifications
   end
