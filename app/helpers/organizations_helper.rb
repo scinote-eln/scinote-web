7bdb81640d8476f2a38683ea15ee8b41f4420494
zmagod <zmago_devetak@yahoo.com>
2016-10-11 16:16:48 +0200

    fixes search

--- app/helpers/organizations_helper.rb
+++ app/helpers/organizations_helper.rb
@@ -1,2 +1,9 @@
 module OrganizationsHelper
+  def current_organization_switch(org)
+    # resets the current organization if needed
+    if org != current_organization
+      current_user.current_organization_id = org.id
+      current_user.save
+    end
+  end
 end
