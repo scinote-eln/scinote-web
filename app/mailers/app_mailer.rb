cccabcdd7b1905b721c169a49c4198be1c8bf186
Luka Murn <luka.murn@biosistemika.com>
2016-10-12 10:44:03 +0200

    Remove invitation_to_organization email
    This email is no longer needed, because if user enables
    assignment notifications, he/she will already receive an
    email whenever he/she is added to a new organization/team (via
    notifications email).

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -5,14 +5,6 @@ class AppMailer < Devise::Mailer
   default from: ENV["MAIL_FROM"]
   default reply: ENV["MAIL_REPLYTO"]
 
-  def invitation_to_organization(user, added_by, organization, opts = {})
-    @user = user
-    @added_by = added_by
-    @org = organization
-    headers = { to: @user.email, subject: (I18n.t('mailer.invitation_to_organization.subject')) }.merge(opts)
-    mail(headers)
-  end
-
   def notification(user, notification)
     @user = user
     @notification = notification
