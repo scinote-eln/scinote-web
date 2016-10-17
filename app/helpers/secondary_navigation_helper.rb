3e416ce1eec8e1687f40f725af3a6a285924a85d
Luka Murn <luka.murn@biosistemika.com>
2016-10-17 13:01:36 +0200

    Add task overview tab, refactor secondary navigation
    This commit also removes:
    * reports from all secondary navigation,
    * task/module archive tab (it will be merged
      into task results page).

--- app/helpers/secondary_navigation_helper.rb
+++ app/helpers/secondary_navigation_helper.rb
@@ -1,58 +1,49 @@
 module SecondaryNavigationHelper
-
-  def is_project_info?
+  def project_info?
     action_name == 'show'
   end
 
-  def is_project_show?
+  def project_show?
     action_name == 'show'
   end
 
-  def is_project_samples?
+  def project_samples?
     action_name == 'samples'
   end
 
-  def is_project_reports?
-    controller_name == 'reports' && action_name == 'index'
-  end
-
-  def is_project_archive?
+  def project_archive?
     action_name == 'experiment_archive'
   end
 
-  def is_experiment_canvas?
+  def experiment_canvas?
     action_name == 'canvas'
   end
 
-  def is_experiment_archive?
+  def experiment_archive?
     action_name == 'module_archive'
   end
 
-  def is_experiment_samples?
+  def experiment_samples?
     action_name == 'samples'
   end
 
-  def is_module_info?
+  def module_overview?
     action_name == 'show'
   end
 
-  def is_module_protocols?
+  def module_protocols?
     action_name == 'protocols'
   end
 
-  def is_module_results?
+  def module_results?
     action_name == 'results'
   end
 
-  def is_module_activities?
+  def module_activities?
     action_name == 'activities'
   end
 
-  def is_module_samples?
+  def module_samples?
     action_name == 'samples'
-  end
-
-  def is_module_archive?
-    action_name == 'archive'
   end
 end
