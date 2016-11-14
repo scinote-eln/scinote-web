74e58b044f71a8b6e06405c5a1eebc431a2386cb
Matej Zrimšek <mzrimsek@biosistemika.com>
2016-11-14 14:27:35 +0100

    Tutorial refactored using one method, for steps 1-5.

--- app/assets/javascripts/projects/show.js
+++ app/assets/javascripts/projects/show.js
@@ -7,6 +7,8 @@
       that.find(".workflowimg-container").hide();
       initProjectExperiment(that);
     });
+
+    initTutorial();
   }
 
   function initProjectExperiment(element){
@@ -60,110 +62,35 @@
       }
     });
   }
-  // init
-  init();
-})();
 
-/* Initialize the first-time demo tutorial if needed. */
-(function() {
-  function initializeTutorial() {
-    if (showTutorial()) {
-      var stepNum = parseInt(Cookies.get('current_tutorial_step'), 10);
-
-      introJs()
-        .setOptions({
-          overlayOpacity: '0.2',
-          prevLabel: 'Back',
-          nextLabel: 'Next',
-          doneLabel: 'End tutorial',
-          skipLabel: 'End tutorial',
-          showBullets: false,
-          showStepNumbers: false,
-          exitOnOverlayClick: false,
-          exitOnEsc: false,
-          disableInteraction: true,
-          tooltipClass: 'custom next-page-link'
-        })
-        .start();
-
-      // Page navigation when coming to this page from previous/next page
-      $(function() {
-        if (stepNum === 4) {
-          $('.introjs-prevbutton').removeClass('introjs-disabled');
-        } else if (stepNum === 5) {
-          $('.introjs-nextbutton').removeClass('introjs-disabled');
-        }
-      });
-
-      // Page navigation when already on this page
-      var tutorialData = JSON.parse(Cookies.get('tutorial_data'));
-      $('.introjs-skipbutton').click(function() {
-        restore_after_tutorial();
-      });
-      $('.introjs-prevbutton').click(function() {
-        Cookies.set('current_tutorial_step', --stepNum);
-
-        if (stepNum === 4) {
-          $('.introjs-prevbutton').removeClass('introjs-disabled');
-        } else if (stepNum < 4) {
-          // Going to previous page
-          var prevPage = tutorialData[0].previousPage;
-          $('.introjs-prevbutton').attr('href', prevPage);
-        }
-      });
-      $('.introjs-nextbutton').click(function() {
-        Cookies.set('current_tutorial_step', ++stepNum);
-
-        if (stepNum === 5) {
-          $('.introjs-nextbutton').removeClass('introjs-disabled');
-        } else if (stepNum > 5) {
-          // Going to next page
-
-          var prevPage = window.location.pathname;
-          tutorialData[0].previousPage = prevPage;
-          Cookies.set('tutorial_data', tutorialData);
-
-          var nextPage = $('[data-canvas-link]').data('canvasLink');
-          $('.introjs-nextbutton').attr('href', nextPage);
-        }
-      });
-    }
+  /**
+   * Initializes tutorial
+   */
+  function initTutorial() {
+    var nextPage = $('[data-canvas-link]').data('canvasLink');
+    initPageTutorialSteps(4, 5, nextPage, tutorialBeforeCb, tutorialAfterCb);
   }
 
-  function showTutorial() {
-    var tutorialData;
-
-    if (Cookies.get('tutorial_data'))
-      tutorialData = JSON.parse(Cookies.get('tutorial_data'));
-    else
-      return false;
-    var currentStep = Cookies.get('current_tutorial_step');
-    if (currentStep < 3 || currentStep > 5)
-      return false;
-    var tutorialProjectId = tutorialData[0].project;
-    var currentProjectId = $("#data-holder").attr("data-project-id");
-
-    return tutorialProjectId == currentProjectId;
-  }
-
-  function project_tutorial_helper(){
-    $(document).ready(function(){
-      if( $('div').hasClass('introjs-overlay')){
-        $.each( $(".panel-title"), function(){
-          $(this).css({ "pointer-events": "none" });
-        });
-        $.each( $(".workflowimg-container"), function(){
-          $(this).css({ "pointer-events": "none" });
-        });
-        $.each( $(".dropdown-experiment-actions").find("li"),
-          function(){
-            $(this).css({ "pointer-events": "none" });
-        });
-      }
+  /**
+   * Callback to be executed before tutorial sterts
+   */
+  function tutorialBeforeCb() {
+    $.each( $(".panel-title"), function(){
+      $(this).css({ "pointer-events": "none" });
+    });
+    $.each( $(".workflowimg-container"), function(){
+      $(this).css({ "pointer-events": "none" });
+    });
+    $.each( $(".dropdown-experiment-actions").find("li"),
+      function(){
+        $(this).css({ "pointer-events": "none" });
     });
   }
 
-  function restore_after_tutorial(){
+  /**
+   * Callback to be executed after tutorial exits
+   */
+  function tutorialAfterCb() {
     $.each( $(".panel-title"), function(){
       $(this).css({ "pointer-events": "auto" });
     });
@@ -176,9 +103,5 @@
     });
   }
 
-  $(document).ready(function(){
-    initializeTutorial();
-    project_tutorial_helper();
-  });
-
+  init();
 })();
--- app/assets/javascripts/sitewide/utils.js
+++ app/assets/javascripts/sitewide/utils.js
@@ -25,3 +25,98 @@ $.fn.onAjaxComplete = function (cb) {
 	cb();
   });
 }
+
+var TUTORIAL_STEPS_CNT = 20;
+
+/**
+ * Initializes tutorial steps for the current page
+ * @param  {number} pageFirstStep Page's first step
+ * @param  {number} pageLastStep Page's last step
+ * @param  {string} nextPagePath Next page absolute path
+ * @param {function} beforeCb Callback called before the fucntion logic
+ * @param {function} afterCb Callback called after the fucntion logic
+ */
+function initPageTutorialSteps(pageFirstStep, pageLastStep, nextPagePath,
+                               beforeCb, afterCb) {
+  var tutorialData = Cookies.get('tutorial_data');
+  if (tutorialData) {
+    tutorialData = JSON.parse(tutorialData);
+    var stepNum = parseInt(Cookies.get('current_tutorial_step'), 10);
+    if (isNaN(stepNum)) {
+      stepNum = 1;
+      Cookies.set('current_tutorial_step', stepNum);
+    }
+    beforeCb();
+
+    // Initialize tutorial for the current page's steps
+    var doneLabel = (pageLastStep === TUTORIAL_STEPS_CNT) ?
+     'Start using sciNote' : 'End tutorial';
+    introJs()
+      .setOptions({
+        overlayOpacity: '0.2',
+        prevLabel: 'Back',
+        nextLabel: 'Next',
+        skipLabel: 'End tutorial',
+        doneLabel: doneLabel,
+        showBullets: false,
+        showStepNumbers: false,
+        exitOnOverlayClick: false,
+        exitOnEsc: false,
+        disableInteraction: true,
+        tooltipClass: 'custom next-page-link'
+      })
+      .goToStep(stepNum - (pageFirstStep - 1))
+      .start();
+
+    // Page navigation when coming to this page from previous/next page
+    $(function() {
+      if (stepNum === pageFirstStep && stepNum > 1) {
+        $('.introjs-prevbutton').removeClass('introjs-disabled');
+      } else if (stepNum === pageLastStep) {
+        $('.introjs-nextbutton').removeClass('introjs-disabled');
+      }
+    });
+
+    // Page navigation when already on this page
+
+    $('.introjs-skipbutton').click(function() {
+      Cookies.remove('tutorial_data');
+      Cookies.remove('current_tutorial_step');
+
+      afterCb();
+      $('.introjs-overlay').remove();
+    });
+
+    $('.introjs-prevbutton').click(function() {
+      if (stepNum > 1) {
+        Cookies.set('current_tutorial_step', --stepNum);
+
+        if (stepNum === pageFirstStep && stepNum > 1) {
+          $('.introjs-prevbutton').removeClass('introjs-disabled');
+        } else if (stepNum < pageFirstStep) {
+          // Going to previous page
+          var prevPage = tutorialData[0].previousPage;
+          $('.introjs-prevbutton').attr('href', prevPage);
+        }
+      }
+    });
+
+    $('.introjs-nextbutton').click(function() {
+      if (stepNum < TUTORIAL_STEPS_CNT) {
+        Cookies.set('current_tutorial_step', ++stepNum);
+
+        if (stepNum === pageLastStep && stepNum < TUTORIAL_STEPS_CNT) {
+          $('.introjs-nextbutton').removeClass('introjs-disabled');
+        } else if (stepNum > pageLastStep) {
+          // Going to next page
+
+          var prevPage = window.location.pathname;
+          tutorialData[0].previousPage = prevPage;
+          Cookies.set('tutorial_data', tutorialData);
+
+          $('.introjs-nextbutton').attr('href', nextPagePath);
+        }
+      }
+    });
+  }
+}
