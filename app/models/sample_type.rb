7658ae264afe78ee96447c63c48c29cf6488bce7
Luka Murn <luka.murn@biosistemika.com>
2016-11-25 15:53:44 +0100

    Add uniqueness to sample_type & sample_group name
    Closes SCI-720.

--- app/models/sample_type.rb
+++ app/models/sample_type.rb
@@ -2,7 +2,8 @@ class SampleType < ActiveRecord::Base
   auto_strip_attributes :name, nullify: false
   validates :name,
             presence: true,
-            length: { maximum: Constants::NAME_MAX_LENGTH }
+            length: { maximum: Constants::NAME_MAX_LENGTH },
+            uniqueness: { scope: :organization, case_sensitive: false }
   validates :organization, presence: true
 
   belongs_to :created_by, foreign_key: 'created_by_id', class_name: 'User'
---
+++ db/migrate/20161125153600_rename_non_unique_sample_types_groups.rb
@@ -0,0 +1,46 @@
+class RenameNonUniqueSampleTypesGroups < ActiveRecord::Migration
+  def up
+    Organization.find_each do |org|
+      st_ids = []
+      org.sample_types.find_each do |st|
+        if SampleType.where(organization_id: org.id)
+                     .where(name: st.name).count > 1
+          st_ids << st.id
+        end
+      end
+
+      next if st_ids.count == 0
+
+      cntr = -1
+      SampleType.where(id: st_ids).find_each do |st|
+        cntr += 1
+        next if cntr == 0
+        new_name = "#{st.name} (#{cntr})"
+        st.update(name: new_name)
+      end
+    end
+
+    Organization.find_each do |org|
+      sg_ids = []
+      org.sample_groups.find_each do |sg|
+        if SampleGroup.where(organization_id: org.id)
+                      .where(name: sg.name).count > 1
+          sg_ids << sg.id
+        end
+      end
+
+      next if sg_ids.count == 0
+
+      cntr = -1
+      SampleGroup.where(id: sg_ids).find_each do |sg|
+        cntr += 1
+        next if cntr == 0
+        new_name = "#{sg.name} (#{cntr})"
+        sg.update(name: new_name)
+      end
+    end
+  end
+
+  def down
+  end
+end
