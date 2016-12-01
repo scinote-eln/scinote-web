7309da9d92e47bbe98ecf9769517ddb7c44352a0
Luka Murn <murn.luka@gmail.com>
2016-12-01 15:54:08 +0100

    Version bump to 1.5.1

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -184,7 +184,7 @@ class Constants
   #=============================================================================
 
   # Application version
-  APP_VERSION = '1.5.0'.freeze
+  APP_VERSION = '1.5.1'.freeze
 
   TEXT_EXTRACT_FILE_TYPES = [
     'application/pdf',
