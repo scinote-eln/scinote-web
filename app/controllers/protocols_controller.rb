d798f1b120b63017fd9d94fcb6e972dcbb073f26
Oleksii Kriuchykhin <okriuchykhin@biosistemika.com>
2016-12-29 15:15:29 +0100

    Fix XSS vulnerability in protocol keywords [SCI-632]

--- app/controllers/protocols_controller.rb
+++ app/controllers/protocols_controller.rb
@@ -159,6 +159,10 @@ class ProtocolsController < ApplicationController
 
   def update_keywords
     respond_to do |format|
+      # sanitize user input
+      params[:keywords].collect! do |keyword|
+        ActionController::Base.helpers.sanitize(keyword)
+      end
       if @protocol.update_keywords(params[:keywords])
         format.json {
           render json: {
