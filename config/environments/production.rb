de8034c5b9d978ab6d44ceacfb101d1ef591c9d6
Luka Murn <luka.murn@biosistemika.com>
2017-02-27 09:40:01 +0100

    Update environment settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,7 +98,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # Disable deface if neccesary
+  # Enable/disable Deface
   config.deface.enabled = ENV['DEFACE_ENABLED'] == 'true'
 
   # Enable first-time tutorial for users signing in the sciNote for
