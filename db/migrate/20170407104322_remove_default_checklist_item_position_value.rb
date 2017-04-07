cf57c5dc872c80bb316fdcdd972af1967bff8539
Mojca Lorber <mojca.lorber@biosistemika.com>
2017-04-07 14:35:47 +0200

    fix default value for position of checklist items

---
+++ db/migrate/20170407104322_remove_default_checklist_item_position_value.rb
@@ -0,0 +1,9 @@
+class RemoveDefaultChecklistItemPositionValue < ActiveRecord::Migration
+  def up
+    change_column :checklist_items, :position, :integer, default: nil, null: true
+  end
+
+  def down
+    change_column :checklist_items, :position, :integer, default: 0, null: false
+  end
+end
