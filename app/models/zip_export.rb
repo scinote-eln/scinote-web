a6183af63d3a3b41d2c38e0052cb62f8ff766c5a
zmagod <zmago_devetak@yahoo.com>
2017-04-13 14:34:58 +0200

    fix method_missing again

--- app/models/zip_export.rb
+++ app/models/zip_export.rb
@@ -67,7 +67,7 @@ class ZipExport < ActiveRecord::Base
   def method_missing(m, *args, &block)
     puts 'Method is missing! To use this zip_export you have to ' \
          'define a method: generate_( type )_zip.'
-    super
+    object.public_send(m, *args, &block)
   end
 
   def respond_to_missing?(method_name, include_private = false)
