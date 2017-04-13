9c28608fbf5593ded1fd6830650066d242f1fa79
Luka Murn <luka.murn@biosistemika.com>
2017-04-13 17:00:27 +0200

    Fix encoding when cloning tables
    Closes SCI-1175.

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,9 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+# Set the encoding
+Encoding.default_external = Encoding::UTF_8
+Encoding.default_internal = Encoding::UTF_8
+
 # Initialize the Rails application.
 Rails.application.initialize!
-
