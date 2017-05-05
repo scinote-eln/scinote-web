f079a097c542466d6d8fb9c3ca63194acc9a86e0
zmagod <zmago_devetak@yahoo.com>
2017-05-05 14:54:22 +0200

    removes TempFile after 7 days [fixes SCI-160]

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -53,6 +53,7 @@ class TeamsController < ApplicationController
             )
 
             if @temp_file.save
+              @temp_file.destroy_obsolete_files
               # format.html
               format.json {
                 render :json => {
--- app/models/temp_file.rb
+++ app/models/temp_file.rb
@@ -3,4 +3,12 @@ class TempFile < ActiveRecord::Base
 
   has_attached_file :file
   do_not_validate_attachment_file_type :file
+
+  def destroy_obsolete_files
+    file = TempFile.find_by_id(self.id)
+    file.destroy! if file
+  end
+
+  handle_asynchronously :destroy_obsolete_files,
+                        run_at: proc { 7.days.from_now }
 end
