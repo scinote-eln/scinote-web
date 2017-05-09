2d698968972bd058b459e3750478df2071d13813
zmagod <zmago_devetak@yahoo.com>
2017-05-09 13:59:59 +0200

    fixes pdf export and image size [fixes SCI-1247]

--- app/models/tiny_mce_asset.rb
+++ app/models/tiny_mce_asset.rb
@@ -25,7 +25,7 @@ class TinyMceAsset < ActiveRecord::Base
   # When using S3 file upload, we can limit file accessibility with url signing
   def presigned_url(style = :large,
                     download: false,
-                    timeout: Constants::URL_SHORT_EXPIRE_TIME)
+                    timeout: Constants::URL_LONG_EXPIRE_TIME)
     if stored_on_s3?
       if download
         download_arg = 'attachment; filename=' + URI.escape(image_file_name)
@@ -46,7 +46,7 @@ class TinyMceAsset < ActiveRecord::Base
     image.options[:storage].to_sym == :s3
   end
 
-  def url(style = :large, timeout: Constants::URL_SHORT_EXPIRE_TIME)
+  def url(style = :large, timeout: Constants::URL_LONG_EXPIRE_TIME)
     if image.is_stored_on_s3?
       presigned_url(style, timeout: timeout)
     else
