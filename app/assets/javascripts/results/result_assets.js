81a3cb0a418b57c2d9dd854aa4924af69d96664c
zmagod <zmago_devetak@yahoo.com>
2017-05-11 16:54:28 +0200

    adds results js code in modules

--- app/assets/javascripts/results/result_assets.js
+++ app/assets/javascripts/results/result_assets.js
@@ -1,93 +1,93 @@
 (function(global) {
   'use strict';
 
-  var ResutlAssetHandler = (function() {
-    var self = this;
-    var publicAPI = Object.freeze({
-      initNewResultAsset: function initNewResultAsset() {
-        // New result asset behaviour
-        $('#new-result-asset').on('ajax:success', function(e, data) {
-          var $form = $(data.html);
-          $('#results').prepend($form);
-          debugger;
-          self.formAjaxResultAsset($form);
+  var ResutlAssets = (function() {
+    // New result asset behaviour
+    function initNewResultAsset() {
+      $('#new-result-asset').on('ajax:success', function(e, data) {
+        var $form = $(data.html);
+        $('#results').prepend($form);
 
-          // Cancel button
-          $form.find('.cancel-new').click(function () {
-            $form.remove();
-            toggleResultEditButtons(true);
-          });
+        _formAjaxResultAsset($form);
 
-          toggleResultEditButtons(false);
-
-          $('#result_name').focus();
+        // Cancel button
+        $form.find('.cancel-new').click(function () {
+          $form.remove();
+          Results.toggleResultEditButtons(true);
         });
 
-        $('#new-result-asset')
-          .on('ajax:error', function(e, xhr, status, error) {
-          animateSpinner(null, false);
-        });
-      },
-      applyEditResultAssetCallback: function applyEditResultAssetCallback() {
-        $('.edit-result-asset').on('ajax:success', function(e, data) {
-          var $result = $(this).closest('.result');
-          var $form = $(data.html);
-          var $prevResult = $result;
-          $result.after($form);
-          $result.remove();
+        Results.toggleResultEditButtons(false);
 
-          formAjaxResultAsset($form);
+        $('#result_name').focus();
+      }).on('ajax:error', function(e, xhr, status, error) {
+        animateSpinner(null, false);
+      });
+    }
 
-          // Cancel button
-          $form.find('.cancel-edit').click(function () {
-            $form.after($prevResult);
-            $form.remove();
-            applyEditResultAssetCallback();
-            toggleResultEditButtons(true);
-            initPreviewModal();
-          });
+    function applyEditResultAssetCallback() {
+      $('.edit-result-asset').on('ajax:success', function(e, data) {
+        var $result = $(this).closest('.result');
+        var $form = $(data.html);
+        var $prevResult = $result;
+        $result.after($form);
+        $result.remove();
 
-          toggleResultEditButtons(false);
+        _formAjaxResultAsset($form);
 
-          $('#result_name').focus();
-        });
-
-        $('.edit-result-asset')
-          .on('ajax:error', function(e, xhr, status, error) {
-          animateSpinner(null, false);
-        });
-      },
-      formAjaxResultAsset: function formAjaxResultAsset($form) {
-        $form.on('ajax:success', function(e, data) {
-          $form.after(data.html);
-          var $newResult = $form.next();
-          initFormSubmitLinks($newResult);
-          $(this).remove();
+        // Cancel button
+        $form.find('.cancel-edit').click(function () {
+          $form.after($prevResult);
+          $form.remove();
           applyEditResultAssetCallback();
-          Results.applyCollapseLinkCallBack();
-
-          toggleResultEditButtons(true);
-          expandResult($newResult);
+          Results.toggleResultEditButtons(true);
           initPreviewModal();
-          Comments.initialize();
-        }).on('ajax:error', function(e, data) {
-          // This check is here only because of remotipart bug, which returns
-          // HTML instead of JSON, go figure
-          var errors = '';
-          if (data.errors)
-            errors = data.errors;
-          else
-            errors = data.responseJSON.errors;
-          $form.renderFormErrors('result', errors, true, e);
-          animateSpinner(null, false);
         });
-      }
+
+        Results.toggleResultEditButtons(false);
+
+        $('#result_name').focus();
+      }).on('ajax:error', function(e, xhr, status, error) {
+        animateSpinner(null, false);
+      });
+    }
+
+    function _formAjaxResultAsset($form) {
+      $form.on('ajax:success', function(e, data) {
+        $form.after(data.html);
+        var $newResult = $form.next();
+        initFormSubmitLinks($newResult);
+        $(this).remove();
+        applyEditResultAssetCallback();
+        Results.applyCollapseLinkCallBack();
+
+        Results.toggleResultEditButtons(true);
+        Results.expandResult($newResult);
+        initPreviewModal();
+        Comments.initialize();
+      }).on('ajax:error', function(e, data) {
+        // This check is here only because of remotipart bug, which returns
+        // HTML instead of JSON, go figure
+        var errors = '';
+        if (data.errors)
+          errors = data.errors;
+        else
+          errors = data.responseJSON.errors;
+        $form.renderFormErrors('result', errors, true, e);
+        animateSpinner(null, false);
+      });
+    }
+
+    var publicAPI = Object.freeze({
+      initNewResultAsset: initNewResultAsset,
+      applyEditResultAssetCallback: applyEditResultAssetCallback
     });
 
     return publicAPI;
   })();
 
-  ResutlAssetHandler.initNewResultAsset();
-  ResutlAssetHandler.applyEditResultAssetCallback();
-  global.initPreviewModal();
+  $(document).ready(function() {
+    ResutlAssets.initNewResultAsset();
+    ResutlAssets.applyEditResultAssetCallback();
+    global.initPreviewModal();
+  });
 })(window);
--- app/assets/javascripts/results/result_texts.js
+++ app/assets/javascripts/results/result_texts.js
@@ -1,81 +1,96 @@
-// New result text behaviour
-$("#new-result-text").on("ajax:success", function(e, data) {
-    var $form = $(data.html);
-    $("#results").prepend($form);
+(function() {
+  'use strict';
 
-    formAjaxResultText($form);
+  var ResultText = (function() {
+    // New result text behaviour
+    function initNewReslutText() {
+      $('#new-result-text').on('ajax:success', function(e, data) {
+          var $form = $(data.html);
+          $('#results').prepend($form);
 
-    // Cancel button
-    $form.find(".cancel-new").click(function () {
-        $form.remove();
-        toggleResultEditButtons(true);
-    });
-    toggleResultEditButtons(false);
-    TinyMCE.refresh();
-    $("#result_name").focus();
-});
+          _formAjaxResultText($form);
 
-$("#new-result-text").on("ajax:error", function(e, xhr, status, error) {
-    //TODO: Add error handling
-});
+          // Cancel button
+          $form.find('.cancel-new').click(function() {
+              $form.remove();
+              Results.toggleResultEditButtons(true);
+          });
+          Results.toggleResultEditButtons(false);
+          TinyMCE.refresh();
+          TinyMCE.highlight();
+          $('#result_name').focus();
+      }).on('ajax:error', function() {
+          TinyMCE.refresh();
+      });
+    }
 
-// Edit result text button behaviour
-function applyEditResultTextCallback() {
-    $(".edit-result-text").on("ajax:success", function(e, data) {
-        var $result = $(this).closest(".result");
-        var $form = $(data.html);
-        var $prevResult = $result;
-        $result.after($form);
-        $result.remove();
 
-        formAjaxResultText($form);
+    // Edit result text button behaviour
+    function applyEditResultTextCallback() {
+        $('.edit-result-text').on('ajax:success', function(e, data) {
+            var $result = $(this).closest('.result');
+            var $form = $(data.html);
+            var $prevResult = $result;
+            $result.after($form);
+            $result.remove();
 
-        // Cancel button
-        $form.find(".cancel-edit").click(function () {
-            $form.after($prevResult);
-            $form.remove();
-            applyEditResultTextCallback();
-            toggleResultEditButtons(true);
+            _formAjaxResultText($form);
+
+            // Cancel button
+            $form.find('.cancel-edit').click(function () {
+                $form.after($prevResult);
+                $form.remove();
+                applyEditResultTextCallback();
+                Results.toggleResultEditButtons(true);
+            });
+            Results.toggleResultEditButtons(false);
+            TinyMCE.refresh();
+            $('#result_name').focus();
         });
-        toggleResultEditButtons(false);
-        TinyMCE.refresh();
-        $("#result_name").focus();
+    }
+
+    // Apply ajax callback to form
+    function _formAjaxResultText($form) {
+        $form.on('ajax:success', function(e, data) {
+            $form.after(data.html);
+            var newResult = $form.next();
+            initFormSubmitLinks(newResult);
+            $(this).remove();
+
+            applyEditResultTextCallback();
+            Results.applyCollapseLinkCallBack();
+            Results.toggleResultEditButtons(true);
+            Results.expandResult(newResult);
+            TinyMCE.destroyAll();
+            Comments.initialize();
+        });
+        $form.on('ajax:error', function(e, xhr, status, error) {
+            var data = xhr.responseJSON;
+            $form.renderFormErrors('result', data);
+            TinyMCE.highlight();
+            if (data['result_text.text']) {
+                var $el = $form.find(
+                  'textarea[name=result\\[result_text_attributes\\]\\[text\\]]'
+                );
+
+                $el.closest('.form-group').addClass('has-error');
+                $el.parent().append('<span class=\'help-block\'>' +
+                  data['result_text.text'] + '</span>');
+            }
+        });
+    }
+
+    var publicAPI = Object.freeze({
+      initNewReslutText: initNewReslutText,
+      applyEditResultTextCallback: applyEditResultTextCallback
     });
 
-    $(".edit-result-text").on("ajax:error", function(e, xhr, status, error) {
-        //TODO: Add error handling
-    });
-}
+    return publicAPI;
+  })();
 
-// Apply ajax callback to form
-function formAjaxResultText($form) {
-    $form.on("ajax:success", function(e, data) {
-        $form.after(data.html);
-        var newResult = $form.next();
-        initFormSubmitLinks(newResult);
-        $(this).remove();
+  $(document).ready(function() {
+    ResultText.initNewReslutText();
+    ResultText.applyEditResultTextCallback();
+  });
 
-        applyEditResultTextCallback();
-        Results.applyCollapseLinkCallBack();
-        toggleResultEditButtons(true);
-        expandResult(newResult);
-        TinyMCE.destroyAll();
-        Comments.initialize();
-    });
-    $form.on("ajax:error", function(e, xhr, status, error) {
-        var data = xhr.responseJSON;
-        $form.renderFormErrors("result", data);
-        TinyMCE.highlight();
-        if (data["result_text.text"]) {
-            var $el = $form.find("textarea[name=result\\[result_text_attributes\\]\\[text\\]]");
-
-            $el.closest(".form-group").addClass("has-error");
-            $el.parent().append("<span class='help-block'>" + data["result_text.text"] + "</span>");
-        }
-    });
-}
-
-$(document).ready(function() {
-  TinyMCE.highlight();
-});
-applyEditResultTextCallback();
+})();
