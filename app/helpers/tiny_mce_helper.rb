67d902db526a20643ed7c8ae095bc4e6a6341f8e
zmagod <zmago_devetak@yahoo.com>
2017-06-05 10:43:58 +0200

    refactor

--- app/helpers/tiny_mce_helper.rb
+++ app/helpers/tiny_mce_helper.rb
@@ -18,6 +18,7 @@ module TinyMceHelper
   end
 
   def generate_image_tag_from_token(text)
+    return unless text
     regex = /\[~tiny_mce_id:([0-9a-zA-Z]+)\]/
     text.gsub(regex) do |el|
       match = el.match(regex)
