b729c06f31f517888b6e283e825b2ec20533c406
Luka Murn <luka.murn@biosistemika.com>
2017-06-08 11:20:11 +0200

    Minor fix

--- config/initializers/extends.rb
+++ config/initializers/extends.rb
@@ -36,7 +36,8 @@ class Extends
                            project_activity: 13, # TODO
                            project_samples: 14, # TODO
                            experiment: 15,
-                           my_module_repository: 16 }
+                           my_module_repository: 17 # Higher number because of addons
+                         }
 
   # Data type name should match corresponding model's name
   REPOSITORY_DATA_TYPES = { RepositoryTextValue: 0,
