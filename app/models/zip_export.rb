4ea8972a6c80402b4bc7fa3131b7d80179b84aaa
Matej Zrimšek <mzrimsek@biosistemika.com>
2017-06-09 19:48:20 +0200

    Fixed rebasing mistake. [SCI-1275]

--- app/models/zip_export.rb
+++ app/models/zip_export.rb
@@ -102,6 +102,11 @@ class ZipExport < ActiveRecord::Base
     end
   end
 
+  def generate_samples_zip(tmp_dir, data, options = {})
+    file = FileUtils.touch("#{tmp_dir}/export.csv").first
+    File.open(file, 'wb') { |f| f.write(data) }
+  end
+
   def generate_repositories_zip(tmp_dir, data, options = {})
     file = FileUtils.touch("#{tmp_dir}/export.csv").first
     File.open(file, 'wb') { |f| f.write(data) }
