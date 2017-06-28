502eb5c211b927ad5e9b65e1ad5f5a7abb9cda19
Zmago Devetak <ZmagoD@users.noreply.github.com>
2017-06-28 15:40:15 +0200

    fixes tabs

--- app/models/token.rb
+++ app/models/token.rb
@@ -3,8 +3,8 @@ class Token < ApplicationRecord
   validates :ttl, presence: true
 
   belongs_to :user,
-						 foreign_key: 'user_id',
-						 class_name: 'User',
-						 inverse_of: :tokens,
-						 optional: true
+	     foreign_key: 'user_id',
+	     class_name: 'User',
+	     inverse_of: :tokens,
+	     optional: true
 end
