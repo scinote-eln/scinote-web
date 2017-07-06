4317da343bbd314fd5b469fe88734a5c71b7446f
Luka Murn <luka.murn@biosistemika.com>
2017-07-06 16:49:50 +0200

    Don't use listen gem in development mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  #config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
