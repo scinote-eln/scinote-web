ba35717243aa7ce7c2c4e37aa1307ad2e95b8a8f
Oleksii Kriuchykhin <okriuchykhin@biosistemika.com>
2017-07-13 14:11:01 +0200

    Fix babel and eslint config [SCI-1461]

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -21,6 +21,6 @@ Hello.propTypes = {
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <Hello name="React" />,
-    document.body.appendChild(document.createElement('div')),
+    document.body.appendChild(document.createElement('div'))
   )
 })
