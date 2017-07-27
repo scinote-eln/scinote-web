0f4286d5d1fd118a720d4d7329bae0edf2e9dfe9
Toni Dezman <tdezman@biosistemika.com>
2017-07-27 14:21:27 +0200

    adds Settings index react page

--- app/javascript/packs/settings/index.jsx
+++ app/javascript/packs/settings/index.jsx
@@ -4,23 +4,23 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
 
-const Hello = props => (
-  <div>Hello {props.name}!</div>
+const Settings = () => (
+  <div>Settings page</div>
 )
 
-Hello.defaultProps = {
-  name: 'David'
-}
+// Settings.defaultProps = {
+//   name: 'David'
+// }
 
-Hello.propTypes = {
-  name: PropTypes.string
-}
+// Settings.propTypes = {
+//   name: PropTypes.string
+// }
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Hello name="React" />,
+    <Settings />,
     document.body.appendChild(document.createElement('div'))
   )
 })
