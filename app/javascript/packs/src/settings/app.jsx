81c39a8b6f672efb7c4df7269f17c8c33c84983a
Toni Dezman <tdezman@biosistemika.com>
2017-08-09 09:30:08 +0200

    adds settings routes and component

--- app/javascript/packs/src/settings/app.jsx
+++ app/javascript/packs/src/settings/app.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import { Provider } from "react-redux";
-import { IntlProvider } from "react-intl";
-import { addLocaleData } from "react-intl";
+import { IntlProvider, addLocaleData } from "react-intl";
 import enLocaleData from "react-intl/locale-data/en";
+
 import { flattenMessages } from "../../locales/utils";
 import store from "../../app/store";
 import messages from "../../locales/messages";
 
 import Navigation from "../../shared/navigation";
+import MainNav from "./components/MainNav";
+import SettingsAccount from "./components/SettingsAccount";
+import SettingsTeams from "./components/SettingsTeams";
 
 addLocaleData([...enLocaleData]);
 const locale = "en-US";
@@ -16,7 +20,11 @@ const locale = "en-US";
 const SettingsPage = () =>
   <div>
     <Navigation page="Settings" />
-    ....
+    <div className="container">
+      <MainNav />
+      <Route path="/settings/account" component={SettingsAccount} />
+      <Route path="/settings/teams" component={SettingsTeams} />
+    </div>
   </div>;
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -26,7 +34,11 @@ document.addEventListener("DOMContentLoaded", () => {
         locale={locale}
         messages={flattenMessages(messages[locale])}
       >
-        <SettingsPage />
+        <div>
+          <BrowserRouter>
+            <SettingsPage />
+          </BrowserRouter>
+        </div>
       </IntlProvider>
     </Provider>,
     document.getElementById("root")
