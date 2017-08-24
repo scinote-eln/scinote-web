309e52b806b12f7d21ec57d0dccc9eac9a6bafe9
Toni Dezman <tdezman@biosistemika.com>
2017-08-24 08:56:17 +0200

    extracts navigation to <MainNav /> component

--- app/javascript/packs/src/settings/app.jsx
+++ app/javascript/packs/src/settings/app.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { IntlProvider, addLocaleData } from "react-intl";
 import enLocaleData from "react-intl/locale-data/en";
@@ -9,32 +9,14 @@ import { flattenMessages } from "../../locales/utils";
 import store from "../../app/store";
 import messages from "../../locales/messages";
 
-import Navigation from "../../shared/navigation";
-import NotFound from "../../shared/404/NotFound";
 import MainNav from "./components/MainNav";
-import SettingsAccount from "./components/account/SettingsAccount";
-import SettingsTeams from "./components/team/SettingsTeams";
 
 addLocaleData([...enLocaleData]);
 const locale = "en-US";
 
 const SettingsPage = () =>
-  <div>
-    <Navigation page="Settings" />
-    <div className="container">
-      <MainNav />
-      <Switch>
-        <Route exact path="/" component={SettingsAccount} />
-        <Route
-          exact
-          path="/settings"
-          render={() => <Redirect to="/settings/account/profile" />}
-        />
-        <Route path="/settings/account" component={SettingsAccount} />
-        <Route path="/settings/teams" component={SettingsTeams} />
-        <Route component={NotFound} />
-      </Switch>
-    </div>
+  <div className="container">
+    <MainNav />
   </div>;
 
 document.addEventListener("DOMContentLoaded", () => {
