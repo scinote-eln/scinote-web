7b5b4c957cfcbaa09bd65fe229aef205f01b44ec
zmagod <zmago_devetak@yahoo.com>
2017-08-25 16:40:23 +0200

    adds formated messages in modal

--- app/javascript/packs/shared/actions/LeaveTeamActions.js
+++ app/javascript/packs/shared/actions/LeaveTeamActions.js
@@ -1,8 +1,8 @@
 import { SHOW_LEAVE_TEAM_MODAL } from "../../app/action_types";
 
-export function leaveTeamModalShow(show, id, teamName) {
-  return({
+export function leaveTeamModalShow(show = false, id = 0, teamName = "") {
+  return {
     payload: { show, id, teamName },
     type: SHOW_LEAVE_TEAM_MODAL
-  });
+  };
 }
--- app/javascript/packs/shared/modals_container/modals/LeaveTeamModal.jsx
+++ app/javascript/packs/shared/modals_container/modals/LeaveTeamModal.jsx
@@ -1,15 +1,24 @@
 import React, { Component } from "react";
-import PropTypes, { bool, number, string } from "prop-types";
-import { Modal, Button } from "react-bootstrap";
-import _ from "lodash";
-import { FormattedMessage } from "react-intl";
+import PropTypes, { bool, number, string, func } from "prop-types";
+import { Modal, Button, Alert, Glyphicon } from "react-bootstrap";
+import { FormattedMessage, FormattedHTMLMessage } from "react-intl";
 import { connect } from "react-redux";
 
+import { leaveTeamModalShow } from "../../actions/LeaveTeamActions";
+
 class LeaveTeamModal extends Component {
+  constructor(props) {
+    super(props);
+    this.onCloseModal = this.onCloseModal.bind(this);
+  }
+
+  onCloseModal() {
+    this.props.leaveTeamModalShow(false);
+  }
 
   render() {
     return (
-      <Modal show={this.props.showModal}>
+      <Modal show={this.props.showModal} onHide={this.onCloseModal}>
         <Modal.Header closeButton>
           <Modal.Title>
             <FormattedMessage
@@ -18,9 +27,28 @@ class LeaveTeamModal extends Component {
             />
           </Modal.Title>
         </Modal.Header>
-        <Modal.Body>BANANAN</Modal.Body>
+        <Modal.Body>
+          <p>
+            <FormattedMessage id="settings_page.leave_team_modal.subtitle" />
+          </p>
+          <Alert bsStyle="danger">
+            <Glyphicon glyph="exclamation-sign" />{" "}
+            <FormattedMessage id="settings_page.leave_team_modal.warnings" />
+            <ul>
+              <li>
+                <FormattedMessage id="settings_page.leave_team_modal.warning_message_one" />
+              </li>
+              <li>
+                <FormattedHTMLMessage id="settings_page.leave_team_modal.warning_message_two" />
+              </li>
+              <li>
+                <FormattedMessage id="settings_page.leave_team_modal.warning_message_three" />
+              </li>
+            </ul>
+          </Alert>
+        </Modal.Body>
         <Modal.Footer>
-          <Button>
+          <Button onClick={this.onCloseModal}>
             <FormattedMessage id="general.close" />
           </Button>
         </Modal.Footer>
@@ -33,13 +61,14 @@ LeaveTeamModal.propTypes = {
   showModal: bool.isRequired,
   teamId: number.isRequired,
   teamName: string.isRequired,
+  leaveTeamModalShow: func.isRequired,
   teams: PropTypes.arrayOf(
     PropTypes.shape({
       id: number.isRequired,
       name: string.isRequired,
       current_team: bool.isRequired,
       role: string.isRequired,
-      members: number.isRequired,
+      members: number.isRequired
     }).isRequired
   )
 };
@@ -49,4 +78,4 @@ const mapStateToProps = ({ showLeaveTeamModal }) => ({
   teamName: showLeaveTeamModal.teamName
 });
 
-export default connect(mapStateToProps)(LeaveTeamModal);
+export default connect(mapStateToProps, { leaveTeamModalShow })(LeaveTeamModal);
