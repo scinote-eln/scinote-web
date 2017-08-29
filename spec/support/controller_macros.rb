f7cb395b6adf4b937fcc04b39fe52d2f48d3012b
zmagod <zmago_devetak@yahoo.com>
2017-08-29 13:11:42 +0200

    fix hound v2

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -1,7 +1,7 @@
 module ControllerMacros
   def login_user
     before(:each) do
-      @request.env["devise.mapping"] = Devise.mappings[:user]
+      @request.env['devise.mapping'] = Devise.mappings[:user]
       user = FactoryGirl.create(:user)
       user.confirm
       sign_in user
