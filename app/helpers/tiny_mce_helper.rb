3dc3ed906f8617f5398613c6632698ea9ee25618
zmagod <zmago_devetak@yahoo.com>
2017-10-02 18:06:42 +0200

    fixes bug with embedded images [fixes SCI.1624]

--- app/helpers/tiny_mce_helper.rb
+++ app/helpers/tiny_mce_helper.rb
@@ -1,5 +1,5 @@
 module TinyMceHelper
-  def parse_tiny_mce_asset_to_token(text, ref = nil)
+  def parse_tiny_mce_asset_to_token(text, obj)
     ids = []
     html = Nokogiri::HTML(text)
     html.search('img').each do |img|
@@ -8,22 +8,24 @@ module TinyMceHelper
       ids << img_id
       token = "[~tiny_mce_id:#{img_id}]"
       img.replace(token)
-      next unless ref
+      next unless obj
       tiny_img = TinyMceAsset.find_by_id(img_id)
-      tiny_img.reference = ref unless tiny_img.step || tiny_img.result_text
+      next if check_parse_token_permissions(obj, tiny_img)
+      tiny_img.reference = obj unless tiny_img.step || tiny_img.result_text
       tiny_img.save
     end
-    destroy_removed_tiny_mce_assets(ids, ref) if ref
+    destroy_removed_tiny_mce_assets(ids, obj) if obj
     html
   end
 
-  def generate_image_tag_from_token(text)
+  def generate_image_tag_from_token(text, obj)
     return unless text
     regex = /\[~tiny_mce_id:([0-9a-zA-Z]+)\]/
     text.gsub(regex) do |el|
       match = el.match(regex)
       img = TinyMceAsset.find_by_id(match[1])
-      next unless img
+      next unless img && img.team == current_team
+      next unless check_image_permissions(obj, img)
       image_tag img.url,
                 class: 'img-responsive',
                 data: { token: Base62.encode(img.id) }
@@ -46,5 +48,18 @@ module TinyMceHelper
 
   def destroy_removed_tiny_mce_assets(ids, ref)
     ref.tiny_mce_assets.where.not('id IN (?)', ids).destroy_all
+  end
+
+  def check_image_permissions(obj, img)
+    if obj.class == Step
+      return img.step == obj
+    elsif obj.class == ResultText
+      return img.result_text == obj
+    end
+  end
+
+  def check_parse_token_permissions(obj, img)
+    return true if obj.class == Step && img.result_text.nil?
+    return true if obj.class == ResultText && img.step.nil?
   end
 end
