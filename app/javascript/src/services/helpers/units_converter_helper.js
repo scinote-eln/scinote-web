f12a645fa38cf42f51d59ea12d71856ab1a505c5
zmagod <zmago_devetak@yahoo.com>
2017-10-10 15:01:25 +0200

    fix hound

--- app/javascript/src/services/helpers/units_converter_helper.js
+++ app/javascript/src/services/helpers/units_converter_helper.js
@@ -5,5 +5,5 @@ export function formatBytes(bytes: number): string {
   const k = 1024;
   const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
   const i = Math.floor(Math.log(bytes) / Math.log(k));
-  return `${parseFloat((bytes / Math.pow(k, i)).toFixed(2))} ${sizes[i]}`;
+  return `${parseFloat((bytes / (k ** i)).toFixed(2))} ${sizes[i]}`;
 }
