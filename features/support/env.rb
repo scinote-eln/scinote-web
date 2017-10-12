bdc1051cbbd3d071daa10c3b8072f30afaed17f8
zmagod <zmago_devetak@yahoo.com>
2017-10-12 17:05:04 +0200

    cleanup

--- features/support/env.rb
+++ features/support/env.rb
@@ -15,10 +15,7 @@ headless.start
 
 Capybara::Webkit.configure do |config|
   # Enable debug mode. Prints a log of everything the driver is doing.
-  config.debug = true
-
-  # By default, requests to outside domains (anything besides localhost) will
-  # result in a warning. Several methods allow you to change this behavior.
+  config.debug = false
 
   # Silently return an empty 200 response for any requests to unknown URLs.
   config.block_unknown_urls
@@ -37,10 +34,6 @@ Capybara::Webkit.configure do |config|
 end
 
 Capybara.javascript_driver = :webkit
-
-Before do
-  `bundle exec rails webpacker:compile`
-end
 
 # Capybara defaults to CSS3 selectors rather than XPath.
 # If you'd prefer to use XPath, just uncomment this line and adjust any
