62dfd6a71380397f0c4d0b96f5f677f2e13361d6
zmagod <zmago_devetak@yahoo.com>
2017-10-24 15:39:06 +0200

    adds wait_for_ajax function to cucumber tests [fixes SCI-1723]

--- features/step_definitions/profile_steps.rb
+++ features/step_definitions/profile_steps.rb
@@ -11,18 +11,17 @@ Then(/^I click on Browse button$/) do
 end
 
 Then(/^I change "([^"]*)" with "([^"]*)" email$/) do |prev_email, new_email|
-  find(:xpath, "//input[@value='#{prev_email}']").set(new_email)
+  find(:css, "input[value='#{prev_email}']").set(new_email)
 end
 
 Then(/^I fill in "([^"]*)" in Current password field$/) do |password|
-  find(:xpath, '//input[@id="settings_page.current_password"]').set(password)
+  find(:css, 'input[id="settings_page.current_password"]').set(password)
 end
 
 Then(/^I fill in "([^"]*)" in New password field$/) do |password|
-  find(:xpath, '//input[@id="settings_page.new_password"]').set(password)
+  find(:css, 'input[id="settings_page.new_password"]').set(password)
 end
 
 Then(/^I fill in "([^"]*)" in New password confirmation field$/) do |password|
-  find(:xpath,
-       '//input[@id="settings_page.new_password_confirmation"]').set(password)
+  find(:css, 'input[id="settings_page.new_password_confirmation"]').set(password)
 end
