cbe3e6740243e7c8240916108be598812ce00f97
Luka Murn <luka.murn@biosistemika.com>
2017-10-24 13:57:33 +0200

    Codestyle refactor

--- app/javascript/src/components/validation/components/ValidatedFormGroup.jsx
+++ app/javascript/src/components/validation/components/ValidatedFormGroup.jsx
@@ -2,24 +2,22 @@ import React, { Component } from "react";
 import { FormGroup } from "react-bootstrap";
 import PropTypes from "prop-types";
 
-class ValidatedFormGroup extends Component {
-  render() {
-    // Remove additional props from the props
-    const { tag, ...cleanProps } = this.props;
+const ValidatedFormGroup = (props, context) => {
+  // Remove additional props from the props
+  const { tag, ...cleanProps } = props;
 
-    const hasError = this.context.hasErrorForTag(tag);
-    const formGroupClass = `form-group ${hasError ? " has-error" : ""}`;
-    const validationState = hasError ? "error" : null;
+  const hasError = context.hasErrorForTag(tag);
+  const formGroupClass = `form-group ${hasError ? " has-error" : ""}`;
+  const validationState = hasError ? "error" : null;
 
-    return (
-      <FormGroup
-        className={formGroupClass}
-        validationState={validationState}
-        {...cleanProps}
-      />
-    );
-  }
-}
+  return (
+    <FormGroup
+      className={formGroupClass}
+      validationState={validationState}
+      {...cleanProps}
+    />
+  );
+};
 
 ValidatedFormGroup.propTypes = {
   tag: PropTypes.string.isRequired
