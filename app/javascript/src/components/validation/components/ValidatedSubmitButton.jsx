329dd4f1b08e802f4454aa5ce933a8e570e8c0b3
Luka Murn <luka.murn@biosistemika.com>
2017-10-30 14:13:36 +0100

    Onboard Flow onto validation components.

--- app/javascript/src/components/validation/components/ValidatedSubmitButton.jsx
+++ app/javascript/src/components/validation/components/ValidatedSubmitButton.jsx
@@ -1,16 +1,18 @@
-import React from "react";
+// @flow
+
+import * as React from "react";
 import { Button } from "react-bootstrap";
 import PropTypes from "prop-types";
 
-const ValidatedSubmitButton = (props, context) =>
+type Props = {
+  children?: React.Node
+};
+
+const ValidatedSubmitButton = (props: Props, context: any) =>
   <Button {...props} disabled={context.hasAnyError()}>
     {props.children}
   </Button>
 ;
-
-ValidatedSubmitButton.propTypes = {
-  children: PropTypes.node
-}
 
 ValidatedSubmitButton.defaultProps = {
   children: undefined
