cceda7503aeddae35d8f6fd2d2d211a570e382e3
zmagod <zmago_devetak@yahoo.com>
2017-11-16 11:26:15 +0100

    test

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,6 +3,7 @@
 # newer version of cucumber-rails. Consider adding your own code to a new file
 # instead of editing this one. Cucumber will automatically load all features/**/*.rb
 # files.
+ENV['CUCUMBER'] = 'cucumber'
 
 require 'cucumber/rails'
 require 'capybara/cucumber'
