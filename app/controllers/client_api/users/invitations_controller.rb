5953126b0e0085544128cb881101bc18ded85e80
mlorb <mlorber@biosistemika.com>
2017-11-28 15:41:52 +0100

    add create, update, delete user_team permission

--- app/controllers/client_api/users/invitations_controller.rb
+++ app/controllers/client_api/users/invitations_controller.rb
@@ -33,7 +33,7 @@ module ClientApi
 
       def check_invite_users_permission
         @team = Team.find_by_id(params[:team_id])
-        if @team && !is_admin_of_team(@team)
+        if @team && !can_create_user_team?(@team)
           respond_to do |format|
             format.json do
               render json: t('client_api.invite_users.permission_error'),
