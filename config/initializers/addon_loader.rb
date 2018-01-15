ed44762ff2c380cab76609de44bd0fdd189fd74c
zmagod <zmago_devetak@yahoo.com>
2018-01-15 08:44:59 +0100

    adds rescue

--- config/initializers/addon_loader.rb
+++ config/initializers/addon_loader.rb
@@ -3,14 +3,23 @@
 
 TARGET_ROOT = "#{Dir.pwd}/app/javascript/src/react-hijack/addons".freeze
 
+begin
+# clear previous build
 FileUtils.rm_f Dir.glob("#{TARGET_ROOT}/*")
+
 Dir.foreach('addons/') do |path|
   addons_client_root = "addons/#{path}/client"
-
   next unless Dir.exist? addons_client_root
   all_files = Dir.glob("#{addons_client_root}/*\.*")
   all_files.each do |component|
     new_path = component.gsub("#{addons_client_root}/", '')
     File.symlink("#{Dir.pwd}/#{component}", "#{TARGET_ROOT}/#{new_path}")
   end
+end
+# handles error on the platforms that does not support symlink
+# http://ruby-doc.org/core-2.2.0/File.html#symlink-method
+rescue NotImplemented => error
+  puts '[sciNote] Unable to load React components from addons!'
+  puts '[sciNote] Your system does not support symlink!'
+  puts "[sciNote] #{error.message}"
 end
