bdc60b9d53cefa2badb1a5fcede71604631a98b8
Oleksii Kriuchykhin <okriuchykhin@biosistemika.com>
2018-01-30 18:24:01 +0100

    Allow optional tagging for logs [SCI-1975]

--- config/initializers/silencer.rb
+++ config/initializers/silencer.rb
@@ -1,8 +1,13 @@
 require 'silencer/logger'
 
 Rails.application.configure do
-  config.middleware.swap Rails::Rack::Logger, Silencer::Logger, silence: [
-    # Silence WickedPDF rendering in logs
-    %r{/projects/[0-9]*/reports/generate.pdf}
-  ]
+  config.middleware.swap(
+    Rails::Rack::Logger,
+    Silencer::Logger,
+    config.log_tags,
+    silence: [
+      # Silence WickedPDF rendering in logs
+      %r{/projects/[0-9]*/reports/generate.pdf}
+    ]
+  )
 end
