09c5e23adf030880d7bd7b89af3d8c3cc18e5055
Matej Zrimšek <mzrimsek@biosistemika.com>
2018-02-13 16:47:28 +0100

    Fixed permission checks for completing step and checking step checkboxes. Disabled turbolinks for comments since it can cause issues. Minor code styling changes.

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -4,7 +4,8 @@ class StepsController < ApplicationController
   include TinyMceHelper
   include StepsActions
 
-  before_action :load_vars, only: [:edit, :update, :destroy, :show]
+  before_action :load_vars, only: %i(edit update destroy show toggle_step_state
+                                     checklistitem_state)
   before_action :load_vars_nested, only: [:new, :create]
   before_action :convert_table_contents_to_utf8, only: [:create, :update]
 
@@ -265,66 +266,55 @@ class StepsController < ApplicationController
 
   # Responds to checkbox toggling in steps view
   def checklistitem_state
-    chkItem = ChecklistItem.find_by_id(params["checklistitem_id"])
-
     respond_to do |format|
-      if chkItem
-        checked = params[:checked] == "true"
-        protocol = chkItem.checklist.step.protocol
-        changed = chkItem.checked != checked
-        chkItem.checked = checked
+      checked = params[:checked] == "true"
+      changed = @chk_item.checked != checked
+      @chk_item.checked = checked
 
-        if chkItem.save
-          format.json { render json: {}, status: :accepted }
+      if @chk_item.save
+        format.json { render json: {}, status: :accepted }
 
-          # Create activity
-          if changed
-            str = if checked
-                    'activities.check_step_checklist_item'
-                  else
-                    'activities.uncheck_step_checklist_item'
-                  end
-            completed_items = chkItem.checklist.checklist_items
+        # Create activity
+        if changed
+          str = if checked
+                  'activities.check_step_checklist_item'
+                else
+                  'activities.uncheck_step_checklist_item'
+                end
+          completed_items = @chk_item.checklist.checklist_items
                                      .where(checked: true).count
-            all_items = chkItem.checklist.checklist_items.count
-            text_activity = smart_annotation_parser(chkItem.text)
-                            .gsub(/\s+/, ' ')
-            message = t(
-              str,
-              user: current_user.full_name,
-              checkbox: text_activity,
-              step: chkItem.checklist.step.position + 1,
-              step_name: chkItem.checklist.step.name,
-              completed: completed_items,
-              all: all_items
-            )
+          all_items = @chk_item.checklist.checklist_items.count
+          text_activity = smart_annotation_parser(@chk_item.text)
+                          .gsub(/\s+/, ' ')
+          message = t(
+            str,
+            user: current_user.full_name,
+            checkbox: text_activity,
+            step: @chk_item.checklist.step.position + 1,
+            step_name: @chk_item.checklist.step.name,
+            completed: completed_items,
+            all: all_items
+          )
 
-            # This should always hold true (only in module can
-            # check items be checked, but still check just in case)
-            if protocol.in_module?
-              Activity.create(
-                user: current_user,
-                project: protocol.my_module.experiment.project,
-                experiment: protocol.my_module.experiment,
-                my_module: protocol.my_module,
-                message: message,
-                type_of: if checked
-                           :check_step_checklist_item
-                         else
-                           :uncheck_step_checklist_item
-                         end
-              )
-            end
+          # This should always hold true (only in module can
+          # check items be checked, but still check just in case)
+          if @protocol.in_module?
+            Activity.create(
+              user: current_user,
+              project: @protocol.my_module.experiment.project,
+              experiment: @protocol.my_module.experiment,
+              my_module: @protocol.my_module,
+              message: message,
+              type_of: if checked
+                         :check_step_checklist_item
+                       else
+                         :uncheck_step_checklist_item
+                       end
+            )
           end
-        else
-          format.json {
-            render json: {}, status: :unauthorized
-          }
         end
       else
-        format.json {
-          render json: {}, status: :not_found
-        }
+        format.json { render json: {}, status: :unprocessable_entity }
       end
     end
   end
@@ -333,32 +323,31 @@ class StepsController < ApplicationController
   def toggle_step_state
     respond_to do |format|
       completed = params[:completed] == 'true'
-      protocol = step.protocol
-      changed = step.completed != completed
-      step.completed = completed
+      changed = @step.completed != completed
+      @step.completed = completed
 
       # Update completed_on
       if changed
-        step.completed_on = completed ? Time.current : nil
+        @step.completed_on = completed ? Time.current : nil
       end
 
-      if step.save
-        if protocol.in_module?
-          ready_to_complete = protocol.my_module.check_completness_status
+      if @step.save
+        if @protocol.in_module?
+          ready_to_complete = @protocol.my_module.check_completness_status
         end
 
         # Create activity
         if changed
-          completed_steps = protocol.steps.where(completed: true).count
-          all_steps = protocol.steps.count
+          completed_steps = @protocol.steps.where(completed: true).count
+          all_steps = @protocol.steps.count
           str = 'activities.uncomplete_step'
           str = 'activities.complete_step' if completed
 
           message = t(
             str,
             user: current_user.full_name,
-            step: step.position + 1,
-            step_name: step.name,
+            step: @step.position + 1,
+            step_name: @step.name,
             completed: completed_steps,
             all: all_steps
           )
@@ -366,12 +355,12 @@ class StepsController < ApplicationController
           # Toggling step state can only occur in
           # module protocols, so my_module is always
           # not nil; nonetheless, check if my_module is present
-          if protocol.in_module?
+          if @protocol.in_module?
             Activity.create(
               user: current_user,
-              project: protocol.my_module.experiment.project,
-              experiment: protocol.my_module.experiment,
-              my_module: protocol.my_module,
+              project: @protocol.my_module.experiment.project,
+              experiment: @protocol.my_module.experiment,
+              my_module: @protocol.my_module,
               message: message,
               type_of: completed ? :complete_step : :uncomplete_step
             )
@@ -385,7 +374,7 @@ class StepsController < ApplicationController
                             t('protocols.steps.options.uncomplete_title')
                           end
         format.json do
-          if ready_to_complete && protocol.my_module.uncompleted?
+          if ready_to_complete && @protocol.my_module.uncompleted?
             render json: {
               task_ready_to_complete: true,
               new_title: localized_title
@@ -395,9 +384,7 @@ class StepsController < ApplicationController
           end
         end
       else
-        format.json {
-          render json: {}, status: :unprocessable_entity
-        }
+        format.json { render json: {}, status: :unprocessable_entity }
       end
     end
   end
@@ -580,6 +567,9 @@ class StepsController < ApplicationController
   def load_vars
     @step = Step.find_by_id(params[:id])
     @protocol = @step.protocol
+    if params[:checklistitem_id]
+      @chk_item = ChecklistItem.find_by_id(params[:checklistitem_id])
+    end
 
     unless @protocol
       render_404
@@ -629,8 +619,7 @@ class StepsController < ApplicationController
   end
 
   def check_complete_and_checkbox_permissions
-    render_403 unless @step.present? &&
-                      can_complete_or_checkbox_step?(@protocol)
+    render_403 unless can_complete_or_checkbox_step?(@protocol)
   end
 
   def step_params
