51af74071c90d9c51dc77d4b63a7b2b9bc9fc2f6
zmagod <zmago_devetak@yahoo.com>
2018-03-13 10:52:53 +0100

    remove comments and activity modal

--- app/assets/javascripts/activities/index.js
+++ app/assets/javascripts/activities/index.js
@@ -3,10 +3,8 @@
 (function() {
   function initActivitiesButton() {
     $(document).ready(function() {
-      // Activity feed modal in main navigation menu
       $(document).find('.btn-more-activities')
         .on('ajax:success', function(e, data) {
-          debugger;
           $(data.html).insertAfter($('#list-activities li').last());
           if(data.more_url) {
             $(this).attr('href', data.more_url);
