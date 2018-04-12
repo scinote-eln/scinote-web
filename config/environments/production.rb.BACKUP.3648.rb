8f924fa742c9febb8b7a27b93c00195edf567e95
Raghuraman Venkatapathy <raghuraman.venkatapathy@ptsied.com>
2018-04-12 06:05:58 -0400

    Latest RTP version as of 04/12/2018

---
+++ config/environments/production.rb.BACKUP.3648.rb
@@ -0,0 +1,173 @@
+Rails.application.configure do
+  # Settings specified here will take precedence over those in config/application.rb.
+
+  # Code is not reloaded between requests.
+  config.cache_classes = true
+
+  # Eager load code on boot. This eager loads most of Rails and
+  # your application in memory, allowing both threaded web servers
+  # and those relying on copy on write to perform better.
+  # Rake tasks automatically ignore this option for performance.
+  config.eager_load = true
+
+  # Full error reports are disabled and caching is turned on.
+  config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
+
+  Rails.application.routes.default_url_options = {
+    host: Rails.application.secrets.mail_server_url
+  }
+
+  # Don't care if the mailer can't send.
+  config.action_mailer.default_url_options = {
+    host: Rails.application.secrets.mail_server_url
+  }
+  config.action_mailer.default_options = {
+    from: Rails.application.secrets.mailer_from,
+    reply_to: Rails.application.secrets.mailer_reply_to
+  }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: Rails.application.secrets.mailer_address,
+    port: Rails.application.secrets.mailer_port,
+    domain: Rails.application.secrets.mailer_domain,
+    authentication: "plain",
+    enable_starttls_auto: true,
+#    ssl: true,
+    user_name: Rails.application.secrets.mailer_user_name,
+    password: Rails.application.secrets.mailer_password
+  }
+  config.action_mailer.perform_deliveries = true
+
+  # Enable Rack::Cache to put a simple HTTP cache in front of your application
+  # Add `rack-cache` to your Gemfile before enabling this.
+  # For large-scale production use, consider using a caching reverse proxy like
+  # NGINX, varnish or squid.
+  # config.action_dispatch.rack_cache = true
+
+  # Compress JavaScripts and CSS.
+  config.assets.js_compressor = :uglifier
+  # config.assets.css_compressor = :sass
+
+  # Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.compile = false
+
+  # Asset digests allow you to set far-future HTTP expiration dates on all assets,
+  # yet still be able to expire them through the digest params.
+  config.assets.digest = true
+
+  # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
+
+  # Specifies the header that your server uses for sending files.
+  # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
+  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
+
+  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
+<<<<<<< HEAD
+=======
+  # changed this to true
+>>>>>>> f46dad8485b273ccec1c355c7fc1096b8396a4b0
+  config.force_ssl = true #ENV['RAILS_FORCE_SSL'].present?
+
+  # Display info and higher on production.
+  config.log_level = :info
+
+  # Prepend all log lines with the following tags.
+  # config.log_tags = [ :subdomain, :uuid ]
+
+  # Use a different logger for distributed setups.
+  # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
+
+  # Use a different cache store in production.
+  # config.cache_store = :mem_cache_store
+
+  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
+  # config.action_controller.asset_host = 'http://assets.example.com'
+
+  # Ignore bad email addresses and do not raise email delivery errors.
+  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+   config.action_mailer.raise_delivery_errors = true
+
+  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
+  # the I18n.default_locale when a translation cannot be found).
+  config.i18n.fallbacks = true
+
+  # Send deprecation notices to registered listeners.
+  config.active_support.deprecation = :notify
+
+  # Use default logging formatter so that PID and timestamp are not suppressed.
+  config.log_formatter = ::Logger::Formatter.new
+
+  # Do not dump schema after migrations.
+  config.active_record.dump_schema_after_migration = false
+
+  # Enable/disable Deface
+  config.deface.enabled = ENV['DEFACE_ENABLED'] != 'false'
+
+  # Enable first-time tutorial for users signing in the sciNote for
+  # the first time.
+  config.x.enable_tutorial = ENV['ENABLE_TUTORIAL'] != 'false'
+
+  # Enable reCAPTCHA
+  config.x.enable_recaptcha = ENV['ENABLE_RECAPTCHA'] == 'true'
+
+  # Enable email confirmations
+  config.x.enable_email_confirmations =
+    ENV['ENABLE_EMAIL_CONFIRMATIONS'] == 'true'
+
+  # Enable user registrations
+  config.x.enable_user_registration =
+    ENV['ENABLE_USER_REGISTRATION'] == 'false' ? false : true
+
+  # Use a different logger for distributed setups.
+  # require 'syslog/logger'
+  # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
+
+  if ENV['RAILS_LOG_TO_STDOUT'].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+  end
+
+  # Use a different cache store in production.
+  # config.cache_store = :mem_cache_store
+
+  # Use a real queuing backend for Active Job (and separate queues per environment)
+  config.active_job.queue_adapter     = :delayed_job
+  config.active_job.queue_name_prefix = "scinote_#{Rails.env}"
+  config.action_mailer.perform_caching = false
+
+  # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
+
+  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
+  # config.action_controller.asset_host = 'http://assets.example.com'
+
+  # Specifies the header that your server uses for sending files.
+   config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
+  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
+
+  # Mount Action Cable outside main process or domain
+  # config.action_cable.mount_path = nil
+  # config.action_cable.url = 'wss://example.com/cable'
+  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+
+  # Disable serving static files from the `/public` folder by default since
+  # Apache or NGINX already handles this.
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
+  # Attempt to read encrypted secrets from `config/secrets.yml.enc`.
+  # Requires an encryption key in `ENV["RAILS_MASTER_KEY"]` or
+  # `config/secrets.yml.key`.
+  config.read_encrypted_secrets = true
+
+  # Enable new team on sign up
+  new_team_on_signup = ENV['NEW_TEAM_ON_SIGNUP'] || 'true'
+  if new_team_on_signup == 'true'
+    config.x.new_team_on_signup = true
+  else
+    config.x.new_team_on_signup = false
+    config.x.enable_tutorial = false
+  end
+end
