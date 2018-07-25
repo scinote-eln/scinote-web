221a59e7d606fde8e44eb36c9da93e712e15a38d
mlorb <mlorber@biosistemika.com>
2018-07-25 16:36:13 +0200

    add deletion of asset files

--- app/services/user_data_deletion.rb
+++ app/services/user_data_deletion.rb
@@ -1,7 +1,43 @@
 class UserDataDeletion
   def self.delete_team_data(team)
     team.transaction(isolation: :serializable) do
+      # Destroy tiny_mce_assets
+      if team.tiny_mce_assets.present?
+        team.tiny_mce_assets.each do |tiny_mce_asset|
+          paperclip_file_destroy(tiny_mce_asset)
+        end
+      end
+
+      # Destroy step assets
+      team.protocols.each do |protocol|
+        protocol.steps.each do |step|
+          next unless step.assets.present?
+          step.assets.each do |asset|
+            paperclip_file_destroy(asset)
+          end
+        end
+      end
+
+      # Destroy repository_cell assets
+      team.repositories.each do |repository|
+        repository.repository_rows.each do |repository_row|
+          repository_row.repository_cells.each do |repository_cell|
+            next unless repository_cell.value_type == 'RepositoryAssetValue'
+            paperclip_file_destroy(repository_cell.value.asset)
+          end
+        end
+      end
+
       team.projects.each do |project|
+        # Destroy result assets
+        project.experiments.each do |experiment|
+          experiment.my_modules.each do |my_module|
+            my_module.results.each do |result|
+              next unless result.asset.present?
+              paperclip_file_destroy(result.asset)
+            end
+          end
+        end
         project.user_projects.destroy_all
         project.activities.destroy_all
         project.tags.destroy_all
@@ -36,5 +72,24 @@ class UserDataDeletion
     end
     # Now, simply destroy all user notification relations left
     user.user_notifications.destroy_all
+  end
+
+  # Workaround for Paperclip preserve_files option, to delete files anyway;
+  # if you call #clear with a list of styles to delete,
+  # it'll call #queue_some_for_delete, which doesn't check :preserve_files.
+  def self.paperclip_file_destroy(asset)
+    if asset.class.name == 'TinyMceAsset'
+      all_styles = asset.image.styles.keys.map do |key|
+        asset.image.styles[key].name
+      end << :original
+      asset.image.clear(*all_styles)
+    else
+      all_styles = asset.file.styles.keys.map do |key|
+        asset.file.styles[key].name
+      end << :original
+      asset.file.clear(*all_styles)
+    end
+    asset.save
+    asset.destroy!
   end
 end
