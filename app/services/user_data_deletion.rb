bacb23fe481cbc5a22363d071bb960c74ab0c44e
mlorb <mlorber@biosistemika.com>
2018-07-26 11:28:43 +0200

    fix script to work without deploy

--- app/services/user_data_deletion.rb
+++ app/services/user_data_deletion.rb
@@ -7,53 +7,97 @@ class UserDataDeletion
           paperclip_file_destroy(tiny_mce_asset)
         end
       end
-
-      # Destroy step assets
-      team.protocols.each do |protocol|
-        protocol.steps.each do |step|
-          next unless step.assets.present?
-          step.assets.each do |asset|
-            paperclip_file_destroy(asset)
-          end
-        end
+      team.protocols.where(my_module: nil).each do |protocol|
+        destroy_protocol(protocol)
       end
-
-      # Destroy repository_cell assets
       team.repositories.each do |repository|
         repository.repository_rows.each do |repository_row|
+          # Destroy repository_cell assets
           repository_row.repository_cells.each do |repository_cell|
             next unless repository_cell.value_type == 'RepositoryAssetValue'
             paperclip_file_destroy(repository_cell.value.asset)
           end
         end
+        repository.destroy
       end
-
       team.projects.each do |project|
-        # Destroy result assets
         project.experiments.each do |experiment|
           experiment.my_modules.each do |my_module|
+            # Destroy result assets
             my_module.results.each do |result|
+              result.result_table.delete if result.result_table.present?
+              result.table.delete if result.table.present?
               next unless result.asset.present?
               paperclip_file_destroy(result.asset)
             end
+            my_module.results.destroy_all
+            my_module.my_module_tags.destroy_all
+            my_module.task_comments.destroy_all
+            my_module.outputs.destroy_all
+            my_module.my_module_repository_rows.destroy_all
+            my_module.user_my_modules.destroy_all
+            my_module.activities.destroy_all
+            my_module.report_elements.destroy_all
+            my_module.sample_my_modules.destroy_all
+            my_module.protocols.each do |protocol|
+              destroy_protocol(protocol)
+            end
+            my_module.delete
           end
+          # Destroy workflow image
+          paperclip_file_destroy(experiment)
+
+          experiment.report_elements.destroy_all
+          experiment.activities.destroy_all
+          experiment.my_module_groups.delete_all
+          experiment.delete
         end
         project.user_projects.destroy_all
         project.activities.destroy_all
         project.tags.destroy_all
-        project.experiments.destroy_all
-        project.destroy
+        project.project_comments.destroy_all
+        project.report_elements.destroy_all
+        project.reports.destroy_all
+        project.delete
       end
       team.samples.destroy_all
+      team.samples_tables.destroy_all
       team.sample_groups.destroy_all
       team.sample_types.destroy_all
       team.custom_fields.destroy_all
-      team.user_teams.destroy_all
+      team.protocol_keywords.destroy_all
+      team.user_teams.delete_all
       User.where(current_team_id: team).each do |user|
         user.update(current_team_id: nil)
       end
-      team.destroy!
+      team.reports.destroy_all
+      team.delete
     end
+  end
+
+  def self.destroy_protocol(protocol)
+    protocol.steps.each do |step|
+      # Destroy step assets
+      if step.assets.present?
+        step.assets.each do |asset|
+          paperclip_file_destroy(asset)
+        end
+      end
+      # Destroy step
+      step.tables.destroy_all
+      step.step_tables.delete_all
+      step.report_elements.destroy_all
+      step.step_comments.destroy_all
+      step.step_assets.destroy_all
+      step.checklists.destroy_all
+      step.assets.destroy_all
+      step.tiny_mce_assets.destroy_all
+      step.delete
+    end
+    # Destroy protocol
+    protocol.protocol_protocol_keywords.destroy_all
+    protocol.protocol_keywords.destroy_all
+    protocol.delete
   end
 
   def self.destroy_notifications(user)
@@ -83,6 +127,11 @@ class UserDataDeletion
         asset.image.styles[key].name
       end << :original
       asset.image.clear(*all_styles)
+    elsif asset.class.name == 'Experiment'
+      all_styles = asset.workflowimg.styles.keys.map do |key|
+        asset.workflowimg.styles[key].name
+      end << :original
+      asset.workflowimg.clear(*all_styles)
     else
       all_styles = asset.file.styles.keys.map do |key|
         asset.file.styles[key].name
