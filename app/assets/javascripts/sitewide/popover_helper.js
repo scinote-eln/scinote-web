d2373980d4c7247a16746ca5c8e81de665366b1d
Zanz2 <zan.zagar@hotmail.com>
2018-08-16 15:07:59 +0200

    Testing out new popover

--- app/assets/javascripts/sitewide/popover_helper.js
+++ app/assets/javascripts/sitewide/popover_helper.js
@@ -2,25 +2,29 @@
   'use strict';
 
   $(document).ready(function() {
-    var hover_timer
-    var text_to_display = $('.popover_v2').data('content')
     $('.popover_v2').popover({
       html: true,
       container: 'body',
       placement: 'auto right',
       trigger: 'manual',
-      template: '<div class="popover" role="tooltip"><div class="popover-body">'+text_to_display+'</div></div>',
+      template: '<div class="popover popover_v2_window" role="tooltip"><div class="popover-body">' +
+      $('.popover_v2').data('content') +
+      '</div><div class="popover-footer" style="position:absolute;bottom:5px;right:5px;">' +
+      '<a class="ax_default link_button" href="https://www.google.si"><i class="fas fa-external-link-alt">nekaj</i></a></div></div>'
     }).on("mouseenter", function () {
-        hover_timer = setTimeout(function () {
-          $('.popover_v2').popover("show")
-        }, 1000);
-        $('body').find(".popover").on("mouseleave", function () {
-            $('.popover_v2').popover('hide');
-        });
-    }).on("mouseleave", function () {
-        clearTimeout(hover_timer)
         setTimeout(function () {
-            if (!$(".popover:hover").length) {
+          $('.popover_v2').popover("show")
+          var top = $('.popover_v2').offset().top;
+          $('.popover_v2_window').css({
+              top: (top) + 'px'
+          });
+          $(".popover_v2_window").on("mouseleave", function () {
+              $('.popover_v2').popover('hide');
+          });
+        }, 1000);
+    }).on("mouseleave", function () {
+        setTimeout(function () {
+            if (!$(".popover_v2_window:hover").length) {
                 $('.popover_v2').popover("hide")
             }
         }, 500);
