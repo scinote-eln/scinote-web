637c7dfb1982aeff924ac1c6b260e91f9b22ce20
Zanz2 <zan.zagar@hotmail.com>
2018-08-17 11:33:44 +0200

    modified styling and behaviour of popovers (tooltips) also made relevant text changes, except for last one in epic

--- app/assets/javascripts/sitewide/popover_helper.js
+++ app/assets/javascripts/sitewide/popover_helper.js
@@ -3,32 +3,36 @@
 
   $(document).ready(function() {
     $('.popover_v2').each(function(i,obj) {
-
+      var link = $(obj).data('popover-link')
       $(obj).popover({
         html: true,
         container: 'body',
         placement: 'auto right',
         trigger: 'manual',
-        template: '<div class="popover popover_'+i+'_window" role="tooltip"><div class="popover-body">' +
+        template: '<div class="popover popover_'+i+'_window" role="tooltip" style="background-color:#F0F0F0;font-family:Lato;font-size:14px;" color="#000000" ><div class="popover-body" >' +
         $(obj).data('content') +
-        '</div><br><br><div class="popover-footer" style="position:absolute;bottom:5px;right:5px;background-color: 	#E8E8E8;border-radius: 3px;">' +
-        '<a style="color:grey;" class="btn btn-link href="https://www.google.si">Read more &nbsp&nbsp<i class="fas fa-external-link-alt"></i></a></div></div>'
+        '</div><br><br><br><div class="popover-footer" style="position:absolute;bottom:5px;right:5px;background-color:#E8E8E8;border-radius: 3px;">' +
+        '<a style="color:grey;" class="btn btn-link text-nowrap" href="'+link+'">Read more&nbsp;&nbsp;&nbsp;<i class="fas fa-external-link-alt"></i></a></div></div>'
       }).on("mouseenter", function () {
           setTimeout(function () {
             if ($(obj).hover().length) {
               $(obj).popover("show")
               var top = $(obj).offset().top;
-              $('body').find('.popover_'+i+'_window').css({
+              $('.popover_'+i+'_window').css({
                   top: (top) + 'px'
               });
-              $('body').find(".popover_"+i+"_window").on("mouseleave", function () {
+              $(".popover_"+i+"_window").on("mouseleave", function () {
+                $(".popover_"+i+"_window").css("background-color","#F0F0F0");
                   $(obj).popover('hide');
+              });
+              $(".popover_"+i+"_window").on("mouseenter", function () {
+                  $(".popover_"+i+"_window").css("background-color","#DADADA");
               });
             }
           }, 1000);
       }).on("mouseleave", function () {
           setTimeout(function () {
-              if (!$('body').find(".popover_"+i+"_window:hover").length) {
+              if (!$(".popover_"+i+"_window:hover").length) {
                   $(obj).popover("hide")
               }
           }, 500);
