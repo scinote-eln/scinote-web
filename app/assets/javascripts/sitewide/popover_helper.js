8c492e1491a12d8835177ccc38fec2d74d4903a7
Zanz2 <zan.zagar@hotmail.com>
2018-08-17 16:03:49 +0200

    started implementing tooltips, and upgraded behaviour, also trying to fix some bugs with data content attribute

--- app/assets/javascripts/sitewide/popover_helper.js
+++ app/assets/javascripts/sitewide/popover_helper.js
@@ -4,14 +4,23 @@
   $(document).ready(function() {
     //if( user has popovers turned on from settings ){
       $('.popover_v2').each(function(i,obj) {
-        var link = $(obj).data('popover-link')
+        var link = $(obj).data('popoverlink')
+        var text_data = $(obj).data('content')
+        var needed_attribute = $(obj).attr("data-content");
+        if (typeof needed_attribute !== typeof undefined && needed_attribute !== false && needed_attribute !== "") {
+          // Do nothing, will negate the above logic later
+        }else{
+          $(obj).attr( "data-content","content" )
+          // without a data-content attribute in its parent element, the popover will NEVER render
+          // it assumes it is empty because I fill data trough the template.
+        }
         $(obj).popover({
           html: true,
           container: 'body',
           placement: 'auto right',
           trigger: 'manual',
           template: '<div class="popover popover_'+i+'_window" role="tooltip" style="background-color:#F0F0F0;font-family:Lato;font-size:14px;" color="#000000" ><div class="popover-body" >' +
-          $(obj).data('content') +
+          text_data +
           '</div><br><br><br><div class="popover-footer" style="position:absolute;bottom:5px;right:5px;background-color:#E8E8E8;border-radius: 3px;">' +
           '<a style="color:grey;" class="btn btn-link text-nowrap" href="'+link+'">Read more&nbsp;&nbsp;&nbsp;<i class="fas fa-external-link-alt"></i></a></div></div>'
         }).on("mouseenter", function () {
@@ -37,6 +46,8 @@
                     $(obj).popover("hide")
                 }
             }, 500);
+        }).on('shown', function(){
+          // hide all other popovers
         });
       })
   //}
@@ -44,5 +55,5 @@
 }());
 
 /* copy and paste shortcut
-<button class="btn btn-default popover_v2" data-popover-link="popover_test.link" data-content="<%= I18n.t('popover_test.text') %>">
+<button class="btn btn-default popover_v2" data-popoverlink="<%= I18n.t('popover_test.link.') %>" data-popovercontent="<%= I18n.t('popover_test.text.') %>">
 */
