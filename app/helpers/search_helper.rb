a844ec4d111a2d132a8b0c72dfe6b7022eb6a6b4
Zanz2 <zan.zagar@hotmail.com>
2018-08-23 14:45:55 +0200

    work in progress, havent fixed anything yet, found out that this is not really a bug, this had to have been in scinote since the start, since some paths dont generate the team id parameter to switch

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -7,13 +7,34 @@ module SearchHelper
     experiments.uniq
   end
 
+  def user_current_team_change(new_team_id)
+    team_id = new_team_id.to_i
+    if @user.teams_ids.include?(team_id)
+      @user.current_team_id = team_id
+      @changed_team = Team.find_by_id(@user.current_team_id)
+      if @user.save
+        flash[:success] = t('users.settings.changed_team_flash',
+                            team: @changed_team.name)
+        redirect_to root_path
+        return
+      end
+    end
+    flash[:alert] = t('users.settings.changed_team_error_flash')
+    redirect_back(fallback_location: root_path)
+  end
+
   def route_to_other_team(path, search_team, text)
     if search_team != current_team
+      user_current_team_change(search_team.id) unless path.include?('?team=')
       link_to text,
               path,
-              data: { no_turbolink: true,
-                      confirm: t('users.settings.changed_team_in_search',
-                                 team: search_team.name) }
+              data: {
+                no_turbolink: true,
+                confirm: t(
+                  'users.settings.changed_team_in_search',
+                  team: search_team.name
+                )
+              }
     else
       link_to text, path, data: { no_turbolink: true }
     end
@@ -21,13 +42,19 @@ module SearchHelper
 
   def route_to_other_team_btn(path, search_team, text)
     if search_team != current_team
+
       link_to text,
               path,
               class: 'btn btn-success',
-              data: { confirm: t('users.settings.changed_team_in_search',
-                                 team: search_team.name) }
+              data: {
+                no_turbolink: true,
+                confirm: t(
+                  'users.settings.changed_team_in_search',
+                  team: search_team.name
+                )
+              }
     else
-      link_to text, path, class: 'btn btn-success'
+      link_to text, path, class: 'btn btn-success', data: { no_turbolink: true }
     end
   end
 end
