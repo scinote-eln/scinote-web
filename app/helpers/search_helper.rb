2cd600b27eb0be02de245e8ea6297d6ee2a1c751
Zanz2 <zan.zagar@hotmail.com>
2018-08-24 13:50:39 +0200

    saving work, still testing what possible solutions there are

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -7,25 +7,10 @@ module SearchHelper
     experiments.uniq
   end
 
-  def user_current_team_change(new_team_id)
-    team_id = new_team_id.to_i
-    if @user.teams_ids.include?(team_id)
-      @user.current_team_id = team_id
-      @changed_team = Team.find_by_id(@user.current_team_id)
-      if @user.save
-        flash[:success] = t('users.settings.changed_team_flash',
-                            team: @changed_team.name)
-        redirect_to root_path
-        return
-      end
-    end
-    flash[:alert] = t('users.settings.changed_team_error_flash')
-    redirect_back(fallback_location: root_path)
-  end
-
   def route_to_other_team(path, search_team, text)
     if search_team != current_team
-      user_current_team_change(search_team.id) unless path.include?('?team=')
+      #byebug
+      current_team_switch(search_team)
       link_to text,
               path,
               data: {
@@ -42,7 +27,6 @@ module SearchHelper
 
   def route_to_other_team_btn(path, search_team, text)
     if search_team != current_team
-
       link_to text,
               path,
               class: 'btn btn-success',
