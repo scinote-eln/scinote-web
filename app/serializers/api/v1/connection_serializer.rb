6a4801466d7615c3b3be673686c5772e1322f9fc
Zanz2 <zan.zagar@hotmail.com>
2018-09-10 15:21:45 +0200

    Fixed serializer types

--- app/serializers/api/v1/connection_serializer.rb
+++ app/serializers/api/v1/connection_serializer.rb
@@ -3,7 +3,7 @@
 module Api
   module V1
     class ConnectionSerializer < ActiveModel::Serializer
-      type :connection
+      type :connections
       attributes :id, :input_id, :output_id
     end
   end
