43e6524392697a3ff8bb55896359686c072d7f16
Zanz2 <zan.zagar@hotmail.com>
2018-09-11 10:31:16 +0200

    standardized serializers by removing specified attributes

--- app/serializers/api/v1/report_serializer.rb
+++ app/serializers/api/v1/report_serializer.rb
@@ -4,8 +4,7 @@ module Api
   module V1
     class ReportSerializer < ActiveModel::Serializer
       type :reports
-      attributes :id, :name, :description, :project_id, :user_id, :created_at,
-                 :updated_at, :last_modified_by_id, :team_id
+      attributes :id, :name, :description, :project_id
 
       belongs_to :project, serializer: ProjectSerializer
     end
