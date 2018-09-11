68385b36cc458c4ac2ee507cd712a0fff760b816
Zanz2 <zan.zagar@hotmail.com>
2018-09-11 15:41:38 +0200

    removed attributes

--- app/serializers/api/v1/protocol_serializer.rb
+++ app/serializers/api/v1/protocol_serializer.rb
@@ -4,8 +4,8 @@ module Api
   module V1
     class ProtocolSerializer < ActiveModel::Serializer
       type :protocols
-      attributes :id, :name, :authors, :description, :added_by_id,
-                 :my_module_id, :team_id, :protocol_type, :parent_id,
+      attributes :id, :name, :authors, :description,
+                 :my_module_id, :team_id, :protocol_type,
                  :nr_of_linked_children
       belongs_to :my_module, serializer: MyModuleSerializer
     end
