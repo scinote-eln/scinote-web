81115f411aba45f8d2f3feb1df0bf234e861e954
Zanz2 <zan.zagar@hotmail.com>
2018-09-13 11:46:25 +0200

    Renaming my module attribute to tasks

--- app/serializers/api/v1/activity_serializer.rb
+++ app/serializers/api/v1/activity_serializer.rb
@@ -4,8 +4,9 @@ module Api
   module V1
     class ActivitySerializer < ActiveModel::Serializer
       type :activities
-      attributes :id, :my_module_id, :user_id, :type_of, :message,
+      attributes :id, :user_id, :type_of, :message,
                  :project_id, :experiment_id
+      attribute :my_module_id, key: :task_id
       belongs_to :my_module, serializer: MyModuleSerializer, optional: true
       belongs_to :experiment, serializer: ExperimentSerializer, optional: true
       belongs_to :project, serializer: ProjectSerializer
