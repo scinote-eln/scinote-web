0643ba216ed014624b64173be47e1a63d9a8c743
Luka Murn <luka.murn@biosistemika.com>
2018-10-07 00:53:39 +0200

    Fix GET /connections, /connections/:id endpoints

--- app/serializers/api/v1/connection_serializer.rb
+++ app/serializers/api/v1/connection_serializer.rb
@@ -4,16 +4,13 @@ module Api
   module V1
     class ConnectionSerializer < ActiveModel::Serializer
       type :connections
-      attributes :id, :input_id, :output_id
-      has_one :input_task, serializer: TaskSerializer
-      has_one :output_task, serializer: TaskSerializer
-      def input_task
-        MyModule.find(object.input_id)
-      end
-
-      def output_task
-        MyModule.find(object.output_id)
-      end
+      attributes :id
+      belongs_to :from, key: :input_task,
+                        serializer: TaskSerializer,
+                        class_name: 'MyModule'
+      belongs_to :to, key: :output_task,
+                      serializer: TaskSerializer,
+                      class_name: 'MyModule'
     end
   end
 end
