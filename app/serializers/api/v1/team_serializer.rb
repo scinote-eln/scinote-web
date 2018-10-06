6faaff550b8dc3341a755e763bed4cdf4031008e
Luka Murn <luka.murn@biosistemika.com>
2018-10-06 12:18:57 +0200

    Update GET /teams, GET teams/:team_id endpoints slightly

--- app/serializers/api/v1/team_serializer.rb
+++ app/serializers/api/v1/team_serializer.rb
@@ -4,6 +4,7 @@ module Api
   module V1
     class TeamSerializer < ActiveModel::Serializer
       attributes :id, :name, :description, :space_taken
+      belongs_to :created_by, serializer: UserSerializer
     end
   end
 end
