c0f47f03b7cfbb7ede2c722499501b398cbcfe56
Luka Murn <luka.murn@biosistemika.com>
2018-10-11 08:51:14 +0200

    Hound is love, Hound is life

--- app/serializers/api/v1/comment_serializer.rb
+++ app/serializers/api/v1/comment_serializer.rb
@@ -7,21 +7,25 @@ module Api
       attributes :id, :message
 
       belongs_to :user, serializer: UserSerializer
-      belongs_to :project, serializer: ProjectSerializer,
-                           if: -> { object.class == ProjectComment &&
-                                    !instance_options[:hide_project] }
+      belongs_to :project,
+                 serializer: ProjectSerializer,
+                 if: -> { object.class == ProjectComment &&
+                          !instance_options[:hide_project] }
       # TODO
-      #belongs_to :my_module, serializer: TaskSerializer,
-      #                       key: :task,
-      #                       class_name: 'MyModule',
-      #                       if: -> { object.class == TaskComment &&
-      #                                !instance_options[:hide_task] }
-      #belongs_to :step, serializer: StepSerializer,
-      #                  if: -> { object.class == StepComment &&
-      #                           !instance_options[:hide_step] }
-      belongs_to :result, serializer: ResultSerializer,
-                          if: -> { object.class == ResultComment &&
-                                   !instance_options[:hide_result] }
+      #belongs_to :my_module,
+      #           serializer: TaskSerializer,
+      #           key: :task,
+      #           class_name: 'MyModule',
+      #           if: -> { object.class == TaskComment &&
+      #                    !instance_options[:hide_task] }
+      #belongs_to :step,
+      #           serializer: StepSerializer,
+      #           if: -> { object.class == StepComment &&
+      #                    !instance_options[:hide_step] }
+      belongs_to :result,
+                 serializer: ResultSerializer,
+                 if: -> { object.class == ResultComment &&
+                          !instance_options[:hide_result] }
     end
   end
 end
