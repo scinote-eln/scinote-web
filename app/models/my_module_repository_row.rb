e62e1873b07d45cb0ce021889ad45e04c97d9b12
Oleksii Kriuchykhin <okriuchykhin@biosistemika.com>
2019-01-17 15:04:20 +0100

    Fix issue with caching in inventories dropdown [SCI-2776]

--- app/models/my_module_repository_row.rb
+++ app/models/my_module_repository_row.rb
@@ -5,9 +5,11 @@ class MyModuleRepositoryRow < ApplicationRecord
              optional: true
   belongs_to :repository_row,
              optional: true,
+             inverse_of: :my_module_repository_rows
+  belongs_to :my_module,
+             optional: true,
              touch: true,
              inverse_of: :my_module_repository_rows
-  belongs_to :my_module, optional: true, inverse_of: :my_module_repository_rows
 
   validates :repository_row, :my_module, presence: true
   validates :repository_row, uniqueness: { scope: :my_module }
