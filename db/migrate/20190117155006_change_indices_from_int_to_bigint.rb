70b1852d64fccafa1568e538370fb1cdd882825f
Miha Mencin <mmencin@biosistemika.com>
2019-01-18 09:33:45 +0100

    SCI-2936 code style

--- db/migrate/20190117155006_change_indices_from_int_to_bigint.rb
+++ db/migrate/20190117155006_change_indices_from_int_to_bigint.rb
@@ -1,22 +1,26 @@
 class ChangeIndicesFromIntToBigint < ActiveRecord::Migration[5.1]
   def up
-  # The request for this migration comes from a ticket SCI-2936,
-  # the request is to change all of the indices (primary and foreign keys) to bigint type
-  # 
-  # What it basically does is finds all of the indices of type int and changes them to bigint
-  # A minor complication/restraint is, postgres prohibits changes of the fields
-  # that are used in postgres view, so firstly we drop all of (user defined) views
-  # then change the column types and recreate vies afterwards.
+    # The request for this migration comes from a ticket SCI-2936,
+    # the request is to change all of the indices (primary and foreign keys)
+    # to bigint type
+    #
+    # What it basically does is finds all of the indices of type int and
+    # changes them to bigint.
+    #
+    # A minor complication/restraint is, postgres prohibits changes
+    # of the fieldsthat are used in postgres view, so firstly we drop all
+    # of (user defined)views then change the column types
+    #  and recreate views afterwards.
 
 
     # find primary and foreign keys of type integer throughout the database
     sql = <<-EOM
       select tc.table_schema, tc.table_name, kc.column_name, ic.data_type
       from information_schema.table_constraints tc
-        join information_schema.key_column_usage kc 
+        join information_schema.key_column_usage kc
           on kc.table_name = tc.table_name and kc.table_schema = tc.table_schema and kc.constraint_name = tc.constraint_name
         join information_schema.columns ic
-          on ic.table_schema = tc.table_schema and ic.table_name=tc.table_name and ic.column_name = kc.column_name 
+          on ic.table_schema = tc.table_schema and ic.table_name=tc.table_name and ic.column_name = kc.column_name
       where (tc.constraint_type = 'PRIMARY KEY' OR tc.constraint_type = 'FOREIGN KEY')
         and kc.ordinal_position is not null
         and ic.data_type = 'integer'
@@ -30,7 +34,7 @@ class ChangeIndicesFromIntToBigint < ActiveRecord::Migration[5.1]
       # get all user defined views
       user_viewes = execute(
         "select  *  from pg_views where schemaname = any (current_schemas(false))"
-                           )
+      )
 
       # drop all existing views
       user_viewes.each do |user_view|
@@ -44,13 +48,15 @@ class ChangeIndicesFromIntToBigint < ActiveRecord::Migration[5.1]
 
       # recreate user defined views
       user_viewes.each do |user_view|
-        execute("create view #{user_view['viewname']} as #{user_view['definition']}")
+        execute(
+          "create view #{user_view['viewname']} as #{user_view['definition']}"
+        )
       end
     end
   end
 
   def down
-    # Bad, bad things can happen if we reverse this migration, so we'll 
+    # Bad, bad things can happen if we reverse this migration, so we'll
     # simply skip it
   end
 end
