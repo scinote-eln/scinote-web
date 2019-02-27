967e6213f310302544e92292f3e811b9966ffde7
Anton Ignatov <aignatov@biosistemika.com>
2019-02-27 15:27:15 +0100

    remove test test query and db sedd examples

--- app/controllers/global_activities_controller.rb
+++ app/controllers/global_activities_controller.rb
@@ -55,10 +55,6 @@ class GlobalActivitiesController < ApplicationController
     end
   end
 
-  def test_query
-    render json: {"Project":[{"id":3,"name":"Demo project - qPCR"},{"id":24,"name":"Demo project - qPCR (1)"},{"id":25,"name":"Demo project - qPCR (2)"},{"id":26,"name":"Demo project - qPCR (3)"},{"id":27,"name":"Demo project - qPCR (4)"},{"id":28,"name":"Demo project - qPCR (5)"},{"id":30,"name":"Demo project - qPCR (6)"},{"id":31,"name":"Demo project - qPCR (7)"},{"id":32,"name":"Demo project - qPCR (8)"},{"id":47,"name":"Demo project - qPCR (9)"},{"id":48,"name":"Demo project - qPCR (10)"},{"id":52,"name":"Demo project - qPCR (11)"},{"id":53,"name":"Demo project - qPCR (12)"},{"id":54,"name":"Demo project - qPCR (13)"},{"id":55,"name":"Demo project - qPCR (14)"},{"id":56,"name":"Demo project - qPCR (15)"},{"id":57,"name":"Demo project - qPCR (16)"},{"id":58,"name":"Demo project - qPCR (17)"},{"id":59,"name":"Demo project - qPCR (18)"},{"id":60,"name":"Demo project - qPCR (19)"}],"Step":[{"id":7021,"name":"Demo step"}]}
-  end
-
   private
 
   def activity_filters
