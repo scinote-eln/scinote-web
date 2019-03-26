576a35c9d60bcf6c3f214a04fc996c36cb5d5395
Anton Ignatov <aignatov@biosistemika.com>
2019-03-26 16:39:56 +0100

    Fix markup

--- app/assets/javascripts/select2_customization.js
+++ app/assets/javascripts/select2_customization.js
@@ -26,7 +26,7 @@ $.fn.extend({
 
     // Create arrow
     $('<div class="select2-arrow"><span class="caret"></span></div>').appendTo(select2.next())
-      .click(() => select2.next().find('.select2-selection').click())
+      .click(() => select2.next().find('.select2-selection').click());
 
     // select all check
     this[0].dataset.singleDisplay = config.singleDisplay || false;
@@ -43,7 +43,7 @@ $.fn.extend({
         var selectElement = this;
         var groups;
         var groupChildrens;
-        $('.select2-dropdown').removeClass('custom-group')
+        $('.select2-dropdown').removeClass('custom-group');
         $('.select2-selection').scrollTo(0);
         $('.select2_select_all').remove();
         // Adding select_all_button
@@ -61,13 +61,13 @@ $.fn.extend({
 
         if (selectElement.dataset.dropdownPosition === 'left') {
           $('.select2-dropdown').parent().addClass('left-position');
-          if (selectElement.dataset.selectAllButton == undefined){
+          if (selectElement.dataset.selectAllButton === undefined) {
             $('.select2-dropdown').parent().addClass('full');
           }
         }
         // Adding select all group members event
         if (selectElement.dataset.selectByGroup === 'true') {
-          $('.select2-dropdown').addClass('custom-group')
+          $('.select2-dropdown').addClass('custom-group');
           setTimeout(() => {
             groups = $('.select2-dropdown').find('.select2-results__group');
             groups.click(e => {
