5008339d8193554583898c0e9fb7153c61cf42c4
Oleksii Kriuchykhin <okriuchykhin@biosistemika.com>
2019-03-28 14:56:34 +0100

    Add handling of links for objects from another team [SCI-3229][SCI-3241]

--- app/helpers/global_activities_helper.rb
+++ app/helpers/global_activities_helper.rb
@@ -29,46 +29,45 @@ module GlobalActivitiesHelper
     return value unless obj
 
     current_value = obj.public_send(getter || 'name')
+    team = activity.team
+    path = ''
 
     case obj
     when User
-      popover_for_user_name(obj, activity.team, false, true)
+      return popover_for_user_name(obj, team, false, true)
     when Tag
       # Not link for now
-      current_value
+      return current_value
     when Team
-      route_to_other_team(projects_path(team: obj), obj, current_value)
+      path = projects_path
     when Repository
-      link_to(current_value, repository_path(obj))
+      path = repository_path(obj)
     when RepositoryRow
-      link_to(current_value, repository_path(obj.repository))
+      path = repository_path(obj.repository)
     when RepositoryColumn
-      link_to(current_value, repository_path(obj.repository))
+      path = repository_path(obj.repository)
     when Project
       path = obj.archived? ? projects_path : project_path(obj)
-      link_to(current_value, path)
     when Experiment
       return current_value unless obj.navigable?
       path = obj.archived? ? experiment_archive_project_path(obj.project) : canvas_experiment_path(obj)
-      link_to(current_value, path)
     when MyModule
       return current_value unless obj.navigable?
       path = obj.archived? ? module_archive_experiment_path(obj.experiment) : protocols_my_module_path(obj)
-      link_to(current_value, path)
     when Protocol
       if obj.in_repository?
-        route_to_other_team(protocols_path, obj.team, current_value)
+        path = protocols_path
       elsif obj.my_module.navigable?
-        link_to(current_value, protocols_my_module_path(obj.my_module))
+        path = protocols_my_module_path(obj.my_module)
       else
-        current_value
+        return current_value
       end
     when Result
       return current_value unless obj.navigable?
       path = obj.archived? ? archive_my_module_path(obj.my_module) : results_my_module_path(obj.my_module)
-      link_to(current_value, path)
     else
-      current_value
+      return current_value
     end
+    route_to_other_team(path, obj, current_value)
   end
 end
