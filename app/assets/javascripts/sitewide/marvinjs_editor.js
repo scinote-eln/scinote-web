baa9b94a63314610bfd0f8e68fcd1dd6b0656883
Anton Ignatov <aignatov@biosistemika.com>
2019-04-26 20:33:20 +0200

    Add new graph button to step

--- app/assets/javascripts/sitewide/marvinjs_editor.js
+++ app/assets/javascripts/sitewide/marvinjs_editor.js
@@ -4,14 +4,51 @@ var MarvinJsEditor = (function() {
   var marvinJsContainer = $('#marvinjs-editor');
   var marvinJsObject = $('#marvinjs-sketch');
 
+  var loadEditor = () => {
+    return MarvinJSUtil.getEditor('#marvinjs-sketch')
+  }
+
   return Object.freeze({
-    open: function() {
+    open: function(config) {
+      MarvinJsEditor().reset()
       $(marvinJsModal).modal('show');
       $(marvinJsObject)
         .css('width', marvinJsContainer.width() + 'px')
         .css('height', marvinJsContainer.height() + 'px')
 
+      marvinJsContainer[0].dataset.objectId = config.objectId;
+      marvinJsContainer[0].dataset.objectType = config.objectType;
+      marvinJsContainer[0].dataset.mode = config.mode;
+      marvinJsModal.find('.file-save-link').off('click').on('click', () => {
+        MarvinJsEditor().save()
+      })
+
+    },
+
+    initNewButton: function(selector) {
+      $(selector).off('click').on('click', function(){
+        var objectId = this.dataset.objectId;
+        var objectType = this.dataset.objectType;
+        MarvinJsEditor().open({
+          mode: 'new',
+          objectId: objectId,
+          objectType: objectType
+        })
+      })
+    },
+
+    save: function(){
+      loadEditor().then(function(sketcherInstance) {
+        sketcherInstance.exportStructure("mrv").then(function(source) {
+          console.log(source);
+        });
+      })
+    },
+
+    reset: function(){
+      marvinJsContainer[0].dataset.objectId = '';
+      marvinJsContainer[0].dataset.objectType = '';
+      marvinJsContainer[0].dataset.mode = '';
     }
   });
 });
-MarvinJsEditor();
