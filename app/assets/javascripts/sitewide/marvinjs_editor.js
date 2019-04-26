40c1c9aef73289a509a2a6b107b5e33a6b64c6c8
Anton Ignatov <aignatov@biosistemika.com>
2019-04-26 18:24:21 +0200

    Add marvin js modal

---
+++ app/assets/javascripts/sitewide/marvinjs_editor.js
@@ -0,0 +1,17 @@
+var MarvinJsEditor = (function() {
+
+  var marvinJsModal = $('#MarvinJsModal');
+  var marvinJsContainer = $('#marvinjs-editor');
+  var marvinJsObject = $('#marvinjs-sketch');
+
+  return Object.freeze({
+    open: function() {
+      $(marvinJsModal).modal('show');
+      $(marvinJsObject)
+        .css('width', marvinJsContainer.width() + 'px')
+        .css('height', marvinJsContainer.height() + 'px')
+
+    }
+  });
+});
+MarvinJsEditor();
