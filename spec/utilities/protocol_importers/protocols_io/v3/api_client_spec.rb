606325d9ade0a89c6fa08e443f92967a8c696be9
Jure Grabnar <jgrabnar@biosistemika.com>
2019-04-29 08:44:09 +0200

    Implement API client for protocols.io
    Closes SCI-3357

---
+++ spec/utilities/protocol_importers/protocols_io/v3/api_client_spec.rb
@@ -0,0 +1,120 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe ProtocolImporters::ProtocolsIO::V3::ApiClient do
+  TOKEN = 'test_token'
+
+  describe '#protocol_list' do
+    URL = 'https://www.protocols.io/api/v3/protocols'
+
+    let(:stub_protocols) do
+      stub_request(:get, URL).with(query: hash_including({}))
+    end
+
+    let(:default_query_params) do
+      {
+        filter: :public,
+        key: '',
+        order_dir: :desc,
+        order_field: :activity,
+        page_id: 1,
+        page_size: 10
+      }
+    end
+
+    it 'returns 200 on successfull call' do
+      stub_protocols.to_return(status: 200, body: '[]', headers: {})
+
+      expect(subject.protocol_list.code).to eq 200
+      expect(stub_protocols).to have_been_requested
+    end
+
+    it 'raises OpenTimeout error on timeout' do
+      stub_protocols.to_timeout
+
+      expect { subject.protocol_list }.to raise_error(Net::OpenTimeout)
+    end
+
+    it 'requests server with default query parameters if none are given' do
+      stub_request(:get, URL).with(query: default_query_params)
+
+      subject.protocol_list
+      expect(WebMock).to have_requested(:get, URL).with(query: default_query_params)
+    end
+
+    it 'requests server with given query parameters' do
+      query = {
+        filter: :user_public,
+        key: 'banana',
+        order_dir: :asc,
+        order_field: :date,
+        page_id: 2,
+        page_size: 15
+      }
+      stub_request(:get, URL).with(query: query)
+
+      subject.protocol_list(query)
+      expect(WebMock).to have_requested(:get, URL).with(query: query)
+    end
+
+    it 'should send authorization token if provided on initialization' do
+      headers = { 'Authorization': "Bearer #{TOKEN}" }
+      stub_request(:get, URL).with(headers: headers, query: default_query_params)
+
+      ProtocolImporters::ProtocolsIO::V3::ApiClient.new(TOKEN).protocol_list
+      expect(WebMock).to have_requested(:get, URL).with(headers: headers, query: default_query_params)
+    end
+  end
+
+  describe '#single_protocol' do
+    PROTOCOL_ID = 15
+    SINGLE_PROTOCOL_URL = "https://www.protocols.io/api/v3/protocols/#{PROTOCOL_ID}"
+
+    let(:stub_single_protocol) do
+      stub_request(:get, SINGLE_PROTOCOL_URL)
+    end
+
+    it 'returns 200 on successfull call' do
+      stub_single_protocol.to_return(status: 200, body: '[]', headers: {})
+
+      expect(subject.single_protocol(PROTOCOL_ID).code).to eq 200
+      expect(stub_single_protocol).to have_been_requested
+    end
+
+    it 'raises OpenTimeout error on timeout' do
+      stub_single_protocol.to_timeout
+
+      expect { subject.single_protocol(PROTOCOL_ID) }.to raise_error(Net::OpenTimeout)
+    end
+
+    it 'should send authorization token if provided on initialization' do
+      headers = { 'Authorization': "Bearer #{TOKEN}" }
+      stub_single_protocol.with(headers: headers)
+
+      ProtocolImporters::ProtocolsIO::V3::ApiClient.new(TOKEN).single_protocol(PROTOCOL_ID)
+      expect(WebMock).to have_requested(:get, SINGLE_PROTOCOL_URL).with(headers: headers)
+    end
+  end
+
+  describe '#protocol_html_preview' do
+    PROTOCOL_URI = 'Extracting-DNA-from-bananas-esvbee6'
+
+    let(:stub_html_preview) do
+      stub_request(:get, "https://www.protocols.io/view/#{PROTOCOL_URI}.html")
+    end
+
+    it 'returns 200 on successfull call' do
+      stub_html_preview.to_return(status: 200, body: '[]', headers: {})
+
+      expect(subject.protocol_html_preview(PROTOCOL_URI).code).to eq 200
+      expect(stub_html_preview).to have_been_requested
+    end
+
+    it 'raises OpenTimeout error on timeout' do
+      stub_html_preview.to_timeout
+
+      expect { subject.protocol_html_preview(PROTOCOL_URI) }.to raise_error(Net::OpenTimeout)
+    end
+  end
+end
