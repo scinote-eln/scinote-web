15cbd8864cc416ae5c82514c06cf6694c02826d5
Urban Rotnik <urban.rotnik@gmail.com>
2019-05-08 13:59:02 +0200

    Add valid factory check to Steps models

--- app/models/result_text.rb
+++ app/models/result_text.rb
@@ -8,5 +8,4 @@ class ResultText < ApplicationRecord
   validates :text, length: { maximum: Constants::RICH_TEXT_MAX_LENGTH }
 
   belongs_to :result, inverse_of: :result_text, touch: true
-  has_many :tiny_mce_assets, inverse_of: :result_text, dependent: :destroy
 end
