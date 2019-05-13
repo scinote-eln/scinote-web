3db61298089e596581f86a873f7be28ce31d9e98
aignatov-bio <47317017+aignatov-bio@users.noreply.github.com>
2019-05-13 08:58:47 +0200

    Fix report saving (#1748)

--- config/initializers/extends.rb
+++ config/initializers/extends.rb
@@ -38,7 +38,8 @@ class Extends
                            project_samples: 14, # TODO
                            experiment: 15,
                            # Higher number because of addons
-                           my_module_repository: 17 }
+                           my_module_repository: 17,
+                           my_module_protocol: 18 }
 
   # Data type name should match corresponding model's name
   REPOSITORY_DATA_TYPES = { RepositoryTextValue: 0,
