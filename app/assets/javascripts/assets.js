4d579db2e2e98e1eb63c051815ba73264aa2e8fc
Oleksii Kriuchykhin <okriuchykhin@biosistemika.com>
2019-06-11 13:14:07 +0200

    Refactor assets polling to use setTimeout [SCI-3461]

--- app/assets/javascripts/assets.js
+++ app/assets/javascripts/assets.js
@@ -1,73 +1,73 @@
 /* global animateSpinner FilePreviewModal */
 
 var Assets = (function() {
-  function refreshAssets() {
+  var DELAY = 5000;
+  var REPETITIONS_LIMIT = 30;
+
+  function finalizeAsset(element) {
+    var $el = $(element);
+
+    $el.attr('data-status', 'asset-failed');
+    if ($el.data('filename')) {
+      $el.html($el.data('filename'));
+    }
+  }
+
+  function refreshAsset(element) {
+    var $el = $(element);
+
+    if ($el.data('refreshCounter') > REPETITIONS_LIMIT) {
+      finalizeAsset($el);
+      return;
+    }
+
+    // Perform an AJAX call to present URL
+    // to check if file already exists
+    $.ajax({
+      url: $el.data('present-url'),
+      type: 'GET',
+      dataType: 'json',
+      success: function(data) {
+        if (data.processing === true) {
+          setTimeout(function() {
+            refreshAsset($el);
+          }, DELAY);
+        }
+
+        if (data.processing === false) {
+          $el.html(data.placeholder_html);
+          $el.attr('data-status', 'asset-loaded');
+          animateSpinner($el, false);
+          FilePreviewModal.init();
+        }
+      },
+      error: function() {
+        animateSpinner(null, false);
+      }
+    });
+
+    $el.data().refreshCounter += 1;
+  }
+
+  function setupAssetsLoading() {
     var elements = $("[data-status='asset-loading']");
 
     if (!elements.length) {
-      return false;
+      return;
     }
 
     $.each(elements, function(_, el) {
       var $el = $(el);
 
-      // Perform an AJAX call to present URL
-      // to check if file already exists
-      $.ajax({
-        url: $el.data('present-url'),
-        type: 'GET',
-        dataType: 'json',
-        success: function(data) {
-          if (data.processing === true) {
-            return;
-          }
+      if ($el.data('refreshIsRunning') === true) return;
 
-          if (data.processing === false) {
-            $el.html(data.placeholder_html);
-            $el.attr('data-status', 'asset-loaded');
-            animateSpinner(null, false);
-            FilePreviewModal.init();
-          }
-        },
-        error: function() {
-          animateSpinner(null, false);
-        }
-      });
+      $el.data('refreshCounter', 1);
+      $el.data('refreshIsRunning', true);
+
+      setTimeout(function() {
+        refreshAsset($el);
+      }, DELAY);
     });
-
-    return true;
-  }
-
-  function finalizeAssets() {
-    var elements = $("[data-status='asset-loading']");
-
-    $.each(elements, function(_, el) {
-      var $el = $(el);
-      $el.attr('data-status', 'asset-failed');
-      if ($el.data('filename')) {
-        $el.html($el.data('filename'));
-      }
-    });
-  }
-
-  function setupAssetsLoading() {
-    var DELAY = 5000;
-    var REPETITIONS = 60;
-    var cntr = 0;
-    var intervalId;
-
-    if (window.assetsPollingIsRunning) return;
-
-    intervalId = window.setInterval(function() {
-      cntr += 1;
-      if (cntr >= REPETITIONS || !refreshAssets()) {
-        finalizeAssets();
-        window.clearInterval(intervalId);
-        window.assetsPollingIsRunning = false;
-      }
-    }, DELAY);
-
-    window.assetsPollingIsRunning = true;
   }
 
   return Object.freeze({
