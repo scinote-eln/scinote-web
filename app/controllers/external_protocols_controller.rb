af7599a66c79a49f267c493ab70aad6db812cec5
Jure Grabnar <jgrabnar@biosistemika.com>
2019-06-13 01:02:44 +0200

    Add ExternalProtocols#create action and tests

--- app/controllers/external_protocols_controller.rb
+++ app/controllers/external_protocols_controller.rb
@@ -1,4 +1,7 @@
 class ExternalProtocolsController < ApplicationController
+  before_action :load_vars
+  before_action :check_import_permissions, only: [:create]
+
   # GET
   def index
     # list_protocols = SearchService.call(index_params)
@@ -29,7 +32,7 @@ class ExternalProtocolsController < ApplicationController
       protocol_source: new_params[:protocol_source],
       protocol_client_id: new_params[:protocol_client_id],
       user_id: current_user.id,
-      team_id: current_team.id
+      team_id: @team.id
     )
 
     if service_call.succeed?
@@ -39,10 +42,29 @@ class ExternalProtocolsController < ApplicationController
     end
   end
 
+  # POST import_external_protocol
   def create
+    service_call = ProtocolImporters::ImportProtocolService.call(
+      protocol_params: create_params[:protocol_params],
+      steps_params: create_params[:steps_paramas],
+      user_id: current_user.id,
+      team_id: @team.id
+    )
+
+    if service_call.succeed?
+      render json: service_call.protocol
+    else
+      render json: { errors: service_call.errors }, status: 400
+    end
   end
 
   private
+
+  def load_vars
+    @team = Team.find_by_id(params[:team_id])
+
+    render_404 unless @team
+  end
 
   def index_params
     params.permit(:protocol_source, :key, :page_id, :page_size, :order_field, :order_dir)
@@ -50,5 +72,13 @@ class ExternalProtocolsController < ApplicationController
 
   def new_params
     params.permit(:protocol_source, :protocol_client_id)
+  end
+
+  def create_params
+    params.permit(:protocol_params, :steps_params)
+  end
+
+  def check_import_permissions
+    render_403 unless can_create_protocols_in_repository?(@team)
   end
 end
