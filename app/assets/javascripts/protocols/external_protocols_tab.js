b3c3b68dc2afc3a043d7314b84ec26448aaf8517
Jure Grabnar <jgrabnar@biosistemika.com>
2019-06-19 22:54:20 +0200

    Add external preview to External Protocols tab
    Closes SCI-3563

---
+++ app/assets/javascripts/protocols/external_protocols_tab.js
@@ -0,0 +1,27 @@
+function applyClickCallbackOnProtocolCards() {
+  $('.protocol-card').off('click').on('click', function(e) {
+    $.ajax({
+      url: $(this).data('show-url'),
+      type: 'GET',
+      dataType: 'json',
+      data: {
+        protocol_source: $(this).data('protocol-source'),
+        protocol_id: $(this).data('show-protocol-id')
+      },
+      success: function (data) {
+        $('.empty-preview-panel').hide();
+        $('.full-preview-panel').show();
+        $('.preview-iframe').contents().find('body').html(data.html);
+      },
+      error: function (error) {
+        // TODO: we should probably show some alert bubble
+        $('.empty-preview-panel').show();
+        $('.full-preview-panel').hide();
+      }
+    });
+    e.preventDefault();
+    return false;
+  });
+}
+
+applyClickCallbackOnProtocolCards();
