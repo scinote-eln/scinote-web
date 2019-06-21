33cc3c29d18d9e6a960b283304af5ae1912e8d61
Jure Grabnar <jgrabnar@biosistemika.com>
2019-06-21 19:42:58 +0200

    Move PDF thumbnail generation to ImageMagick
    Closes SCI-3579

--- lib/paperclip_processors/custom_file_preview.rb
+++ lib/paperclip_processors/custom_file_preview.rb
@@ -10,16 +10,26 @@ module Paperclip
       dst = TempfileFactory.new.generate("#{basename}.#{options[:format]}")
 
       begin
-        Paperclip.run(
-          libreoffice_path,
-          "--headless --invisible --convert-to png --outdir #{directory} #{@file.path}"
-        )
+        if @file.content_type == 'application/pdf'
+          # We use special convert options for PDFs to improve quality and
+          # background, we append [0] to convert only the first page
+          convert(
+            ":source -resize '#{options[:geometry]}' -format #{options[:format]} -flatten -quality 70 :dest",
+            source: File.expand_path(@file.path) + '[0]',
+            dest: File.expand_path(dst.path)
+          )
+        else
+          Paperclip.run(
+            libreoffice_path,
+            "--headless --invisible --convert-to png --outdir #{directory} #{@file.path}"
+          )
 
-        convert(
-          ":source -resize '#{options[:geometry]}' -format #{options[:format]} #{options[:convert_options]} :dest",
-          source: File.expand_path(original_preview_file),
-          dest: File.expand_path(dst.path)
-        )
+          convert(
+            ":source -resize '#{options[:geometry]}' -format #{options[:format]} #{options[:convert_options]} :dest",
+            source: File.expand_path(original_preview_file),
+            dest: File.expand_path(dst.path)
+          )
+        end
       ensure
         File.delete(original_preview_file) if File.file?(original_preview_file)
       end
