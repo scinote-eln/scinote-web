e5defb95d4fce6b00d364a9925120179edbbb444
Jure Grabnar <jgrabnar@biosistemika.com>
2019-07-02 20:21:05 +0200

    Hound formatting issues

--- babel.config.js
+++ babel.config.js
@@ -74,5 +74,5 @@ module.exports = function(api) {
         }
       ]
     ].filter(Boolean)
-  }
-}
+  };
+};
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,5 @@
 const { environment } = require('@rails/webpacker')
 
-environment.loaders.delete('nodeModules')
+environment.loaders.delete('nodeModules');
 
 module.exports = environment
