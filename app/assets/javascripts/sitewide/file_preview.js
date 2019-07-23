cdad167be27ac0f4ea1684641eee07c6b4ebd9ea
Jure Grabnar <jgrabnar@biosistemika.com>
2019-07-23 15:02:33 +0200

    Add spinner when editing image annotation
    Close SCI-3710

--- app/assets/javascripts/sitewide/file_preview.js
+++ app/assets/javascripts/sitewide/file_preview.js
@@ -258,6 +258,7 @@ var FilePreviewModal = (function() {
       'colorpicker.title.color': '#fff'
     };
 
+    animateSpinner(null, true);
     imageEditor = new tui.ImageEditor('#tui-image-editor', {
       includeUI: {
         loadImage: {
@@ -284,6 +285,7 @@ var FilePreviewModal = (function() {
 
     imageEditor.on('image_loaded', () => {
       $('.file-save-link').css('display', '');
+      animateSpinner(null, false);
     });
 
     ps = new PerfectScrollbar($('.tui-image-editor-wrap')[0], { wheelSpeed: 0.5 });
