a602fb2d47b68d309e76669450ddcfc2f183fc0c
Mojca Lorber <mlorber@biosistemika.com>
2019-08-30 15:23:04 +0200

    change inlane editing css, add shared inventory subtitle and add tooltips to shared inventories icons

--- app/helpers/inventories_helper.rb
+++ app/helpers/inventories_helper.rb
@@ -4,16 +4,17 @@ module InventoriesHelper
   def inventory_shared_status_icon(inventory, team)
     if inventory.shared_with?(team)
       if can_manage_repository_rows?(inventory)
-        draw_custom_icon('shared-edit')
+        draw_custom_icon('shared-edit', inventory.team)
       else
-        draw_custom_icon('shared-read')
+        draw_custom_icon('shared-read', inventory.team)
       end
     else
       # The icon should be hiden if repo is not shared (we're updating it dinamically)
       css_classes = ["repository-share-status"]
       css_classes.push("hidden") unless inventory.i_shared?(current_team)
+      css_title = t('repositories.icon_title.i_shared')
 
-      content_tag :span, class: css_classes.join(" ") do
+      content_tag :span, title: css_title, class: css_classes.join(" ") do
         draw_custom_icon('i-shared')
       end
     end
