0cbc699c626815d21732a8953189649fcb751cab
aignatov-bio <aignatov@biosistemika.com>
2019-10-03 15:44:03 +0200

    Remove image icon from preview

--- app/assets/javascripts/sitewide/marvinjs_editor.js
+++ app/assets/javascripts/sitewide/marvinjs_editor.js
@@ -193,6 +193,7 @@ var MarvinJsEditorApi = (function() {
             .attr('src', json.url);
         } else {
           $('#modal_link' + json.id + ' img').attr('src', json.url);
+          $('#modal_link' + json.id + ' img').css('opacity', '0');
           $('#modal_link' + json.id + ' .attachment-label').html(json.file_name);
         }
         $(marvinJsModal).modal('hide');
