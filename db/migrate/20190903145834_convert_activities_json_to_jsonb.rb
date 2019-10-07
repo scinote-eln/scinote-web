1c45b0b209aa4d99fbbb13807f45b1195e828cb1
aignatov-bio <aignatov@biosistemika.com>
2019-10-07 13:54:38 +0200

    Fix migration

--- db/migrate/20190903145834_convert_activities_json_to_jsonb.rb
+++ db/migrate/20190903145834_convert_activities_json_to_jsonb.rb
@@ -4,7 +4,12 @@ class ConvertActivitiesJsonToJsonb < ActiveRecord::Migration[5.2]
   def change
     change_column :activities, :values, 'jsonb USING CAST(values AS jsonb)'
     ActiveRecord::Base.connection.execute("
-      UPDATE activities SET values = REGEXP_REPLACE(values::text, '^\"||\"$||\\\\', '', 'g')::jsonb
+      UPDATE activities SET
+      values = REGEXP_REPLACE(
+        REGEXP_REPLACE(
+          REGEXP_REPLACE(values::text, '\\\\\"', '\"', 'g'),
+          '^\"||\"$', '', 'g'),
+        '\\\\\\\\', '\\\\', 'g')::jsonb
     ")
   end
 end
