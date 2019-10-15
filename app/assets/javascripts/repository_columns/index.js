34f7a7ba4dca7c3a25cd91ad15fad17ff14d44c5
Mojca Lorber <mlorber@biosistemika.com>
2019-10-15 16:11:37 +0200

    Reafactor javascript repository column

--- app/assets/javascripts/repository_columns/index.js
+++ app/assets/javascripts/repository_columns/index.js
@@ -2,7 +2,7 @@
 /* no-restricted-globals */
 var RepositoryColumns = (function() {
   function initColumnTypeSelector() {
-    $('[name="repository_column[data_type]"]')
+    $('#repository_column_data_type')
       .on('click', function() {
         $('.column-type').hide();
         $('[data-column-type="' + $(this).val() + '"]').show();
@@ -68,43 +68,35 @@ var RepositoryColumns = (function() {
     // var colRadID = '#repository_column_data_type_repositorylistvalue';
     // var tagsInputID = '[data-role="tagsinput"]';
     // var formID = '[data-role="manage-repository-column-form"]';
-
-    $('[data-action="edit"]').off('click').on('click', function() {
+    $('.repository-columns-body').off('click', '.edit-repo-column').on('click', '.edit-repo-column', function() {
       var editUrl = $(this).closest('li').attr('data-edit-url');
       $.get(editUrl, function(data) {
-        $(data.html).appendTo('body').promise().done(function() {
-          $(modalID).modal('show').promise().done(function() {
-            $(modalID).on('hidden.bs.modal', function() {
-              // remove edit modal window
-              $(modalID).remove();
-              $('.modal-backdrop').remove();
-            });
+        $(data.html).appendTo('body');
+        $(modalID).modal('show');
 
-            initColumnTypeSelector();
-            $('#repository_column_data_type').val($(modalID).attr('data-edit-type')).trigger('click');
-            $('#repository_column_data_type').prop('disabled', true);
-            setTimeout(function() {
-              $('#repository_column_name').focus();
-            }, 500);
+        initColumnTypeSelector();
+        $('#repository_column_data_type').val($(modalID).attr('data-edit-type')).trigger('click');
+        $('#repository_column_data_type').prop('disabled', true);
+        setTimeout(function() {
+          $('#repository_column_name').focus();
+        }, 500);
 
-            // if ($(modalID).attr('data-edit-type') === 'RepositoryListValue') {
-            //   var values = JSON.parse($(tagsInputID).attr('data-value'));
-            //   $('#repository_column_data_type').val('RepositoryListValue');
-            //   $(colRadID).click().promise().done(function() {
-            //     $.each(tagsInputValues, function(index, element) {
-            //       $(tagsInputID).tagsinput('add', element);
-            //     });
-            //   });
-            // }
+        // if ($(modalID).attr('data-edit-type') === 'RepositoryListValue') {
+        //   var values = JSON.parse($(tagsInputID).attr('data-value'));
+        //   $('#repository_column_data_type').val('RepositoryListValue');
+        //   $(colRadID).click().promise().done(function() {
+        //     $.each(tagsInputValues, function(index, element) {
+        //       $(tagsInputID).tagsinput('add', element);
+        //     });
+        //   });
+        // }
 
-            $('[data-action="save"]').on('click', function() {
-              // if ($('#repository_column_data_type').val() === 'RepositoryListValue') {
-              //   $('#list_items').val($(tagsInputID).val());
-              // }
-              //
-              // processResponse($(formID), 'update', modalID);
-            });
-          });
+        $('#new-repo-column-submit').on('click', function() {
+          // if ($('#repository_column_data_type').val() === 'RepositoryListValue') {
+          //   $('#list_items').val($(tagsInputID).val());
+          // }
+          //
+          // processResponse($(formID), 'update', modalID);
         });
       }).fail(function() {
         HelperModule.flashAlertMsg(
@@ -122,7 +114,7 @@ var RepositoryColumns = (function() {
   }
 
   function initDeleteSubmitAction(modal, form) {
-    modal.find('[data-action="delete"]').on('click', function() {
+    modal.find('#delete-repo-column-submit').on('click', function() {
       form.submit();
       modal.modal('hide');
       animateSpinner();
@@ -138,16 +130,13 @@ var RepositoryColumns = (function() {
   }
 
   function initDeleteColumnModal() {
-    $('[data-action="destroy"]').off('click').on('click', function() {
+    $('.repository-columns-body').off('click', '.delete-repo-column').on('click', '.delete-repo-column', function() {
       var element = $(this);
       var modalHtml = $('#deleteRepositoryColumn');
       $.get(element.closest('li').attr('data-destroy-url'), function(data) {
-        modalHtml.find('.modal-body').html(data.html)
-          .promise()
-          .done(function() {
-            modalHtml.modal('show');
-            initDeleteSubmitAction(modalHtml, $(modalHtml.find('form')));
-          });
+        modalHtml.find('.modal-body').html(data.html);
+        modalHtml.modal('show');
+        initDeleteSubmitAction(modalHtml, $(modalHtml.find('form')));
       }).fail(function() {
         HelperModule.flashAlertMsg(
           I18n.t('libraries.repository_columns.no_permissions'), 'danger'
@@ -165,11 +154,11 @@ var RepositoryColumns = (function() {
                   </div>
                   <div class="col-xs-4">
                     <span class="controlls pull-right">
-                      <button class="btn btn-default" data-action="edit">
+                      <button class="btn btn-default edit-repo-column" data-action="edit">
                         <span class="fas fa-pencil-alt"></span>&nbsp;
                         <%= I18n.t "libraries.repository_columns.index.edit_column" %>
                       </button>&nbsp;
-                      <button class="btn btn-default delete-column" data-action="destroy">
+                      <button class="btn btn-default delete-repo-column" data-action="destroy">
                         <span class="fas fa-trash-alt"></span>&nbsp;
                         <%= I18n.t "libraries.repository_columns.index.delete_column" %>
                       </button>
@@ -179,52 +168,40 @@ var RepositoryColumns = (function() {
 
     // remove element if already persent
     $('[data-id="' + column.id + '"]').remove();
-    $(html).insertBefore('.repository-columns-body ul li:first')
-      .promise()
-      .done(function() {
-        initDeleteColumnModal();
-        initEditCoumnModal();
-      });
+    $(html).insertBefore('.repository-columns-body ul li:first');
     // remove 'no column' list item
     $('[data-attr="no-columns"]').remove();
   }
 
   function initNewColumnModal() {
     var modalID = '#manageRepositoryColumn';
-    $('[data-action="new-column-modal"]').off('click').on('click', function() {
+    $('.repository-columns-header').off('click', '#new-repo-column-modal').on('click', '#new-repo-column-modal', function() {
       var modalUrl = $(this).attr('data-modal-url');
       $.get(modalUrl, function(data) {
-        $(data.html).appendTo('body').promise().done(function() {
-          $(modalID).modal('show').promise().done(function() {
-            $(modalID).on('hidden.bs.modal', function() {
-              // remove create new modal window
-              $(modalID).remove();
-              $('.modal-backdrop').remove();
-            });
+        $(data.html).appendTo('body');
+        $(modalID).modal('show');
 
-            initColumnTypeSelector();
-            $('[data-column-type="RepositoryTextValue"]').show();
-            setTimeout(function() {
-              $('#repository_column_name').focus();
-            }, 500);
+        initColumnTypeSelector();
+        $('[data-column-type="RepositoryTextValue"]').show();
+        setTimeout(function() {
+          $('#repository_column_name').focus();
+        }, 500);
 
-            $('[data-action="save"]').on('click', function() {
-              var url = $('#repository_column_data_type').find(':selected').data('create-url');
-              $.ajax({
-                url: url,
-                data: { repository_column: { name: $('#repository_column_name').val() } },
-                type: 'POST',
-                success: function(data2) {
-                  insertNewListItem(data2);
-                  HelperModule.flashAlertMsg(data2.message, 'success');
-                  if (modalID) {
-                    $(modalID).modal('hide');
-                  }
-                },
-                error: function() {
-                }
-              });
-            });
+        $('#new-repo-column-submit').on('click', function() {
+          var url = $('#repository_column_data_type').find(':selected').data('create-url');
+          $.ajax({
+            url: url,
+            data: { repository_column: { name: $('#repository_column_name').val() } },
+            type: 'POST',
+            success: function(data2) {
+              insertNewListItem(data2);
+              HelperModule.flashAlertMsg(data2.message, 'success');
+              if (modalID) {
+                $(modalID).modal('hide');
+              }
+            },
+            error: function() {
+            }
           });
         });
       });
@@ -233,7 +210,7 @@ var RepositoryColumns = (function() {
 
   return {
     init: () => {
-      if ($('#manageRepositoryColumn').length > 0) {
+      if ($('.repository-columns-header').length > 0) {
         initEditCoumnModal();
         initDeleteColumnModal();
         initNewColumnModal();
