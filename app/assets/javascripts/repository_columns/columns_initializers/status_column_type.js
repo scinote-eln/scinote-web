6e87ee395c1cfbc93bf1fb37a5f54868d07e2923
Urban Rotnik <urban.rotnik@gmail.com>
2019-11-07 14:37:01 +0100

    Add simple emoji picker

--- app/assets/javascripts/repository_columns/columns_initializers/status_column_type.js
+++ app/assets/javascripts/repository_columns/columns_initializers/status_column_type.js
@@ -7,11 +7,18 @@ var RepositoryStatusColumnType = (function() {
     return `
     <div class="status-item-container loading">
       <div class="status-item-icon"></div>
-        <input placeholder=${I18n.t('libraries.manange_modal_column.name_placeholder')}
-               class="status-item-field"
-               type="text"/>
-        <span class="status-item-icon-trash fas fa-trash"></span>
-      </div>
+      <input placeholder=${I18n.t('libraries.manange_modal_column.name_placeholder')}
+             class="status-item-field"
+             type="text"/>
+      <span class="status-item-icon-trash fas fa-trash"></span>
+    </div>
+    <div class="emojis-picker">
+      <span data-emoji-code="128540">&#128540;</span>
+      <span data-emoji-code="128520">&#128520;</span>
+      <span data-emoji-code="128526">&#128526;</span>
+      <span data-emoji-code="128531">&#128531;</span>
+      <span data-emoji-code="128535">&#128535;</span>
+      <span data-emoji-code="128536">&#128536;</span>
     </div>`;
   }
 
@@ -19,6 +26,8 @@ var RepositoryStatusColumnType = (function() {
     var $manageModal = $(manageModal);
     var addStatusOptionBtn = '.add-status';
     var deleteStatusOptionBtn = '.status-item-icon-trash';
+    var icon = '.status-item-icon';
+    var emojis = '.emojis-picker>span';
 
     $manageModal.off('click', addStatusOptionBtn).on('click', addStatusOptionBtn, function() {
       var newStatusRow = $(statusTemplate()).insertBefore($(this));
@@ -32,14 +41,37 @@ var RepositoryStatusColumnType = (function() {
     });
 
     $manageModal.off('click', deleteStatusOptionBtn).on('click', deleteStatusOptionBtn, function() {
-      var statusRow = $(this).parent();
+      // if existing item is deleted, flag it as deleted
+      // if new item is deleted, remove it from HTML
+
+      var $statusRow = $(this).parent();
+      var $emojis = $statusRow.next('.emojis-picker');
+      var isNewRow = $statusRow.data('id') == null;
+
       setTimeout(function() {
-        statusRow.addClass('loading');
-        statusRow.css('height', '0px');
+        $statusRow.addClass('loading');
+        $statusRow.css('height', '0px');
       }, 0);
       setTimeout(function() {
-        statusRow.remove();
+        if (isNewRow) {
+          $statusRow.remove();
+        } else {
+          $statusRow.attr('data-removed', 'true');
+          $statusRow.removeClass('loading');
+        }
+        $emojis.remove();
       }, 300);
+    });
+
+    $manageModal.off('click', icon).on('click', icon, function() {
+      var $emojiPicker = $(this).parent().next('.emojis-picker');
+      $emojiPicker.show();
+    });
+
+    $manageModal.off('click', emojis).on('click', emojis, function() {
+      var $clickedEmoji = $(this);
+      $clickedEmoji.parent().hide();
+      $clickedEmoji.parent().prev().find('.status-item-icon').html($clickedEmoji.html());
     });
   }
 
