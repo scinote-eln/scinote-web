dfa6a1b57effd2e7c2b9244f4370d433951583a4
Urban Rotnik <urban.rotnik@gmail.com>
2019-11-28 15:56:34 +0100

    DateTime HTML and JS simplified

--- app/assets/javascripts/repository_columns/columns_initializers/time_column.js
+++ app/assets/javascripts/repository_columns/columns_initializers/time_column.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-unused-vars */
-var RepositoryDateTimeColumnType = (function() {
+var RepositoryTimeColumnType = (function() {
   return {
     init: () => {},
     checkValidation: () => {
       return true;
     },
     loadParams: () => {
-      var isRange = $('#datetime-range').is(':checked');
+      var isRange = $('#time-range').is(':checked');
       var columnType = $('#repository-column-data-type').val();
       if (isRange) {
         columnType = columnType.replace('Value', 'RangeValue');
