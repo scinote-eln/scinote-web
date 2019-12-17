19582fa3368dbc41096717b4ac992ec32921c14f
Samo Hafner <samo@scinote.net>
2019-12-17 11:36:19 +0100

    fix markup

--- features/step_definitions/invite_user_steps.rb
+++ features/step_definitions/invite_user_steps.rb
@@ -1,14 +1,15 @@
+# frozen_string_literal: true
 
-Given("Settings page of BioSistemika Process team of a Karli Novak user") do 
-  visit "/users/settings/teams/1"
+Given('Settings page of BioSistemika Process team of a Karli Novak user') do
+  visit '/users/settings/teams/1'
 end
 
-Then("I fill in {string} to Invite users to team BioSistemika Process input field") do |email|
+Then('I fill in {string} to Invite users to team BioSistemika Process input field') do |email|
   page.find('.bootstrap-tagsinput>input').set email
 end
 
-Then("I click on {string} in Invite dropdown") do |text1|
-  find("[data-role='invite-with-role-div'] a" ,text: text1).click
+Then('I click on {string} in Invite dropdown') do |text1|
+  find("[data-role='invite-with-role-div'] a", text: text1).click
 end
 
 Then('I should see {string} massage of {string} modal window') do |massage, modal|
@@ -16,6 +17,6 @@ Then('I should see {string} massage of {string} modal window') do |massage, moda
   expect(modal_object).to have_selector '.results-wrap', text: massage
 end
 
-Given("I click on Sign up button") do
+Given('I click on Sign up button') do
   find('.btn').click
 end
