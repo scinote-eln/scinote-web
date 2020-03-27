b9e81f208daff814a62c36028604029cad9a0e9e
aignatov-bio <aignatov@biosistemika.com>
2020-03-27 10:34:06 +0100

    Fix i18n

--- app/helpers/form_tag_helper.rb
+++ app/helpers/form_tag_helper.rb
@@ -1,12 +1,10 @@
-# frozen_string_literal: true
-
 module FormTagHelper
   def recaptcha_input_tag
     if Rails.configuration.x.enable_recaptcha
       res = "<div class='form-group sci-input-container"
       res << 'has-error' if flash[:recaptcha_error]
       res << "'>"
-      res << label_tag(:recaptcha_label, 'Let us know you’re human. Enter the captcha below.')
+      res << label_tag(:recaptcha_label, I18n.t('users.registrations.new.captcha_description'))
       res << recaptcha_tags
       if flash[:recaptcha_error]
         res << "<span class='help-block'>"
