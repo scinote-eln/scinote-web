0384a64034044a8a5f3ef8aaa5b265624405f528
Urban Rotnik <urban.rotnik@gmail.com>
2020-05-06 22:16:40 +0200

    Multi elements support

--- features/support/shared_methods.rb
+++ features/support/shared_methods.rb
@@ -8,47 +8,32 @@
 
 ### Improvments
 # Threads? When first is successful, kill others?
+# NOT WOKRING: first(".btn:nth(#{position-1})", text: text)
+# Wait time could be problem. Find&First is witing until first element appear, but all is waiting until max wait
+# time, what is 30s by default. This is why override with 1 second.
 
 def check_active_team(team)
   expect(page).to have_selector '#team-switch button', text: team
 end
 
 def sci_click_on_button(text:, position: 1)
-  # default wait 1 second for now...
   raise 'Position cannot be lower than 1' if position < 1
 
-  # Find method is faster, so try with find, if nothing found, go with all
-  # Use wait only for all, otherwise testsuit will wait on first Find if button is an form submit
   if position == 1
-    # begin
-      btn = first('.btn', text: text)
-    # rescue Capybara::ElementNotFound # We assume and hope button is an input, so try to find input
-    #   btn = first("input.btn[value=#{text}]", wait: wait)
-    # end
+    btn = first('.btn', text: text)
   else
-    raise "Not Implemented"
-
-    # buttons = all('.btn', text: text, wait: wait)
-
-    # unless buttons.any? # Maybe .btn is an input field, and does not have text, check for value
-    #   buttons = all("input.btn[value=#{text}]", wait: wait)
-    # end
-
-    # btn = buttons[position - 1]
-    btn = first(".btn:nth(#{position-1})", text: text)
+    btn = all('.btn', text: text, wait: 1)[position-1]
   end
-
   btn.click
 end
 
-def sci_click_on_icon(icon_class:, position: 1, wait: 1)
+def sci_click_on_icon(icon_class:, position: 1)
+  raise 'Position cannot be lower than 1' if position < 1
+
   if position == 1
-    icon = first(".fas.#{icon_class}", wait: wait)
-    # Fallback here in icon is not in a and span... ?
+    icon = first(".fas.#{icon_class}")
   else
-    raise "Not Implemented"
-    # icon = all("a>span.#{icon_class}:not(.hidden-lg)", wait: wait)[position-1]
-    icon = first(".fas.#{icon_class}:nth-child(#{position-1}n)", wait: wait)
+    icon = all(".fas.#{icon_class}")[position-1]
   end
 
   icon.click
