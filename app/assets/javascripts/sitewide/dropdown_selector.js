4fc37e12a0fb2c13efb9403e29be9714efb7f46f
Urban Rotnik <urban.rotnik@gmail.com>
2020-06-01 18:41:58 +0200

    Fix dropdown render for columns manage modal

--- app/assets/javascripts/sitewide/dropdown_selector.js
+++ app/assets/javascripts/sitewide/dropdown_selector.js
@@ -42,6 +42,7 @@
     closeOnSelect: boolean, // Close dropdown after select
     disableSearch: boolean, // Disable search
     emptyOptionAjax: boolean, // Add empty option for ajax request
+    labelHTML: bolean, // render as HTMLelement or text
   }
 
 
@@ -635,7 +636,15 @@ var dropdownSelector = (function() {
                   </div>
                   <i class="fas fa-times ${selector.data('config').singleSelect ? 'hidden' : ''}"></i>
                 </div>`).insertBefore(container.find('.input-field .search-field'));
-      tag.find('.tag-label').text(label);
+
+      console.log(selector.data('config').labelHTML);
+
+      if (selector.data('config').labelHTML) {
+        tag.find('.tag-label').html(label);
+      } else {
+        tag.find('.tag-label').text(label);
+      }
+
       // Now we need add delete action to tag
       tag.find('.fa-times').click(function(e) {
         var tagLabel = $(this).prev();
