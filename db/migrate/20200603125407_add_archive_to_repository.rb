d3806e53a5ff3b2f0d1b73292e50674a00346597
Mojca Lorber <mlorber@biosistemika.com>
2020-06-04 15:44:47 +0200

    Add archive to repository

---
+++ db/migrate/20200603125407_add_archive_to_repository.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class AddArchiveToRepository < ActiveRecord::Migration[6.0]
+  def change
+    change_table :repositories, bulk: true do |t|
+      t.boolean :archived, default: false, null: false
+      t.datetime :archived_on
+      t.references :archived_by, index: true, foreign_key: { to_table: :users }
+    end
+  end
+end
