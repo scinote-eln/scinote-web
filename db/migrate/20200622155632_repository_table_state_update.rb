6c7edbb9be693ff536af4ce2bf781624c20a4891
aignatov-bio <aignatov@biosistemika.com>
2020-06-26 14:32:55 +0200

    Fix markup

--- db/migrate/20200622155632_repository_table_state_update.rb
+++ db/migrate/20200622155632_repository_table_state_update.rb
@@ -7,37 +7,36 @@ class RepositoryTableStateUpdate < ActiveRecord::Migration[6.0]
 
   def up
     TempStateModel.find_each do |table_state|
-      begin
-        state = table_state.state
+      state = table_state.state
+      order_state = state.dig('order', 0, 0)
+
+      unless order_state
+        order = state.dig('order', '0')
+        state['order'] = [order]
         order_state = state.dig('order', 0, 0)
-
-        unless order_state
-          order = state.dig('order', '0')
-          state['order'] = [order]
-          order_state = state.dig('order', 0, 0)
-        end
-
-        state['order'][0][0] = order_state.to_i + 2 if order_state.to_i > 5
-
-        if state['columns'].class == Hash
-          columns_array = []
-          state['columns'].each{ |k, v| columns_array[k.to_i] = v}
-          state['columns'] = columns_array
-        end
-
-        2.times do
-          state['columns'].insert(6,
-                                  'search' => {
-                                    'regex' => false, 'smart' => true, 'search' => '', 'caseInsensitive' => true
-                                  },
-                                  'visible' => false)
-        end
-        state['ColReorder'] = state['ColReorder'].map { |i| i.to_i > 5 ? i.to_i + 2 : i.to_i } + [6, 7] if state['ColReorder']
-        table_state.update(state: state)
-      rescue
-        next
       end
+
+      state['order'][0][0] = order_state.to_i + 2 if order_state.to_i > 5
+
+      if state['columns'].class == Hash
+        columns_array = []
+        state['columns'].each { |k, v| columns_array[k.to_i] = v }
+        state['columns'] = columns_array
+      end
+
+      2.times do
+        state['columns'].insert(6,
+                                'search' => {
+                                  'regex' => false, 'smart' => true, 'search' => '', 'caseInsensitive' => true
+                                },
+                                'visible' => false)
+      end
+      if state['ColReorder']
+        state['ColReorder'] = state['ColReorder'].map { |i| i.to_i > 5 ? i.to_i + 2 : i.to_i } + [6, 7]
+      end
+      table_state.update(state: state)
+    rescue StandardError
+      next
     end
   end
 end
-
