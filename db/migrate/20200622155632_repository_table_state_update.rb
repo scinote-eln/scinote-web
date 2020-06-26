2da0a6c6bf33cb9f2db6bdaf2414965ffc90b311
aignatov-bio <aignatov@biosistemika.com>
2020-06-26 14:31:10 +0200

    Improve repository state migration

--- db/migrate/20200622155632_repository_table_state_update.rb
+++ db/migrate/20200622155632_repository_table_state_update.rb
@@ -7,18 +7,36 @@ class RepositoryTableStateUpdate < ActiveRecord::Migration[6.0]
 
   def up
     TempStateModel.find_each do |table_state|
-      state = table_state.state
-      order_state = state['order'][0][0]
-      state['order'][0][0] = order_state + 2 if order_state > 5
-      2.times do
-        state['columns'].insert(6,
-                                'search' => {
-                                  'regex' => false, 'smart' => true, 'search' => '', 'caseInsensitive' => true
-                                },
-                                'visible' => false)
+      begin
+        state = table_state.state
+        order_state = state.dig('order', 0, 0)
+
+        unless order_state
+          order = state.dig('order', '0')
+          state['order'] = [order]
+          order_state = state.dig('order', 0, 0)
+        end
+
+        state['order'][0][0] = order_state.to_i + 2 if order_state.to_i > 5
+
+        if state['columns'].class == Hash
+          columns_array = []
+          state['columns'].each{ |k, v| columns_array[k.to_i] = v}
+          state['columns'] = columns_array
+        end
+
+        2.times do
+          state['columns'].insert(6,
+                                  'search' => {
+                                    'regex' => false, 'smart' => true, 'search' => '', 'caseInsensitive' => true
+                                  },
+                                  'visible' => false)
+        end
+        state['ColReorder'] = state['ColReorder'].map { |i| i.to_i > 5 ? i.to_i + 2 : i.to_i } + [6, 7] if state['ColReorder']
+        table_state.update(state: state)
+      rescue
+        next
       end
-      state['ColReorder'] = state['ColReorder'].map { |i| i > 5 ? i + 2 : i } + [6, 7]
-      table_state.update(state: state)
     end
   end
 end
