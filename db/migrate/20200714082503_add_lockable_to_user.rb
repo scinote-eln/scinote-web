d8794972d5cc639f373af226e1e6b605bc7fdecf
Mojca Lorber <mlorber@biosistemika.com>
2020-07-17 16:35:03 +0200

    Change migration

--- db/migrate/20200714082503_add_lockable_to_user.rb
+++ db/migrate/20200714082503_add_lockable_to_user.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-class AddLockableToDevise < ActiveRecord::Migration[6.0]
+class AddLockableToUser < ActiveRecord::Migration[6.0]
   def change
     change_table :users, bulk: true do |t|
       t.integer :failed_attempts, default: 0, null: false
       t.datetime :locked_at
-      t.string :unlock_token, index: true, foreign_key: { to_table: :users }, unique: true
+      t.string :unlock_token, index: { unique: true }
     end
   end
 end
