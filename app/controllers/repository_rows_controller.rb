ae013b6fa8c5ede01d3a52215c5060b91decb75f
Miha Mencin <miha.mencin@gmail.com>
2020-08-06 14:06:23 +0200

    join two renders

--- app/controllers/repository_rows_controller.rb
+++ app/controllers/repository_rows_controller.rb
@@ -52,8 +52,7 @@ class RepositoryRowsController < ApplicationController
 
   def show
     @repository_row = RepositoryRow.find_by(id: params[:id])
-    render_403 unless can_read_repository?(@repository_row.repository)
-    render_403 unless @repository_row.repository_id == params[:repository_id]
+    render_403 if !can_read_repository?(@repository_row.repository) || @repository_row.repository_id != params[:repository_id]
     
     @assigned_modules = @repository_row.my_modules.joins(experiment: :project)
     @viewable_modules = @assigned_modules.viewable_by_user(current_user, current_user.teams)
