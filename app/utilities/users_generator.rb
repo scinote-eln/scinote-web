657da8e3c32fa68237fbdbbd7fae96d2d660a726
Urban Rotnik <urban.rotnik@gmail.com>
2020-08-12 14:05:22 +0200

    Fix password generator

--- app/utilities/users_generator.rb
+++ app/utilities/users_generator.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module UsersGenerator
   # Simply validate the user with the given data,
   # and return an array of errors (which is 0-length
@@ -31,20 +33,18 @@ module UsersGenerator
                     password: password,
                     password_confirmation: password }.merge(options))
 
-    nu.confirmed_at = Time.now if confirmed
+    nu.confirmed_at = Time.zone.now if confirmed
     nu.save!
 
     # TODO: If user is not confirmed, maybe additional email
     # needs to be sent with his/her password & email?
 
     # Create user's own team of needed
-    if private_team_name.present?
-      create_private_user_team(nu, private_team_name)
-    end
+    create_private_user_team(nu, private_team_name) if private_team_name.present?
 
     # Assign user to additional teams
     team_ids.each do |team_id|
-      team = Team.find_by_id(team_id)
+      team = Team.find_by(id: team_id)
       UserTeam.create(user: nu, team: team, role: :admin) if team.present?
     end
 
@@ -74,7 +74,28 @@ module UsersGenerator
 
   def generate_user_password
     require 'securerandom'
-    SecureRandom.hex(5)
+    password = SecureRandom.hex(5)
+
+    password += Devise.password_complexity.inject('') do |second_part_pass, elem|
+      n = elem[1]
+      random_string = case elem[0]
+                      when :digit
+                        rand.to_s[2..2 + n]
+                      when :lower
+                        (0...n).map { ('a'..'z').to_a[rand(26)] }.join
+                      when :upper
+                        (0...n).map { ('A'..'Z').to_a[rand(26)] }.join
+                      when :symbol
+                        (0...n).map do
+                          ((32..47).to_a + (58..64).to_a + (91..96).to_a + (123..126).to_a)
+                            .pack('U*').chars.to_a[rand(33)]
+                        end.join
+                      end
+
+      second_part_pass += random_string
+      second_part_pass
+    end
+    password
   end
 
   def get_user_initials(full_name)
