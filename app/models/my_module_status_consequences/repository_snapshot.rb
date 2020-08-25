4a837951dbbefc0b5fa0be8d19eee5fe7b541a29
aignatov-bio <aignatov@biosistemika.com>
2020-08-25 15:02:25 +0200

    Update variable name

--- app/models/my_module_status_consequences/repository_snapshot.rb
+++ app/models/my_module_status_consequences/repository_snapshot.rb
@@ -3,8 +3,8 @@
 module MyModuleStatusConsequences
   class RepositorySnapshot < MyModuleStatusConsequence
     def call(my_module)
-      my_module.assigned_repositories.each do |repo|
-        repo.provision_snapshot(my_module)
+      my_module.assigned_repositories.each do |repository|
+        repository.provision_snapshot(my_module)
       end
     end
   end
