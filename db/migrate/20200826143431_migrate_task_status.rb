6229ba6041883a3a645e52b164773d0d1374ad82
aignatov-bio <aignatov@biosistemika.com>
2020-08-27 12:13:13 +0200

    Add morkflow generator

--- db/migrate/20200826143431_migrate_task_status.rb
+++ db/migrate/20200826143431_migrate_task_status.rb
@@ -2,10 +2,19 @@
 
 class MigrateTaskStatus < ActiveRecord::Migration[6.0]
   def up
-    in_progress_status = execute("SELECT id FROM my_module_statuses WHERE name = 'In progress' LIMIT 1")[0]&['id']
-    completed_status = execute("SELECT id FROM my_module_statuses WHERE name = 'Completed' LIMIT 1")[0]&['id']
+    MyModuleStatusFlow.ensure_default
 
-    execute "UPDATE my_modules SET my_module_status_id = #{in_progress_status} WHERE state = 0"
-    execute "UPDATE my_modules SET my_module_status_id = #{completed_status} WHERE state = 1"
+    in_progress_status = execute("SELECT id FROM my_module_statuses WHERE name = 'In progress' LIMIT 1").to_a[0]
+    completed_status = execute("SELECT id FROM my_module_statuses WHERE name = 'Completed' LIMIT 1").to_a[0]
+
+    if in_progress_status
+      execute("UPDATE my_modules
+               SET my_module_status_id = #{in_progress_status['id']} WHERE state = 0")
+    end
+
+    if completed_status
+      execute("UPDATE my_modules
+               SET my_module_status_id = #{completed_status['id']} WHERE state = 1")
+    end
   end
 end
