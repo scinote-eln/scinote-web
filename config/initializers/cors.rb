7668e1641942b75bf1e33ad126a7bd386c730a09
Oleksii Kriuchykhin <okriuchykhin@biosistemika.com>
2020-09-04 11:47:45 +0200

    Enable CORS for PWA [SCI-4979]

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,22 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'example.com'
-#
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+if ENV['SCINOTE_PWA_DOMAIN_NAME'].present?
+  Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins ENV['SCINOTE_PWA_DOMAIN_NAME']
+
+      resource '/oauth/token',
+               headers: :any,
+               methods: %i(post)
+
+      resource '/rails/active_storage/*',
+               headers: :any,
+               methods: %i(get post options head)
+
+      resource '/api/*',
+               headers: :any,
+               methods: %i(get post put patch delete options head)
+    end
+  end
+end
