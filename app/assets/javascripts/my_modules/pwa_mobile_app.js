6d7b55301f28824d4277ef97db3066961a86d45a
Mojca Lorber <mlorber@biosistemika.com>
2020-10-21 14:56:20 +0200

    Fix pwa url and change when banner is displayed

--- app/assets/javascripts/my_modules/pwa_mobile_app.js
+++ app/assets/javascripts/my_modules/pwa_mobile_app.js
@@ -2,4 +2,9 @@
   $('.open-mobile-app-container').on('click', '.dismiss-mobile-app-container', function() {
     $('.open-mobile-app-container').hide();
   });
+
+  // Show banner only on mobile devices
+  if ('ontouchstart' in window) {
+    $('.open-mobile-app-container').show();
+  }
 }());
--- app/helpers/pwa_helper.rb
+++ app/helpers/pwa_helper.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 module PwaHelper
-  def pwa_mobile_app_url(team_id, project_id, experiment_id, task_id, protocol_id, step_id)
-    url = Constants::PWA_URL
+  def pwa_mobile_app_url(team_id, project_id, experiment_id, task_id, protocol_id, step_id, domain)
+    url = Constants::PWA_URL.dup
     {
       ':pwa_domain' => Rails.configuration.x.pwa_domain,
       ':team_id' => team_id,
@@ -10,7 +10,8 @@ module PwaHelper
       ':experiment_id' => experiment_id,
       ':task_id' => task_id,
       ':protocol_id' => protocol_id,
-      ':step_id' => step_id
+      ':step_id' => step_id,
+      ':domain' => domain
     }.each { |k, v| url.gsub!(k, v.to_s) }
     url
   end
