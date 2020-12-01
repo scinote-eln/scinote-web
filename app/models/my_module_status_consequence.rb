d1af011c0c5cab69f75c50384a047a36602a0251
aignatov-bio <aignatov@biosistemika.com>
2020-12-01 15:10:28 +0100

    [SCI-5260] Fix markup

--- app/models/my_module_status_consequence.rb
+++ app/models/my_module_status_consequence.rb
@@ -3,11 +3,9 @@
 class MyModuleStatusConsequence < ApplicationRecord
   belongs_to :my_module_status
 
-  def forward(my_module)
-  end
+  def forward(my_module); end
 
-  def backward(my_module)
-  end
+  def backward(my_module); end
 
   def runs_in_background?
     false
