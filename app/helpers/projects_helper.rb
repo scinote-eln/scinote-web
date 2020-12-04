2d139416f18c605c0cdb5df859b45c5846b43b2a
aignatov-bio <aignatov@biosistemika.com>
2020-12-04 10:46:21 +0100

    Add backend for porjects sidebar [SCI-5178]

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,14 +1,40 @@
-module ProjectsHelper
+# frozen_string_literal: true
 
+module ProjectsHelper
   def user_project_role_to_s(user_project)
-    t("user_projects.enums.role." + user_project.role)
+    t('user_projects.enums.role.' + user_project.role)
   end
 
   def construct_module_connections(my_module)
-    conns = Array.new
+    conns = []
     my_module.outputs.each do |output|
       conns.push(output.to.id)
     end
     conns.to_s[1..-2]
+  end
+
+  def sidebar_folders_tree(team)
+    records = team.projects + ProjectFolder.inner_folders(team)
+    folders_recursive_builder(nil, records)
+  end
+
+  private
+
+  def folders_recursive_builder(folder, records)
+    children = records.select do |i|
+      (defined?(i.parent_folder_id) && i.parent_folder_id == folder&.id) ||
+        (defined?(i.project_folder_id) && i.project_folder_id == folder&.id)
+    end
+
+    children.map do |i|
+      if i.class == Project
+        { project: i }
+      else
+        {
+          folder: i,
+          children: folders_recursive_builder(i, records)
+        }
+      end
+    end
   end
 end
