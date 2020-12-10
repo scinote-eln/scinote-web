2c09b7672aa731d4927dd4178381cb7bfeca27a3
Jacob Specht <jacob@gqc.com>
2020-12-10 08:30:22 -0500

    Add missing gemfile, and missing route.

--- Gemfile
+++ Gemfile
@@ -74,6 +74,7 @@ gem 'nested_form_fields'
 gem 'nokogiri', '~> 1.10.8' # HTML/XML parser
 gem 'rails_autolink', '~> 1.1', '>= 1.1.6'
 gem 'rgl' # Graph framework for project diagram calculations
+gem 'spreadsheet', '~> 1.1.4' # @@@20190719JS Spreadsheet required before roo?
 gem 'roo', '~> 2.8.2' # Spreadsheet parser
 gem 'rotp'
 gem 'rqrcode' # QR code generator
--- config/routes.rb
+++ config/routes.rb
@@ -639,6 +639,9 @@ Rails.application.routes.draw do
     devise_scope :user do
       get 'avatar/:id/:style' => 'users/registrations#avatar', as: 'avatar'
       get 'users/auth_token_sign_in' => 'users/sessions#auth_token_create'
+      # @@@20190520JS - Sessions controller doesn't require pre-authentication,
+      #                 so we will do the factsheet download here:
+      get 'users/download_factsheet' => 'users/sessions#download_factsheet', as: 'factsheet'
       get 'users/sign_up_provider' => 'users/registrations#new_with_provider'
       get 'users/two_factor_recovery' => 'users/sessions#two_factor_recovery'
       post 'users/authenticate_with_two_factor' => 'users/sessions#authenticate_with_two_factor'
