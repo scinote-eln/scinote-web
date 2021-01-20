33b68b8aa7ee6e8366a539a40bfb2609d457b023
Urban Rotnik <urban.rotnik@gmail.com>
2021-01-20 09:00:45 +0100

    Fix permissions for result deletion

--- app/permissions/result.rb
+++ app/permissions/result.rb
@@ -8,4 +8,8 @@ Canaid::Permissions.register_for(Result) do
   can :manage_result do |user, result|
     can_manage_module?(user, result.my_module) && result.active? && result.unlocked?(result)
   end
+
+  can :delete_result do |user, result|
+    can_manage_module?(user, result.my_module) && result.archived? && result.unlocked?(result)
+  end
 end
