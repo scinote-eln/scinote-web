986013e84c037f03c4fb0e5eaabee30fb2f8d922
aignatov-bio <aignatov@biosistemika.com>
2021-01-26 15:47:01 +0100

    Add filters to experiments pages [SCI-5366]

--- app/assets/javascripts/projects/show.js
+++ app/assets/javascripts/projects/show.js
@@ -1,4 +1,61 @@
+/* global filterDropdown */
 (function() {
+  let experimentsCurrentSort;
+  let experimentsViewSearch;
+  let startedOnFromFilter;
+  let startedOnToFilter;
+  let modifiedOnFromFilter;
+  let modifiedOnToFilter;
+  let archivedOnFromFilter;
+  let archivedOnToFilter;
+
+  function initExperimentsFilters() {
+    var $filterDropdown = filterDropdown.init();
+
+    let $experimentsFilter = $('.experiments-filters');
+    let $startedOnFromFilter = $('.started-on-filter .from-date', $experimentsFilter);
+    let $startedOnToFilter = $('.started-on-filter .to-date', $experimentsFilter);
+    let $modifiedOnFromFilter = $('.modified-on-filter .from-date', $experimentsFilter);
+    let $modifiedOnToFilter = $('.modified-on-filter .to-date', $experimentsFilter);
+    let $archivedOnFromFilter = $('.archived-on-filter .from-date', $experimentsFilter);
+    let $archivedOnToFilter = $('.archived-on-filter .to-date', $experimentsFilter);
+    let $textFilter = $('#textSearchFilterInput', $experimentsFilter);
+
+    function appliedFiltersMark() {
+      let filtersEnabled = experimentsViewSearch
+        || startedOnFromFilter
+        || startedOnToFilter
+        || modifiedOnFromFilter
+        || modifiedOnToFilter
+        || archivedOnFromFilter
+        || archivedOnToFilter;
+      filterDropdown.toggleFilterMark($filterDropdown, filtersEnabled);
+    }
+
+    $filterDropdown.on('filter:apply', function() {
+      startedOnFromFilter = $startedOnFromFilter.val();
+      startedOnToFilter = $startedOnToFilter.val();
+      modifiedOnFromFilter = $modifiedOnFromFilter.val();
+      modifiedOnToFilter = $modifiedOnToFilter.val();
+      archivedOnFromFilter = $archivedOnFromFilter.val();
+      archivedOnToFilter = $archivedOnToFilter.val();
+      experimentsViewSearch = $textFilter.val();
+      appliedFiltersMark();
+      //refreshCurrentView();
+    });
+
+    // Clear filters
+    $filterDropdown.on('filter:clear', function() {
+      if ($startedOnFromFilter.data('DateTimePicker')) $startedOnFromFilter.data('DateTimePicker').clear();
+      if ($startedOnToFilter.data('DateTimePicker')) $startedOnToFilter.data('DateTimePicker').clear();
+      if ($modifiedOnFromFilter.data('DateTimePicker')) $modifiedOnFromFilter.data('DateTimePicker').clear();
+      if ($modifiedOnToFilter.data('DateTimePicker')) $modifiedOnToFilter.data('DateTimePicker').clear();
+      if ($archivedOnFromFilter.data('DateTimePicker')) $archivedOnFromFilter.data('DateTimePicker').clear();
+      if ($archivedOnToFilter.data('DateTimePicker')) $archivedOnToFilter.data('DateTimePicker').clear();
+      $textFilter.val('');
+    });
+  }
+
   function init() {
     $('.workflowimg-container').each(function() {
       let container = $(this);
@@ -18,6 +75,8 @@
         });
       }
     });
+
+    initExperimentsFilters();
   }
 
   init();
--- app/assets/javascripts/sitewide/filter_dropdown.js
+++ app/assets/javascripts/sitewide/filter_dropdown.js
@@ -1,3 +1,116 @@
-var FilterDropdown = (function() {
+var filterDropdown = (function() {
+  var $filterContainer = '';
 
-}())
+  function initClearButton() {
+    $('.clear-button', $filterContainer).click(function(e) {
+      e.stopPropagation();
+      e.preventDefault();
+      $filterContainer.trigger('filter:clear');
+    });
+  }
+
+  function preventDropdownClose() {
+    $('.dropdown-menu', $filterContainer).click((e) => {
+      if (!$(e.target).is('input,a')) {
+        e.stopPropagation();
+        e.preventDefault();
+        $('#textSearchFilterHistory').hide();
+        $('#textSearchFilterInput', $filterContainer).closest('.dropdown').removeClass('open');
+        $filterContainer.trigger('filter:clickBody');
+      }
+    });
+  }
+
+  function initSearchField() {
+    var $textFilter = $('#textSearchFilterInput', $filterContainer);
+
+    $filterContainer.on('show.bs.dropdown', function() {
+      let $filterDropdown = $filterContainer.find('.dropdown-menu');
+      let teamId = $filterDropdown.data('team-id');
+      $('#textSearchFilterHistory').find('li').remove();
+      try {
+        let storagePath = `${$filterDropdown.data('search-field-history-key')}/${teamId}/recent_search_keywords`;
+        let recentSearchKeywords = JSON.parse(localStorage.getItem(storagePath));
+        $.each(recentSearchKeywords, function(i, keyword) {
+          $('#textSearchFilterHistory').append($(
+            `<li class="dropdown-item">
+              <a class="projects-search-keyword" href="#" data-keyword="${keyword}">
+                <i class="fas fa-history"></i>
+                <span class="keyword-text">${keyword}</span>
+              </a>
+            </li>`
+          ));
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    }).on('hide.bs.dropdown', function() {
+      $('#textSearchFilterHistory').hide();
+      $('.apply-filters', $filterContainer).click();
+    });
+
+    $textFilter.click(function(e) {
+      e.stopPropagation();
+      $('#textSearchFilterHistory').toggle();
+      $(this).closest('.dropdown').toggleClass('open');
+    }).on('input', () => {
+      $('#textSearchFilterHistory').hide();
+      $(this).closest('.dropdown').removeClass('open');
+    });
+
+    $filterContainer.on('click', '.projects-search-keyword', function(e) {
+      e.stopPropagation();
+      e.preventDefault();
+      $textFilter.val($(this).data('keyword'));
+      $('#textSearchFilterHistory').hide();
+      $textFilter.closest('.dropdown').removeClass('open');
+    });
+  }
+
+  function initApplyButton() {
+    $('.apply-filters', $filterContainer).click(function(e) {
+      e.stopPropagation();
+      e.preventDefault();
+
+      let $filterDropdown = $filterContainer.find('.dropdown-menu');
+      let teamId = $filterDropdown.data('team-id');
+      let projectsViewSearch = $('#textSearchFilterInput').val();
+      try {
+        let storagePath = `${$filterDropdown.data('search-field-history-key')}/${teamId}/recent_search_keywords`;
+        let recentSearchKeywords = JSON.parse(localStorage.getItem(storagePath));
+        if (!Array.isArray(recentSearchKeywords)) recentSearchKeywords = [];
+        if (recentSearchKeywords.indexOf(projectsViewSearch) !== -1) {
+          recentSearchKeywords.splice(recentSearchKeywords.indexOf(projectsViewSearch), 1);
+        }
+        if (recentSearchKeywords.length > 4) {
+          recentSearchKeywords = recentSearchKeywords.slice(0, 4);
+        }
+        recentSearchKeywords.unshift(projectsViewSearch);
+        localStorage.setItem(storagePath, JSON.stringify(recentSearchKeywords));
+      } catch (error) {
+        console.error(error);
+      }
+      $filterContainer.trigger('filter:apply');
+      $(this).closest('.dropdown').removeClass('open');
+    });
+  }
+
+  return {
+    init: function() {
+      $filterContainer = $('.filter-container');
+      initClearButton();
+      preventDropdownClose();
+      initApplyButton();
+      initSearchField();
+      return $filterContainer;
+    },
+    toggleFilterMark: function(filterContainer, filtersEnabled) {
+      if (filtersEnabled) {
+        filterContainer.addClass('filters-applied');
+      } else {
+        filterContainer.removeClass('filters-applied');
+      }
+    }
+
+  };
+}());
