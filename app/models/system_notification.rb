2b5709e75fecb228a84ed6e710259afd4489fb13
Oleksii Kriuchykhin <okriuchykhin@biosistemika.com>
2021-03-02 17:26:44 +0100

    Improve behavior of the scheduler, add caching to system notifications sync service [SCI-5526]

--- app/models/system_notification.rb
+++ app/models/system_notification.rb
@@ -13,7 +13,7 @@ class SystemNotification < ApplicationRecord
                                            }
   #  ignoring: :accents
 
-  has_many :user_system_notifications
+  has_many :user_system_notifications, dependent: :destroy
   has_many :users, through: :user_system_notifications
 
   validates :title, :modal_title, :modal_body, :description, :source_created_at, :source_id, :last_time_changed_at,
