5c63ee55e7016a2a8594e077aa0bfc8bdb722af6
Oleksii Kriuchykhin <okriuchykhin@biosistemika.com>
2021-04-22 13:59:04 +0200

    Comment out wicked_pdf initializer patch

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -10,16 +10,16 @@ end
 # WickedPdfHelper patch that fixes issue with including application.css
 # in environments like Heroku where assets.compile option is disabled and
 # it is not acceptable to enable it.
-if Rails.env.production? && Rails.configuration.assets.compile == false
-  WickedPdf::WickedPdfHelper::Assets.module_eval do
-    def read_asset(source)
-      manifest = Rails.application.assets_manifest
-      path = File.join(manifest.dir, manifest.assets[source])
-      File.read(path)
-    end
-
-    def asset_exists?(source)
-      Rails.application.assets_manifest.assets.key?(source)
-    end
-  end
-end
+# if Rails.env.production? && Rails.configuration.assets.compile == false
+#   WickedPdf::WickedPdfHelper::Assets.module_eval do
+#     def read_asset(source)
+#       manifest = Rails.application.assets_manifest
+#       path = File.join(manifest.dir, manifest.assets[source])
+#       File.read(path)
+#     end
+#
+#     def asset_exists?(source)
+#       Rails.application.assets_manifest.assets.key?(source)
+#     end
+#   end
+# end
