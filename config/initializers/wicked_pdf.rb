8a915c8acf567a3d611a044c152030fefa35b8f9
Oleksii Kriuchykhin <okriuchykhin@biosistemika.com>
2021-04-28 14:37:16 +0200

    Fix assets inclusion into report [SCI-5680]

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -6,20 +6,3 @@ ENV['PATH'].split(':').each do |path|
   exe_path = File.join(path, 'wkhtmltopdf')
   WickedPdf.config[:exe_path] = File.join(path, 'wkhtmltopdf') if File.file?(exe_path)
 end
-
-# WickedPdfHelper patch that fixes issue with including application.css
-# in environments like Heroku where assets.compile option is disabled and
-# it is not acceptable to enable it.
-# if Rails.env.production? && Rails.configuration.assets.compile == false
-#   WickedPdf::WickedPdfHelper::Assets.module_eval do
-#     def read_asset(source)
-#       manifest = Rails.application.assets_manifest
-#       path = File.join(manifest.dir, manifest.assets[source])
-#       File.read(path)
-#     end
-#
-#     def asset_exists?(source)
-#       Rails.application.assets_manifest.assets.key?(source)
-#     end
-#   end
-# end
