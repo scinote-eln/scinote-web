931c587af766cf9a355ffacf8dcfda062e9a0130
aignatov-bio <aignatov@biosistemika.com>
2021-05-03 13:44:27 +0200

    Add view mode for checkbox template component [SCI-5675]

--- app/components/reports/multi_checkbox_input_component.rb
+++ app/components/reports/multi_checkbox_input_component.rb
@@ -2,7 +2,7 @@
 
 module Reports
   class MultiCheckboxInputComponent < TemplateValueComponent
-    def initialize(report:, name:, label:, placeholder: nil, editing: false, items: {})
+    def initialize(report:, name:, label:, placeholder: nil, editing: true, items: {})
       super(report: report, name: name, label: label, placeholder: placeholder, editing: editing)
       @items = items
     end
