a9d6c4bb94fce527778cf68e41951a345e106242
Martin Artnik <martin@scinote.net>
2021-06-18 14:19:35 +0200

    Code style fixes [SCI-5828]

--- db/migrate/20210618104853_add_code_index_to_experiments.rb
+++ db/migrate/20210618104853_add_code_index_to_experiments.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class AddCodeIndexToExperiments < ActiveRecord::Migration[6.1]
   def change
-    add_index :experiments, "('EX' || id)", name: "index_experiments_on_experiment_code", unique: true
+    add_index :experiments, "('EX' || id)", name: 'index_experiments_on_experiment_code', unique: true
   end
 end
