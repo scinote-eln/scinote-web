cd14bf45b19b2f03cc8f2a7bd9f8a217e6667b7c
aignatov-bio <aignatov@biosistemika.com>
2021-07-19 12:53:41 +0200

    Add barcodes to repository item card [SCI-5883][SCI-5884]

--- app/assets/javascripts/sitewide/repository_row_info_modal.js
+++ app/assets/javascripts/sitewide/repository_row_info_modal.js
@@ -1,3 +1,5 @@
+/* global dropdownSelector */
+
 (function() {
   'use strict';
 
@@ -16,6 +18,15 @@
         $(this).find('.modal-body #repository_row-info-table').DataTable().destroy();
         $(this).remove();
       });
+
+      let barCodeCanvas = bwipjs.toCanvas('bar-code-canvas', {
+        bcid:        'qrcode',
+        text:        $('#modal-info-repository-row #bar-code-canvas').data('id').toString(),
+        scale:       3
+      });
+      $('#modal-info-repository-row #bar-code-image').attr('src', barCodeCanvas.toDataURL('image/png'))
+
+
       $('#repository_row-info-table').DataTable({
         dom: 'RBltpi',
         stateSave: false,
@@ -39,5 +50,28 @@
     });
     e.preventDefault();
     return false;
+  });
+
+  $(document).on('click', '.print-label-button', function() {
+    $.ajax({
+      method: 'GET',
+      url: $(this).data('url'),
+      dataType: 'json'
+    }).done(function(xhr, settings, data) {
+      $('body').append($.parseHTML(data.responseJSON.html));
+      $('#modal-print-repository-row-label').modal('show', {
+        backdrop: true,
+        keyboard: false
+      }).on('hidden.bs.modal', function() {
+        $(this).remove();
+      });
+
+      dropdownSelector.init('#modal-print-repository-row-label #printers', {
+        noEmptyOption: true,
+        singleSelect: true,
+        closeOnSelect: true,
+        selectAppearance: 'simple'
+      });
+    });
   });
 })();
