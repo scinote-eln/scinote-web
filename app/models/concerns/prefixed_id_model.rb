19977c6d64e882077f52136d479945e208068fc3
Martin Artnik <martin@scinote.net>
2021-07-19 14:18:20 +0200

    Fix index warning for id prefixed models issue setting up project [SCI-5909]

--- app/models/concerns/prefixed_id_model.rb
+++ app/models/concerns/prefixed_id_model.rb
@@ -4,16 +4,19 @@ module PrefixedIdModel
   extend ActiveSupport::Concern
 
   included do
-    indexdef = "CREATE INDEX index_#{table_name}_on_#{name.underscore}_code"\
-    " ON public.#{table_name} USING gin ((('#{self::ID_PREFIX}'::text || id)) gin_trgm_ops)"
+    begin
+      indexdef = "CREATE INDEX index_#{table_name}_on_#{name.underscore}_code"\
+      " ON public.#{table_name} USING gin ((('#{self::ID_PREFIX}'::text || id)) gin_trgm_ops)"
 
-    index_exists = ActiveRecord::Base.connection.execute(
-      "SELECT indexdef FROM pg_indexes WHERE tablename NOT LIKE 'pg%';"
-    ).to_a.map(&:values).flatten.include?(indexdef)
+      index_exists = ActiveRecord::Base.connection.execute(
+        "SELECT indexdef FROM pg_indexes WHERE tablename NOT LIKE 'pg%';"
+      ).to_a.map(&:values).flatten.include?(indexdef)
 
-    # rubocop:disable Rails/Output
-    puts("\nWARNING missing index\n#{indexdef}\nfor prefixed id model #{name}!\n\n") unless index_exists
-    # rubocop:enable Rails/Output
+      # rubocop:disable Rails/Output
+      puts("\nWARNING missing index\n#{indexdef}\nfor prefixed id model #{name}!\n\n") unless index_exists
+      # rubocop:enable Rails/Output
+    rescue ActiveRecord::NoDatabaseError # only applicable when setting up project
+    end
 
     self::PREFIXED_ID_SQL = "('#{self::ID_PREFIX}' || #{table_name}.id)"
 
