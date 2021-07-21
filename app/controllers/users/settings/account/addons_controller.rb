f2e5dc8b2fee1a8b53ef24eef0cc59929375cac7
aignatov-bio <47317017+aignatov-bio@users.noreply.github.com>
2021-07-21 13:48:38 +0200

    Add printers to addon page [SCI-5904][SCI-5931] (#3450)
    * Add printers to addon page [SCI-5904]
    
    * Fix markup

--- app/controllers/users/settings/account/addons_controller.rb
+++ app/controllers/users/settings/account/addons_controller.rb
@@ -3,6 +3,18 @@ module Users
     module Account
       class AddonsController < ApplicationController
         layout 'fluid'
+
+        before_action :load_printers, only: :index
+
+        def label_printer
+          @printer = { printer_type: :fluics, ready: true, api_key: 'ISVO42192IUDV168ATFI314UVYGU151USHYEV42' }
+        end
+
+        private
+
+        def load_printers
+          @fluics_printer = { ready: true, enabled: true }
+        end
       end
     end
   end
