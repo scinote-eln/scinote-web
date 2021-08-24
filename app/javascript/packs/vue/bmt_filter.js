ee8a12a2a959d41ce26b081a71c13b232e659fc6
aignatov-bio <47317017+aignatov-bio@users.noreply.github.com>
2021-08-24 14:09:48 +0200

    Add save bmt filters front end [SCI-5962] (#3505)

--- app/javascript/packs/vue/bmt_filter.js
+++ app/javascript/packs/vue/bmt_filter.js
@@ -6,12 +6,31 @@ Vue.use(TurbolinksAdapter);
 Vue.prototype.i18n = window.I18n;
 
 window.initBMTFilter = () => {
-  const app = new Vue({
+  const bmtFilterContainer = new Vue({
     el: '#bmtFilterContainer',
+    data: () => {
+      return {
+        saved_filters: []
+      };
+    },
     components: {
       'filter-container': FilterContainer
     }
   });
-
   $('#bmtFilterContainer').on('click', (e) => e.stopPropagation());
+
+  $( "#saveBmtFilterForm" ).off().submit(function(e) {
+    let filterName = $(this).find('input#filter_name').val();
+    bmtFilterContainer.saved_filters.push({
+      id: bmtFilterContainer.saved_filters.length,
+      filter_name: filterName,
+      filters: []
+    });
+  });
+
 };
+
+$('.repository-show').on('click', '.open-save-bmt-modal', () => {
+  $('#modalSaveBmtFilter').modal('show')
+})
+
