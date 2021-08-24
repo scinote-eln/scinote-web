90d4a5f7d1bd4c17c949e268bac18dd3295ebfb2
aignatov-bio <47317017+aignatov-bio@users.noreply.github.com>
2021-08-24 14:09:48 +0200

    Add save bmt filters front end [SCI-5962] (#3505)

---
+++ app/javascript/vue/bmt_filter/saved_filter.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="saved-filter-element">
+    {{ saved_filter.filter_name }}
+    <button class="btn btn-light icon-btn" @click="$emit('saved_filter:delete')">
+      <i class="fas fa-trash"></i>
+    </button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'SavedFilterElement',
+    props: {
+      saved_filter: Object
+    }
+  }
+</script>
+</script>
