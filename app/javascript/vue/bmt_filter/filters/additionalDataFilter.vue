e08b4061856edb9a10a145b61735adbeaba26976
Martin Artnik <martin@scinote.net>
2021-08-25 12:07:44 +0200

    Added additionalDataFilter [SCI-6026]

--- app/javascript/vue/bmt_filter/filters/additionalDataFilter.vue
+++ app/javascript/vue/bmt_filter/filters/additionalDataFilter.vue
@@ -1,3 +1,37 @@
 <template>
-  <h1>TODO: additionalData</h1>
+  <div class="filter-form">
+    <div class="form-select">
+      <select @change="updateFilterData" v-model="attribute">
+        <option
+          v-for="attribute in additionalDataAttributes"
+          :key="attribute.name" :value="attribute.name">
+            {{ attribute.name }}
+        </option>
+      </select>
+    </div>
+    <input
+      @input="updateFilterData"
+      class="form-control"
+      type="text"
+      name="value"
+      v-model="value"
+    />
+  </div>
 </template>
+
+<script>
+  import FilterMixin from 'vue/bmt_filter/mixins/filter.js'
+  export default {
+    name: 'additionalDataFilter',
+    mixins: [FilterMixin],
+    props: {
+      additionalDataAttributes: Array
+    },
+    data() {
+      return {
+        attribute: null,
+        value: null
+      }
+    }
+  }
+</script>
