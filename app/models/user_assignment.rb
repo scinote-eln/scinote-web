5ed401848eb0f1d57ad08a4cf82f2ffe870b8fd3
Martin Artnik <martin@scinote.net>
2021-10-19 15:58:39 +0200

    Fixed cache invalidation for assignables, current_user missing in cache keys [SCI-6137]

--- app/models/user_assignment.rb
+++ app/models/user_assignment.rb
@@ -1,21 +1,10 @@
 # frozen_string_literal: true
 
 class UserAssignment < ApplicationRecord
-  belongs_to :assignable, polymorphic: true
+  belongs_to :assignable, polymorphic: true, touch: true
   belongs_to :user_role
   belongs_to :user
   belongs_to :assigned_by, class_name: 'User', optional: true
 
   enum assigned: { automatically: 0, manually: 1 }, _suffix: true
-
-  after_destroy :touch_assignable
-  after_save :touch_assignable
-
-  private
-
-  def touch_assignable
-    # This busts the cache of cached assigned user lists.
-    # Using .update_column instead of .touch, to avoid triggering an audit trail callback
-    assignable.update_column(:updated_at, Time.zone.now)
-  end
 end
