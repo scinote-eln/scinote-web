3746332c308693c293de2310649e9c0b4ab08102
artoscinote <85488244+artoscinote@users.noreply.github.com>
2021-10-19 16:00:52 +0200

    Fixed cached user lists when changing assignments [SCI-6137] (#3597)
    Fixed cache invalidation for assignables, current_user missing in cache keys [SCI-6137]

--- app/models/user_assignment.rb
+++ app/models/user_assignment.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class UserAssignment < ApplicationRecord
-  belongs_to :assignable, polymorphic: true
+  belongs_to :assignable, polymorphic: true, touch: true
   belongs_to :user_role
   belongs_to :user
   belongs_to :assigned_by, class_name: 'User', optional: true
