992d3bffa354a5007a1a3a98348271811e8c5c38
aignatov-bio <47317017+aignatov-bio@users.noreply.github.com>
2021-10-28 13:58:31 +0200

    small permissions interface updates [SCI-6145] (#3620)
    Co-authored-by: Anton <anton@scinote.net>

--- app/helpers/user_roles_helper.rb
+++ app/helpers/user_roles_helper.rb
@@ -3,7 +3,7 @@
 module UserRolesHelper
   def user_roles_collection
     Rails.cache.fetch([current_user, 'available_user_roles']) do
-      @user_roles_collection ||= UserRole.all.pluck(:name, :id)
+      @user_roles_collection ||= UserRole.order(id: :asc).pluck(:name, :id)
     end
   end
 end
