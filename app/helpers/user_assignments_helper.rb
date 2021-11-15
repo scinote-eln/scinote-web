f3bc7174e019624bcc0afcd8d7f01403557d8a90
Anton <anton@scinote.net>
2021-11-15 13:51:44 +0100

    Refactor permissions breadcrumbs for users [SCI-6260]

--- app/helpers/user_assignments_helper.rb
+++ app/helpers/user_assignments_helper.rb
@@ -10,44 +10,17 @@ module UserAssignmentsHelper
     sanitize_input(display_name)
   end
 
-  def user_assignment_resource_role_name(user_assignment, user, resource)
-    # Triggers N+1 but the partial is cached
-
-    if resource.is_a?(Experiment)
-      project_user_assignment_name = resource.permission_parent
-                                             .user_assignments
-                                             .find_by(user: user)
-                                             .user_role
-                                             .name
-      current_user_assignment_name = user_assignment.user_role.name
-
-      [
-        t('user_assignment.from_project', user_role: project_user_assignment_name),
-        (current_user_assignment_name unless current_user_assignment_name == project_user_assignment_name)
-      ].compact.join(' / ')
-    elsif resource.is_a?(MyModule)
-      project_user_assignment_name = resource.permission_parent
-                                             .permission_parent
-                                             .user_assignments
-                                             .find_by(user: user)
-                                             .user_role
-                                             .name
-      experiment_user_assignment_name = resource.permission_parent
-                                                .user_assignments
-                                                .find_by(user: user)
-                                                .user_role
-                                                .name
-      current_user_assignment_name = user_assignment.user_role.name
-
-      [
-        t('user_assignment.from_project',
-          user_role: project_user_assignment_name),
-        (t('user_assignment.from_experiment',
-           user_role: experiment_user_assignment_name) unless project_user_assignment_name == experiment_user_assignment_name),
-        (current_user_assignment_name unless experiment_user_assignment_name == current_user_assignment_name)
-      ].compact.join(' / ')
-    else
-      user_assignment.user_role.name
+  def user_assignment_resource_role_name(user, resource, inherit = '')
+    user_assignment = resource.user_assignments.find_by(user: user)
+    if resource.class != Project && user_assignment.automatically_assigned?
+      parent = resource.permission_parent
+      return user_assignment_resource_role_name(user, parent, '_inherit')
     end
+
+    "#{user_assignment.user_role.name}
+            <span class='permission-object-tag'
+              title='#{t("access_permissions.partials.#{resource.class.to_s.downcase}_tooltip#{inherit}")}'>
+              #{t("access_permissions.partials.#{resource.class.to_s.downcase}")}
+            </span>".html_safe
   end
 end
