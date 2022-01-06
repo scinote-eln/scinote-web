277b5f6eec0d84f47eac5ddb9683a0da5861f712
artoscinote <85488244+artoscinote@users.noreply.github.com>
2022-01-06 10:11:20 +0100

    Fixed reCaptcha causing stack overflows on invite modal [SCI-6052] (#3534)

--- app/helpers/form_tag_helper.rb
+++ app/helpers/form_tag_helper.rb
@@ -1,10 +1,14 @@
 module FormTagHelper
-  def recaptcha_input_tag
+  def recaptcha_input_tag(include_description: true)
     if Rails.configuration.x.enable_recaptcha
       res = "<div class='form-group sci-input-container"
       res << 'has-error' if flash[:recaptcha_error]
       res << "'>"
-      res << label_tag(:recaptcha_label, I18n.t('users.registrations.new.captcha_description'))
+
+      if include_description
+        res << label_tag(:recaptcha_label, I18n.t('users.registrations.new.captcha_description'))
+      end
+
       res << recaptcha_tags
       if flash[:recaptcha_error]
         res << "<span class='help-block'>"
