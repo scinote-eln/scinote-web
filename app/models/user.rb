05547da457edb194d7f3b6bc91401a52c7787129
aignatov-bio <47317017+aignatov-bio@users.noreply.github.com>
2022-01-19 13:19:56 +0100

    Add stock table renders [SCI-6412] (#3787)
    Co-authored-by: Anton <anton@scinote.net>

--- app/models/user.rb
+++ app/models/user.rb
@@ -247,6 +247,16 @@ class User < ApplicationRecord
            foreign_key: 'last_modified_by_id',
            inverse_of: :last_modified_by,
            dependent: :nullify
+  has_many :created_repository_stock_value,
+           class_name: 'RepositoryStockValue',
+           foreign_key: 'created_by_id',
+           inverse_of: :created_by,
+           dependent: :nullify
+  has_many :modified_repository_stock_value,
+           class_name: 'RepositoryStockValue',
+           foreign_key: 'created_by_id',
+           inverse_of: :last_modified_by,
+           dependent: :nullify
   has_many :created_repository_checklist_types,
            class_name: 'RepositoryChecklistItem',
            foreign_key: 'created_by_id',
