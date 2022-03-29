f75265974ea92a998b1a2f0579a7ed4216895c41
ajugo <andrej.jugovic7@gmail.com>
2022-03-29 12:09:33 +0200

    Print protocol smart annotation and table improvements [SCI-6566] (#3925)
    * Improve table printing in the print menu [SCI-6566]
    
    * Open smart annotation in new tab for print view [SCI-6566]
    
    * Unify naming of same parameter [SCI-6566]
    
    * Fix typo [SCI-6566]
    
    * Fix hound errors [SCI-6566]

--- app/helpers/input_sanitize_helper.rb
+++ app/helpers/input_sanitize_helper.rb
@@ -23,12 +23,13 @@ module InputSanitizeHelper
     team = options.fetch(:team) { nil }
     wrapper_tag = options.fetch(:wrapper_tag) { {} }
     tags = options.fetch(:tags) { [] }
+    preview_repository = options.fetch(:preview_repository) { false }
     format_opt = wrapper_tag.merge(sanitize: false)
     base64_encoded_imgs = options.fetch(:base64_encoded_imgs) { false }
     text = sanitize_input(text, tags)
     text = simple_format(sanitize_input(text), {}, format_opt) if simple_f
     auto_link(
-      smart_annotation_parser(text, team, base64_encoded_imgs),
+      custom_link_open_new_tab(smart_annotation_parser(text, team, base64_encoded_imgs, preview_repository)),
       link: :urls,
       sanitize: false,
       html: { target: '_blank' }
