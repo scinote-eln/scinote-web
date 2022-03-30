09a383c2ab1f4c45edc9c02b2c9b36d1695f4161
Martin Artnik <martin@scinote.net>
2022-03-30 15:10:46 +0200

    Disable stock reminders if stock management is disabled [SCI-6654]

--- app/models/concerns/reminder_repository_cell_joinable.rb
+++ app/models/concerns/reminder_repository_cell_joinable.rb
@@ -5,24 +5,33 @@ module ReminderRepositoryCellJoinable
 
   included do
     def self.reminder_repository_cells_scope(relation, user)
-      relation.joins( # datetime reminders
+      reminder_scope = relation.joins( # datetime reminders
         'LEFT OUTER JOIN "repository_date_time_values" ON '\
         '"repository_date_time_values"."id" = "repository_cells"."value_id" AND '\
         '"repository_cells"."value_type" = \'RepositoryDateTimeValueBase\' '\
         'AND repository_columns.metadata ? \'reminder_delta\' AND '\
         '(repository_date_time_values.data - NOW()) <= '\
         '(repository_columns.metadata ->> \'reminder_delta\')::int * interval \'1 sec\''
-      ).joins( # stock reminders
-        'LEFT OUTER JOIN "repository_stock_values" ON "repository_stock_values"."id" = "repository_cells"."value_id" AND '\
-        '"repository_cells"."value_type" = \'RepositoryStockValue\' AND '\
-        'repository_stock_values.amount <= repository_stock_values.low_stock_threshold'
-      ).joins(
+      )
+
+      if RepositoryStockValue.enabled?
+        reminder_scope = reminder_scope.joins( # stock reminders
+          'LEFT OUTER JOIN "repository_stock_values" ON '\
+          '"repository_stock_values"."id" = "repository_cells"."value_id" AND '\
+          '"repository_cells"."value_type" = \'RepositoryStockValue\' AND '\
+          'repository_stock_values.amount <= repository_stock_values.low_stock_threshold'
+        )
+      end
+
+      reminder_scope.joins(
         'LEFT OUTER JOIN "hidden_repository_cell_reminders" ON '\
         '"repository_cells"."id" = "hidden_repository_cell_reminders"."repository_cell_id" AND '\
         '"hidden_repository_cell_reminders"."user_id" = ' + user.id.to_s
       ).where(
-        'hidden_repository_cell_reminders.id IS NULL AND '\
-        '(repository_date_time_values.id IS NOT NULL OR repository_stock_values.id IS NOT NULL)'
+        "hidden_repository_cell_reminders.id IS NULL AND "\
+        "(repository_date_time_values.id IS NOT NULL"\
+        "#{' OR repository_stock_values.id IS NOT NULL' if RepositoryStockValue.enabled?}"\
+        ")"
       )
     end
   end
