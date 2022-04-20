992b820b0eeb26875fef22ac098ecba4516eb0ec
Jacob Specht <specht.jacob@epa.gov>
2022-04-20 08:57:28 -0400

    Remove bad migrations.

--- db/migrate/20150713061603_add_user_columns.rb
+++
@@ -1,9 +0,0 @@
-class AddUserColumns < ActiveRecord::Migration[4.2]
-  def up
-    add_attachment :users, :avatar
-  end
-
-  def down
-    remove_attachment :users, :avatar
-  end
-end
--- db/migrate/20160808083040_add_attachment_workflowimg_to_experiments.rb
+++
@@ -1,9 +0,0 @@
-class AddAttachmentWorkflowimgToExperiments < ActiveRecord::Migration[4.2]
-  def self.up
-    add_attachment :experiments, :workflowimg
-  end
-
-  def self.down
-    remove_attachment :experiments, :workflowimg
-  end
-end
