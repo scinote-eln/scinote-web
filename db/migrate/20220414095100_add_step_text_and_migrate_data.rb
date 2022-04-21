989503febe9663e63c16c2ac5a1088829165614c
Oleksii Kriuchykhin <okriuchykhin@biosistemika.com>
2022-04-21 13:59:15 +0200

    Migrate tiny MCE images to step texts [SCI-6712]

--- db/migrate/20220414095100_add_step_text_and_migrate_data.rb
+++ db/migrate/20220414095100_add_step_text_and_migrate_data.rb
@@ -23,9 +23,20 @@ class AddStepTextAndMigrateData < ActiveRecord::Migration[6.1]
       end
       StepText.import(step_texts, validate: false)
     end
+
+    Step.joins(:tiny_mce_assets, :step_texts)
+        .preload(:tiny_mce_assets, :step_texts).find_each do |step|
+      step_text_id = step.step_texts.take.id
+      step.tiny_mce_assets.update_all(object_type: 'StepText', object_id: step_text_id)
+    end
   end
 
   def down
+    StepText.joins(:tiny_mce_assets)
+            .preload(:step, :tiny_mce_assets).find_each do |step_text|
+      step_text.tiny_mce_assets.update_all(object_type: 'Step', object_id: step_text.step.id)
+    end
+
     drop_table :step_texts
   end
 end
