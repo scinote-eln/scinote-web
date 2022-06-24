589a7d3219d824477ab9a3f5fac44b30170c4fee
Oleksii Kriuchykhin <okriuchykhin@biosistemika.com>
2022-06-24 10:44:28 +0200

    Add team reference to user assignments model [SCI-6915]

--- app/models/user_assignment.rb
+++ app/models/user_assignment.rb
@@ -1,12 +1,15 @@
 # frozen_string_literal: true
 
 class UserAssignment < ApplicationRecord
+  before_validation -> { self.team ||= (assignable.is_a?(Team) ? assignable : assignable.team) }
+
   belongs_to :assignable, polymorphic: true, touch: true
   belongs_to :user_role
   belongs_to :user
+  belongs_to :team
   belongs_to :assigned_by, class_name: 'User', optional: true
 
   enum assigned: { automatically: 0, manually: 1 }, _suffix: true
 
-  validates :user, uniqueness: { scope: :assignable }
+  validates :user, uniqueness: { scope: %i(assignable team_id) }
 end
