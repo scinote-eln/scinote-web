5bc59dc7bd4bc777da09d69ec4e65899cd975f07
Martin Artnik <martin@scinote.net>
2022-06-28 15:55:27 +0200

    Generalize reorder validation [SCI-6891]

--- app/controllers/api/service/protocols_controller.rb
+++ app/controllers/api/service/protocols_controller.rb
@@ -3,6 +3,8 @@
 module Api
   module Service
     class ProtocolsController < BaseController
+      include Api::Service::ReorderValidation
+
       before_action :load_team, :load_project, :load_experiment, :load_task
       before_action :load_protocol, only: :reorder_steps
       before_action :validate_step_order, only: :reorder_steps
@@ -26,12 +28,7 @@ module Api
       end
 
       def validate_step_order
-        # contains all step ids, positions have values from 0 to number of steps - 1
-        valid_step_order =
-          @protocol.steps.order(:id).pluck(:id) == step_reorder_params.pluck(:id).sort &&
-          step_reorder_params.pluck(:position).sort == (0...step_reorder_params.length).to_a
-
-        unless valid_step_order
+        unless reorder_params_valid?(@protocol.steps, step_reorder_params)
           render json: { error: I18n.t('activerecord.errors.models.protocol.attributes.step_order.invalid') },
                  status: :bad_request
         end
