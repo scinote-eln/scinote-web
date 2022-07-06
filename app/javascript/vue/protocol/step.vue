c9e7e68369e9e2df8a06a178afd4ed85fd984c52
artoscinote <85488244+artoscinote@users.noreply.github.com>
2022-07-06 14:11:48 +0200

    Insert element dropdown fixes [SCI-6925] (#4184)
    * Insert element dropdown fixes [SCI-6925]
    
    * Dropdown bugfix, rounder CSS size [SCI-6925]

--- app/javascript/vue/protocol/step.vue
+++ app/javascript/vue/protocol/step.vue
@@ -41,12 +41,12 @@
         </span>
       </div>
       <div class="step-actions-container">
-        <div  v-if="urls.update_url"  class="dropdown">
+        <div ref="actionsDropdownButton" v-if="urls.update_url"  class="dropdown">
           <button class="btn btn-light dropdown-toggle insert-button" type="button" :id="'stepInserMenu_' + step.id" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
             {{ i18n.t('protocols.steps.insert.button') }}
             <span class="caret"></span>
           </button>
-          <ul class="dropdown-menu insert-element-dropdown" :aria-labelledby="'stepInserMenu_' + step.id">
+          <ul ref="actionsDropdown" class="dropdown-menu insert-element-dropdown" :aria-labelledby="'stepInserMenu_' + step.id">
             <li class="title">
               {{ i18n.t('protocols.steps.insert.title') }}
             </li>
@@ -220,6 +220,7 @@
     },
     mounted() {
       $(this.$refs.comments).data('closeCallback', this.closeCommentsSidebar)
+      $(this.$refs.actionsDropdownButton).on('shown.bs.dropdown hidden.bs.dropdown', this.handleDropdownPosition);
     },
     computed: {
       reorderableElements() {
@@ -354,6 +355,19 @@
       },
       closeReorderModal() {
         this.reordering = false;
+      },
+      handleDropdownPosition() {
+        this.$refs.actionsDropdownButton.classList.toggle("dropup", !this.isInViewport(this.$refs.actionsDropdown));
+      },
+      isInViewport(el) {
+          let rect = el.getBoundingClientRect();
+
+          return (
+              rect.top >= 0 &&
+              rect.left >= 0 &&
+              rect.bottom <= (window.innerHeight || $(window).height()) &&
+              rect.right <= (window.innerWidth || $(window).width())
+          );
       },
       copyPasteImageModal(pasteImages) {
         this.pasteImages = pasteImages;
