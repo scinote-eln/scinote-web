135724b9c2b58a8ce4566f57d4d002bdd8043720
Andrej <andrej.jugovic7@gmail.com>
2022-07-20 16:26:52 +0200

    Fix save day time filter problem [SCI-6840]

--- app/models/repository_time_range_value.rb
+++ app/models/repository_time_range_value.rb
@@ -15,30 +15,57 @@ class RepositoryTimeRangeValue < RepositoryDateTimeRangeValueBase
     super(:time)
   end
 
+  def self.saving_time_correction(origin_time, filter_time)
+    origin_time += 1.hour if origin_time.dst? && !filter_time.dst?
+    origin_time -= 1.hour if filter_time.dst? && !origin_time.dst?
+    origin_time
+  end
+
+  def self.time_convert_helper(time_value_start, time_value_end, parameters)
+    return saving_time_correction(time_value_start, Time.zone.parse(parameters['start_time'])).utc.strftime('%H%M%S'),
+           saving_time_correction(time_value_end, Time.zone.parse(parameters['end_time'])).utc.strftime('%H%M%S'),
+           Time.zone.parse(parameters['start_time']).utc.strftime('%H%M%S'),
+           Time.zone.parse(parameters['end_time']).utc.strftime('%H%M%S')
+  end
+
   def self.add_filter_condition(repository_rows, join_alias, filter_element)
     parameters = filter_element.parameters
-    case filter_element.operator
-    when 'equal_to'
-      repository_rows.where("#{join_alias}.start_time::time = (?)::time AND #{join_alias}.end_time::time = (?)::time",
-                            Time.zone.parse(parameters['start_time']), Time.zone.parse(parameters['end_time']))
-    when 'unequal_to'
-      repository_rows
-        .where.not("#{join_alias}.start_time::time = (?)::time AND #{join_alias}.end_time::time = (?)::time",
-                   Time.zone.parse(parameters['start_time']), Time.zone.parse(parameters['end_time']))
-    when 'greater_than'
-      repository_rows.where("#{join_alias}.start_time::time > (?)::time", Time.zone.parse(parameters['end_time']))
-    when 'greater_than_or_equal_to'
-      repository_rows.where("#{join_alias}.start_time::time >= (?)::time", Time.zone.parse(parameters['end_time']))
-    when 'less_than'
-      repository_rows.where("#{join_alias}.end_time::time < (?)::time", Time.zone.parse(parameters['start_time']))
-    when 'less_than_or_equal_to'
-      repository_rows.where("#{join_alias}.end_time::time <= (?)::time", Time.zone.parse(parameters['start_time']))
-    when 'between'
-      repository_rows.where("#{join_alias}.start_time::time > (?)::time AND #{join_alias}.end_time::time < (?)::time",
-                            Time.zone.parse(parameters['start_time']), Time.zone.parse(parameters['end_time']))
-    else
-      raise ArgumentError, 'Wrong operator for RepositoryTimeRangeValue!'
+    compare_operator = filter_element.operator
+    unfiltered_data = repository_rows.pluck('repository_rows.id',
+                                            "#{join_alias}.start_time",
+                                            "#{join_alias}.end_time").map do |id, start_time, end_time|
+      { id: id, start_time: start_time,
+        end_time: end_time }
     end
+
+    filtered_data = unfiltered_data.select do |row|
+      if row[:start_time] && row[:end_time]
+        time_value_start, time_value_end, filter_time_start, filter_time_end = time_convert_helper(
+          Time.zone.parse(row[:start_time].to_s), Time.zone.parse(row[:end_time].to_s), parameters
+        )
+        case compare_operator
+        when 'equal_to'
+          time_value_start == filter_time_start && time_value_end == filter_time_end
+        when 'unequal_to'
+          time_value_start != filter_time_start || time_value_end != filter_time_end
+        when 'greater_than'
+          time_value_start > filter_time_end
+        when 'greater_than_or_equal_to'
+          time_value_start >= filter_time_end
+        when 'less_than'
+          time_value_end < filter_time_start
+        when 'less_than_or_equal_to'
+          time_value_end <= filter_time_start
+        when 'between'
+          time_value_start > filter_time_start && time_value_end < filter_time_end
+        else
+          raise ArgumentError, 'Wrong operator for RepositoryTimeValue!'
+        end
+      else
+        false
+      end
+    end
+    RepositoryRow.where(id: filtered_data.pluck(:id))
   end
 
   def self.new_with_payload(payload, attributes)
--- app/models/repository_time_value.rb
+++ app/models/repository_time_value.rb
@@ -12,27 +12,55 @@ class RepositoryTimeValue < RepositoryDateTimeValueBase
     super(:time)
   end
 
+  def self.saving_time_correction(origin_time, filter_time)
+    origin_time += 1.hour if origin_time.dst? && !filter_time.dst?
+    origin_time -= 1.hour if filter_time.dst? && !origin_time.dst?
+    origin_time
+  end
+
+  def self.time_convert_helper(time_value, parameters, compare_operator)
+    time_field = compare_operator == 'between' ? 'start_time' : 'time'
+    time_value = saving_time_correction(time_value, Time.zone.parse(parameters[time_field]))
+    filter_time = Time.zone.parse(parameters[time_field])
+    filter_time_end = Time.zone.parse(parameters['end_time']) if compare_operator == 'between'
+    return time_value.utc.strftime('%H%M%S'),
+           filter_time.utc.strftime('%H%M%S'),
+           filter_time_end&.utc&.strftime('%H%M%S')
+  end
+
   def self.add_filter_condition(repository_rows, join_alias, filter_element)
     parameters = filter_element.parameters
-    case filter_element.operator
-    when 'equal_to'
-      repository_rows.where("#{join_alias}.data::time = (?)::time", Time.zone.parse(parameters['time']))
-    when 'unequal_to'
-      repository_rows.where.not("#{join_alias}.data::time = (?)::time", Time.zone.parse(parameters['time']))
-    when 'greater_than'
-      repository_rows.where("#{join_alias}.data::time > (?)::time", Time.zone.parse(parameters['time']))
-    when 'greater_than_or_equal_to'
-      repository_rows.where("#{join_alias}.data::time >= (?)::time", Time.zone.parse(parameters['time']))
-    when 'less_than'
-      repository_rows.where("#{join_alias}.data::time < (?)::time", Time.zone.parse(parameters['time']))
-    when 'less_than_or_equal_to'
-      repository_rows.where("#{join_alias}.data::time <= (?)::time", Time.zone.parse(parameters['time']))
-    when 'between'
-      repository_rows.where("#{join_alias}.data::time > (?)::time AND #{join_alias}.data::time < (?)::time",
-                            Time.zone.parse(parameters['start_time']), Time.zone.parse(parameters['end_time']))
-    else
-      raise ArgumentError, 'Wrong operator for RepositoryTimeValue!'
+    compare_operator = filter_element.operator
+    unfiltered_data = repository_rows.pluck('repository_rows.id',
+                                            "#{join_alias}.data").map { |id, data| { id: id, data: data } }
+    filtered_data = unfiltered_data.select do |row|
+      if row[:data]
+        time_value, filter_time, filter_time_end = time_convert_helper(Time.zone.parse(row[:data].to_s),
+                                                                       parameters,
+                                                                       compare_operator)
+        case compare_operator
+        when 'equal_to'
+          time_value == filter_time
+        when 'unequal_to'
+          time_value != filter_time
+        when 'greater_than'
+          time_value > filter_time
+        when 'greater_than_or_equal_to'
+          time_value >= filter_time
+        when 'less_than'
+          time_value < filter_time
+        when 'less_than_or_equal_to'
+          time_value <= filter_time
+        when 'between'
+          filter_time < time_value && time_value < filter_time_end
+        else
+          raise ArgumentError, 'Wrong operator for RepositoryTimeValue!'
+        end
+      else
+        false
+      end
     end
+    RepositoryRow.where(id: filtered_data.pluck(:id))
   end
 
   def self.new_with_payload(payload, attributes)
