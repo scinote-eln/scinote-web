7874a3e1c56ad8f778b27979a4bb1763dd9a1009
Alex Kriuchykhin <okriuchykhin@biosistemika.com>
2022-07-20 10:14:48 +0200

    Fix updating protocol from repository [SCI-6993] (#4266)

--- app/models/step_table.rb
+++ app/models/step_table.rb
@@ -5,5 +5,5 @@ class StepTable < ApplicationRecord
 
   belongs_to :step, inverse_of: :step_tables, touch: true
   belongs_to :table, inverse_of: :step_table
-  has_many :step_orderable_elements, as: :orderable, dependent: :destroy
+  has_one :step_orderable_element, as: :orderable, dependent: :destroy
 end
--- app/serializers/checklist_item_serializer.rb
+++ app/serializers/checklist_item_serializer.rb
@@ -23,15 +23,17 @@ class ChecklistItemSerializer < ActiveModel::Serializer
     step = object.checklist.step
     my_module = object.checklist.step.protocol.my_module
 
+    if can_manage_step?(scope[:user] || @instance_options[:user], step)
+      urls_list[:update_url] = step_checklist_checklist_item_path(step, object.checklist, object)
+      urls_list[:delete_url] = step_checklist_checklist_item_path(step, object.checklist, object)
+    end
+
+    return urls_list unless my_module
+
     if !object.checked && can_check_my_module_steps?(scope[:user] || @instance_options[:user], my_module) ||
        object.checked && can_uncheck_my_module_steps?(scope[:user] || @instance_options[:user], my_module)
       urls_list[:toggle_url] =
         toggle_step_checklist_checklist_item_path(step, object.checklist, object)
-    end
-
-    if can_manage_step?(scope[:user] || @instance_options[:user], step)
-      urls_list[:update_url] = step_checklist_checklist_item_path(step, object.checklist, object)
-      urls_list[:delete_url] = step_checklist_checklist_item_path(step, object.checklist, object)
     end
 
     urls_list
