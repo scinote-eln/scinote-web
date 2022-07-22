ffb1e0435f78b41789f13ee3df945ea048723887
Martin Artnik <martin@scinote.net>
2022-07-22 15:09:42 +0200

    Add active reminders filter [SCI-7004]

--- app/assets/javascripts/repositories/repository_datatable.js
+++ app/assets/javascripts/repositories/repository_datatable.js
@@ -276,6 +276,22 @@ var RepositoryDatatable = (function(global) {
     });
   }
 
+  function initActiveRemindersFilter() {
+    $(TABLE_WRAPPER_ID).find('#only_reminders').on('change', function() {
+      var $activeRemindersFilter = $(this).closest('.active-reminders-filter');
+
+      $(TABLE_WRAPPER_ID).find('table').attr('data-only-reminders', $(this).is(':checked'));
+
+      if ($(this).is(':checked')) {
+        $activeRemindersFilter.attr('title', $activeRemindersFilter.data('checkedTitle'));
+      } else {
+        $activeRemindersFilter.attr('title', $activeRemindersFilter.data('uncheckedTitle'));
+      }
+
+      TABLE.ajax.reload();
+    });
+  }
+
   function resetTableView() {
     var filterSaveButtonVisible = !$('#saveRepositoryFilters').hasClass('hidden');
     $.getJSON($(TABLE_ID).data('toolbar-url'), (data) => {
@@ -428,6 +444,11 @@ var RepositoryDatatable = (function(global) {
           if ($('[data-repository-filter-json]').attr('data-repository-filter-json')) {
             d.advanced_search = JSON.parse($('[data-repository-filter-json]').attr('data-repository-filter-json'));
           }
+
+          if ($('[data-only-reminders]').attr('data-only-reminders') === 'true') {
+            d.only_reminders = true;
+          }
+
           return JSON.stringify(d);
         },
         global: false,
@@ -618,6 +639,8 @@ var RepositoryDatatable = (function(global) {
 
         initAssignedTasksDropdown(TABLE_ID);
         initReminderDropdown(TABLE_ID);
+
+        initActiveRemindersFilter();
         renderFiltersDropdown();
         setTimeout(function() {
           adjustTableHeader();
