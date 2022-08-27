e2e0d2f855951772506b0eda795af45dc566c9bb
Andrej <andrej.jugovic7@gmail.com>
2022-08-27 13:50:55 +0200

    Fix hound [SCI-7104]

--- app/assets/javascripts/sitewide/repository_row_info_modal.js
+++ app/assets/javascripts/sitewide/repository_row_info_modal.js
@@ -92,26 +92,6 @@
         }
       });
 
-      dropdownSelector.init('#modal-print-repository-row-label ' + LABEL_TEMPLATE_SELECTOR, {
-        noEmptyOption: true,
-        singleSelect: true,
-        closeOnSelect: true,
-        selectAppearance: 'simple',
-        localFilter: function(options) {
-          var printerType = JSON.parse($('option:selected', LABEL_PRINTER_SELECTOR).attr('data-params')).type;
-          return options.filter(function(option, value) {
-            var labelType = JSON.parse($(value).attr('data-params')).type;
-            var showLabel = false;
-            if (printerType === FLUICS_LABEL) {
-              showLabel = [FLUICS_LABEL, ZEBRA_LABEL].some(el => labelType.toLowerCase().includes(el));
-            } else if (printerType === ZEBRA_LABEL) {
-              showLabel = labelType.toLowerCase().includes(ZEBRA_LABEL);
-            }
-            return showLabel;
-          });
-        }
-      });
-
       dropdownSelector.init('#modal-print-repository-row-label ' + LABEL_PRINTER_SELECTOR, {
         noEmptyOption: true,
         singleSelect: true,
