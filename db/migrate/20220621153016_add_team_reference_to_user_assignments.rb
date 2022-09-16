37345e9225aa84b3da40f47ac32388382a0b4d7c
Martin Artnik <martin@scinote.net>
2022-09-16 15:33:19 +0200

    Migration fixes [SCI-7219]

--- db/migrate/20220621153016_add_team_reference_to_user_assignments.rb
+++ db/migrate/20220621153016_add_team_reference_to_user_assignments.rb
@@ -3,6 +3,7 @@
 class AddTeamReferenceToUserAssignments < ActiveRecord::Migration[6.1]
   def up
     add_reference :user_assignments, :team, index: true, foreign_key: true
+    UserAssignment.reset_column_information
 
     UserAssignment.preload(:assignable).find_each do |user_assignment|
       team = user_assignment.assignable.is_a?(Team) ? user_assignment.assignable : user_assignment.assignable.team
