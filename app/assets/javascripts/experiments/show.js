49fbc2c5616a7a35a62620bf5c3beeb385c2c822
Oleksii Kriuchykhin <okriuchykhin@biosistemika.com>
2022-09-28 23:18:36 +0200

    Add basic modal for direct task creation [SCI-7197]

---
+++ app/assets/javascripts/experiments/show.js
@@ -0,0 +1,30 @@
+(function() {
+  function initNewMyModuleModal() {
+    let experimentWrapper = '#experiment-canvas';
+    let newMyModuleModal = '#new-my-module-modal';
+
+    // Modal's submit handler function
+    $(experimentWrapper)
+      .on('ajax:success', newMyModuleModal, function() {
+        $(newMyModuleModal).modal('hide');
+      })
+      .on('ajax:error', newMyModuleModal, function(ev, data) {
+        $(this).renderFormErrors('my_module', data.responseJSON);
+      });
+
+    $(experimentWrapper)
+      .on('ajax:success', '.new-my-module-button', function(ev, data) {
+        // Add and show modal
+        $(experimentWrapper).append($.parseHTML(data.html));
+        $(newMyModuleModal).modal('show');
+        $(newMyModuleModal).find("input[type='text']").focus();
+
+        // Remove modal when it gets closed
+        $(newMyModuleModal).on('hidden.bs.modal', function() {
+          $(newMyModuleModal).remove();
+        });
+      });
+  }
+
+  initNewMyModuleModal();
+}());
