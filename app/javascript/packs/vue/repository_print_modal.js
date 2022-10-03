884fe58525ed6aa18b3131d0cd842f1d2efced71
Andrej <andrej.jugovic7@gmail.com>
2022-10-03 17:16:50 +0200

    Fix label print open issue [SCI-7296]

--- app/javascript/packs/vue/repository_print_modal.js
+++ app/javascript/packs/vue/repository_print_modal.js
@@ -7,37 +7,35 @@ Vue.prototype.i18n = window.I18n;
 
 function initPrintModalComponent() {
   const container = $('.print-label-modal-container');
-  if (!window.PrintModalComponent && container.length) {
-    window.PrintModalComponent = new Vue({
-      el: '.print-label-modal-container',
-      name: 'PrintModalComponent',
-      components: {
-        'print-modal-container': PrintModalContainer
-      },
-      data() {
-        return {
-          showModal: false,
-          row_ids: [],
-          zebraEnabled: container.data('zebra-enabled'),
-          urls: {
-            print: container.data('print-url'),
-            zebraProgress: container.data('zebra-progress-url'),
-            printers: container.data('printers-url'),
-            labelTemplates: container.data('label-templates-url'),
-            rows: container.data('rows-url'),
-            fluicsInfo: container.data('fluics-info-url'),
-            printValidation: container.data('print-validation-url'),
-            labelPreview: container.data('label-preview-url')
-          }
-        };
-      },
-      methods: {
-        closeModal() {
-          this.showModal = false;
+  window.PrintModalComponent = new Vue({
+    el: '.print-label-modal-container',
+    name: 'PrintModalComponent',
+    components: {
+      'print-modal-container': PrintModalContainer
+    },
+    data() {
+      return {
+        showModal: false,
+        row_ids: [],
+        zebraEnabled: container.data('zebra-enabled'),
+        urls: {
+          print: container.data('print-url'),
+          zebraProgress: container.data('zebra-progress-url'),
+          printers: container.data('printers-url'),
+          labelTemplates: container.data('label-templates-url'),
+          rows: container.data('rows-url'),
+          fluicsInfo: container.data('fluics-info-url'),
+          printValidation: container.data('print-validation-url'),
+          labelPreview: container.data('label-preview-url')
         }
+      };
+    },
+    methods: {
+      closeModal() {
+        this.showModal = false;
       }
-    });
-  }
+    }
+  });
 }
 
 initPrintModalComponent();
