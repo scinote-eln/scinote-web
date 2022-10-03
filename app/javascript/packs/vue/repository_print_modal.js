e05ad9749809eb267dbad853a5434c9bcd743543
Andrej <andrej.jugovic7@gmail.com>
2022-10-03 13:39:18 +0200

    Enable label print modal for smart annotations [SCI-7270]

--- app/javascript/packs/vue/repository_print_modal.js
+++ app/javascript/packs/vue/repository_print_modal.js
@@ -7,36 +7,37 @@ Vue.prototype.i18n = window.I18n;
 
 function initPrintModalComponent() {
   const container = $('.print-label-modal-container');
-
-  window.PrintModalComponent = new Vue({
-    el: '.print-label-modal-container',
-    name: 'PrintModalComponent',
-    components: {
-      'print-modal-container': PrintModalContainer
-    },
-    data() {
-      return {
-        showModal: false,
-        row_ids: [],
-        zebraEnabled: container.data('zebra-enabled'),
-        urls: {
-          print: container.data('print-url'),
-          zebraProgress: container.data('zebra-progress-url'),
-          printers: container.data('printers-url'),
-          labelTemplates: container.data('label-templates-url'),
-          rows: container.data('rows-url'),
-          fluicsInfo: container.data('fluics-info-url'),
-          printValidation: container.data('print-validation-url'),
-          labelPreview: container.data('label-preview-url')
+  if (!window.PrintModalComponent && container.length) {
+    window.PrintModalComponent = new Vue({
+      el: '.print-label-modal-container',
+      name: 'PrintModalComponent',
+      components: {
+        'print-modal-container': PrintModalContainer
+      },
+      data() {
+        return {
+          showModal: false,
+          row_ids: [],
+          zebraEnabled: container.data('zebra-enabled'),
+          urls: {
+            print: container.data('print-url'),
+            zebraProgress: container.data('zebra-progress-url'),
+            printers: container.data('printers-url'),
+            labelTemplates: container.data('label-templates-url'),
+            rows: container.data('rows-url'),
+            fluicsInfo: container.data('fluics-info-url'),
+            printValidation: container.data('print-validation-url'),
+            labelPreview: container.data('label-preview-url')
+          }
+        };
+      },
+      methods: {
+        closeModal() {
+          this.showModal = false;
         }
-      };
-    },
-    methods: {
-      closeModal() {
-        this.showModal = false;
       }
-    }
-  });
+    });
+  }
 }
 
 initPrintModalComponent();
