47d593f0dbae34e8938915e7ea3af1551c3d2d85
Giga Chubinidze <gchubinidze@unisens.ge>
2022-11-23 15:38:01 +0400

    Missing navigate away pop up at inventory page [SCI-7492]

--- app/assets/javascripts/repositories/repository_datatable.js
+++ app/assets/javascripts/repositories/repository_datatable.js
@@ -90,6 +90,8 @@ var RepositoryDatatable = (function(global) {
         $('#editDeleteCopy').show();
         $('#toolbarPrintLabel').show();
       }
+      $("#team-switch").css({"pointer-events":"visible", "opacity":"1"});
+      $("#navigationGoBtn").prop('disabled', false);
     } else if (currentMode === 'editMode') {
       $(TABLE_WRAPPER_ID).addClass('editing');
       $('.repository-save-changes-link').on('click', function() {
@@ -119,6 +121,8 @@ var RepositoryDatatable = (function(global) {
       $('.dataTables_filter input').prop('disabled', true);
       $('#toolbarPrintLabel').hide();
       $('.repository-edit-overlay').show();
+      $("#team-switch").css({"pointer-events":"none", "opacity":"0.6"});
+      $("#navigationGoBtn").prop('disabled', true);
     }
   }
 
