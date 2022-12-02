b29aeab67a4ce607412458398017a9ca72f21a76
Oleksii Kriuchykhin <okriuchykhin@biosistemika.com>
2022-12-02 14:54:05 +0100

    Add API endpoint for retrieving a list of users [SCI-7455]

--- config/routes.rb
+++ config/routes.rb
@@ -852,6 +852,7 @@ Rails.application.routes.draw do
               end
             end
             resources :project_folders, only: %i(index show create update)
+            resources :users, only: %i(index)
           end
           resources :users, only: %i(show) do
             resources :user_identities,
