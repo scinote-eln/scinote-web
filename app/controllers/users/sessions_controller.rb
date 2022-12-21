41362f300e421a86dfea60fde2fb576a6fa832c7
Andrej <andrej.jugovic7@gmail.com>
2022-12-21 11:59:25 +0100

    Implement 2fa screen after sign in [SCI-7612]

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -28,7 +28,7 @@ class Users::SessionsController < Devise::SessionsController
         sign_out
         session[:otp_user_id] = user.id
         store_location_for(:user, initial_page)
-        redirect_to users_two_factor_auth_path
+        redirect_to redirect_to_two_factor_auth
         return
       end
     end
@@ -135,5 +135,9 @@ class Users::SessionsController < Devise::SessionsController
 
   def redirect_to_two_factor_auth?(user)
     user.two_factor_auth_enabled? && !bypass_two_factor_auth?
+  end
+
+  def redirect_to_two_factor_auth
+    users_two_factor_auth_path
   end
 end
