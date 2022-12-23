b9323feaea856a7a59e5e3a9507deb128452b0b5
Andrej <andrej.jugovic7@gmail.com>
2022-12-23 09:42:59 +0100

    Fix due date overflow [SCI-7649]

--- app/assets/javascripts/experiments/table.js
+++ app/assets/javascripts/experiments/table.js
@@ -95,6 +95,15 @@ var ExperimnetTable = {
       });
 
       $(element).on('dp.show', function() {
+        var datePicker = $('.bootstrap-datetimepicker-widget.dropdown-menu')[0];
+
+        // show full datepicker menu for due date
+        if (datePicker.getBoundingClientRect().bottom > window.innerHeight) {
+          datePicker.scrollIntoView(false);
+        } else if (datePicker.getBoundingClientRect().top < 0) {
+          datePicker.scrollIntoView();
+        }
+
         dateText.attr('data-original-title', '').tooltip('hide');
         if (dueDateContainer.find('.due-date-label').data('due-date')) {
           clearDate.addClass('open');
