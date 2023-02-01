6fcb9152dcda14a23111bae8fc49cc9440295f44
ajugo <andrej.jugovic7@gmail.com>
2023-02-01 12:01:18 +0100

    Fix CSS for generated pdf/docx reports [SCI-7660] (#4911)
    * Fix CSS for generated pdf/docx reports [SCI-7660]
    
    * Call correct asset previewable function [SCI-7660]

--- app/services/reports/docx/draw_result_asset.rb
+++ app/services/reports/docx/draw_result_asset.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Reports::Docx::DrawResultAsset
-  def draw_result_asset(result)
+  def draw_result_asset(result, settings)
     asset = result.asset
     timestamp = asset.created_at
     asset_url = Rails.application.routes.url_helpers.asset_download_url(asset)
@@ -27,6 +27,10 @@ module Reports::Docx::DrawResultAsset
       text ' ' + I18n.t('projects.reports.elements.result_asset.file_name', file: asset.file_name)
       text ' ' + I18n.t('projects.reports.elements.result_asset.user_time',
                         user: result.user.full_name, timestamp: I18n.l(timestamp, format: :full)), color: color[:gray]
+
+      if settings.dig(:task, :file_results_previews) && ActiveStorageFileUtil.previewable_document?(asset&.file&.blob)
+        text " #{I18n.t('projects.reports.elements.result_asset.full_preview_attached')}", color: color[:gray]
+      end
     end
 
     draw_result_comments(result) if @settings.dig('task', 'result_comments')
