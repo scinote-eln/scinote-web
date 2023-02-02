8d15fe5188ca09da26f7dbfb98090217d1775856
Martin Artnik <martin@scinote.net>
2023-02-02 15:07:48 +0100

    Fix sidebar not showing on user profile settings [SCI-7884]

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -6,7 +6,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
                 only: %i(new create new_with_provider create_with_provider)
   before_action :sign_up_with_provider_enabled?,
                 only: %i(new_with_provider create_with_provider)
-  layout :layout
+  layout 'fluid', only: :edit
 
   def avatar
     user = User.find_by_id(params[:id]) || current_user
@@ -255,10 +255,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   private
-
-  def layout
-    'fluid' if action_name == 'edit'
-  end
 
   def check_captcha
     if Rails.configuration.x.enable_recaptcha
