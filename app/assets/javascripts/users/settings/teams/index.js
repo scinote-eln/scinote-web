587c96f1c5816cf6a95cb822dadee394c1578dbe
sboursen-scinote <soufiane@scinote.net>
2023-02-22 14:36:41 +0100

    Fix hound issues [SCI-7512]

--- app/assets/javascripts/users/settings/teams/index.js
+++ app/assets/javascripts/users/settings/teams/index.js
@@ -1,59 +1,59 @@
 function initLeaveTeams() {
   // Bind the "leave team" buttons in teams table
   $(document)
-  .on(
-    "ajax:success",
-    "[data-action='leave-user-team']",
-    function (e, data, status, xhr) {
+    .on(
+      'ajax:success',
+      "[data-action='leave-user-team']",
+      function(e, data) {
       // Populate the modal heading & body
-      var modal = $("#modal-leave-user-team");
-      var modalHeading = modal.find(".modal-header").find(".modal-title");
-      var modalBody = modal.find(".modal-body");
-      modalHeading.html(data.heading);
-      modalBody.html(data.html);
+        var modal = $('#modal-leave-user-team');
+        var modalHeading = modal.find('.modal-header').find('.modal-title');
+        var modalBody = modal.find('.modal-body');
+        modalHeading.html(data.heading);
+        modalBody.html(data.html);
 
-      // Show the modal
-      modal.modal("show");
-    }
-  )
-  .on(
-    "ajax:error",
-    "[data-action='destroy-user-team']",
-    function (e, data, status, xhr) {
+        // Show the modal
+        modal.modal('show');
+      }
+    )
+    .on(
+      'ajax:error',
+      "[data-action='destroy-user-team']",
+      function() {
       // TODO
-    }
-  );
+      }
+    );
 
   // Also, bind the click action on the modal
-  $("#modal-leave-user-team")
-  .on("click", "[data-action='submit']", function() {
-    var btn = $(this);
-    var form = btn
-      .closest(".modal")
-      .find(".modal-body")
-      .find("form[data-id='leave-user-team-form']");
+  $('#modal-leave-user-team')
+    .on('click', "[data-action='submit']", function() {
+      var btn = $(this);
+      var form = btn
+        .closest('.modal')
+        .find('.modal-body')
+        .find("form[data-id='leave-user-team-form']");
 
-    // Simply submit the form!
-    form.submit();
-  });
+      // Simply submit the form!
+      form.submit();
+    });
 
   // Lastly, bind on the ajax form
   $(document)
-  .on(
-    "ajax:success",
-    "[data-id='leave-user-team-form']",
-    function (e, data, status, xhr) {
+    .on(
+      'ajax:success',
+      "[data-id='leave-user-team-form']",
+      function() {
       // Simply reload the page
-      location.reload();
-    }
-  )
-  .on(
-    "ajax:error",
-    "[data-id='destroy-user-team-form']",
-    function (e, data, status, xhr) {
+        location.reload();
+      }
+    )
+    .on(
+      'ajax:error',
+      "[data-id='destroy-user-team-form']",
+      function() {
       // TODO
-    }
-  );
+      }
+    );
 }
 
 initLeaveTeams();
