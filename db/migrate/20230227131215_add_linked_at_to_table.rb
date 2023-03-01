2133640f38f94e03d47698fed192c42ca51260d2
Andrej <andrej.jugovic7@gmail.com>
2023-03-01 10:40:16 +0100

    Add activity subject_type constrain for migration query [SCI-7964]

--- db/migrate/20230227131215_add_linked_at_to_table.rb
+++ db/migrate/20230227131215_add_linked_at_to_table.rb
@@ -9,7 +9,7 @@ class AddLinkedAtToTable < ActiveRecord::Migration[6.1]
        SET linked_at = act.activities_created_at
        FROM (SELECT MAX(activities.created_at) AS activities_created_at, MAX(activities.subject_id) AS subject_id
              FROM protocols
-             LEFT JOIN activities ON activities.subject_id = protocols.id
+             LEFT JOIN activities ON activities.subject_id = protocols.id AND activities.subject_type=\'Protocol\'
              WHERE protocols.protocol_type = 1 AND activities.type_of = 46
              GROUP BY protocols.id) as act
        WHERE id = act.subject_id'
