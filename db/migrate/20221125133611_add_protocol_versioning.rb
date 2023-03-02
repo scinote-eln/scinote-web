8917170507cb1a9240e20445008e9173f6d11422
Oleksii Kriuchykhin <oleksii@scinote.net>
2023-03-02 14:01:11 +0100

    Change migration of protocol templates - do not publish unlinked protocols [SCI-8062]

--- db/migrate/20221125133611_add_protocol_versioning.rb
+++ db/migrate/20221125133611_add_protocol_versioning.rb
@@ -14,25 +14,37 @@ class AddProtocolVersioning < ActiveRecord::Migration[6.1]
     end
 
     execute(
+      'UPDATE "protocols" SET "protocol_type" = 3, "archived" = TRUE WHERE "protocols"."protocol_type" = 4;'
+    )
+    execute(
+      'UPDATE "protocols" SET "visibility" = 1 WHERE "protocols"."protocol_type" = 3;'
+    )
+    execute(
+      'UPDATE "protocols" SET "protocol_type" = 2 ' \
+      'WHERE "id" IN (' \
+      'SELECT DISTINCT "protocols"."id" FROM "protocols" ' \
+      'JOIN "protocols" "linked_children" ON "linked_children"."parent_id" = "protocols"."id" ' \
+      'WHERE "protocols"."protocol_type" IN (2, 3)' \
+      ');'
+    )
+    execute(
+      'UPDATE "protocols" SET "protocol_type" = 3 ' \
+      'WHERE "id" IN (' \
+      'SELECT DISTINCT "protocols"."id" FROM "protocols" ' \
+      'LEFT OUTER JOIN "protocols" "linked_children" ON "linked_children"."parent_id" = "protocols"."id" ' \
+      'WHERE "protocols"."protocol_type" IN (2, 3) AND "linked_children"."id" IS NULL' \
+      ');'
+    )
+    execute(
       'UPDATE "protocols" SET "published_on" = "created_at", "published_by_id" = "added_by_id" ' \
-      'WHERE "protocols"."protocol_type" IN (2, 4) ' \
+      'WHERE "protocols"."protocol_type" = 2 ' \
       'AND "protocols"."published_on" IS NULL;'
-    )
-    execute(
-      'UPDATE "protocols" SET "published_by_id" = "added_by_id", "visibility" = 1 ' \
-      'WHERE "protocols"."protocol_type" = 3;'
-    )
-    execute(
-      'UPDATE "protocols" SET "archived" = TRUE WHERE "protocols"."protocol_type" = 4;'
-    )
-    execute(
-      'UPDATE "protocols" SET "protocol_type" = 2 WHERE "protocols"."protocol_type" IN (3, 4);'
     )
   end
 
   def down
     execute(
-      'UPDATE "protocols" SET "protocol_type" = 4 WHERE "protocols"."protocol_type" = 2 AND ' \
+      'UPDATE "protocols" SET "protocol_type" = 4 WHERE "protocols"."protocol_type" IN (2, 3, 4) AND ' \
       '"protocols"."archived" = TRUE;'
     )
     change_table :protocols, bulk: true do |t|
