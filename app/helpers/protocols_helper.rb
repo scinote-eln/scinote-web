248b13aa72e589cad05d2c73d56c03db16b68579
sboursen-scinote <soufiane@scinote.net>
2023-03-03 14:32:08 +0100

    Fix hound [SCI-8030]

--- app/helpers/protocols_helper.rb
+++ app/helpers/protocols_helper.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 module ProtocolsHelper
-  def templates_view_mode_archived?
-    @type == :archived || @protocol&.archived?
+  def templates_view_mode_archived?(type: nil, protocol: nil)
+    type == :archived || protocol&.archived?
   end
 end
