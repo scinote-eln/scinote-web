c814c93de04556b4a825cff5ae5a1c16eafa44e5
Oleksii Kriuchykhin <oleksii@scinote.net>
2023-03-15 15:57:40 +0100

    Improve special character escaping in data attributes [SCI-8137]

--- app/serializers/step_serializer.rb
+++ app/serializers/step_serializer.rb
@@ -5,6 +5,7 @@ class StepSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
   include ApplicationHelper
   include CommentHelper
+  include InputSanitizeHelper
 
   attributes :name, :position, :completed, :attachments_manageble, :urls, :assets_view_mode, :assets_order,
              :marvinjs_enabled, :bio_eddie_service_enabled, :bio_eddie_context, :marvinjs_context,
@@ -106,6 +107,6 @@ class StepSerializer < ActiveModel::Serializer
   end
 
   def created_by
-    object.user.full_name
+    escape_input(object.user.full_name)
   end
 end
