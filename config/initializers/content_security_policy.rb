4e8e8784c7c2e990140a7cf690445e51a7dcf473
sboursen-scinote <soufiane@scinote.net>
2023-03-20 09:01:54 +0100

    Create a draft CSP [SCI-8119]

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -3,26 +3,59 @@
 # Define an application-wide content security policy
 # For further information see the following documentation
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
+require 'uri'
+
+def valid_url?(url)
+  uri = URI.parse(url)
+  return false unless %w(http https ws wss).include?(uri.scheme)
+  return false if uri.host.nil? || uri.host.empty? || uri.host.start_with?('.')
+
+  true
+rescue URI::InvalidURIError
+  false
+end
+
+def wepacker_port
+  uri = URI.parse("http://#{Webpacker.dev_server.host_with_port}")
+  uri.port
+end
+
+def host_to_url(protocol, str, port)
+  "#{protocol}://#{str}:#{port}" if str.present? && protocol.present? && port.present?
+end
+
+hosts = Rails.application.config.hosts
 
 Rails.application.config.content_security_policy do |policy|
-#   policy.default_src :self, :https
-#   policy.font_src    :self, :https, :data
-#   policy.img_src     :self, :https, :data
-  policy.object_src :none
-#   policy.script_src  :self, :https
-#   policy.style_src   :self, :https
+  policy.default_src :self, :https
+  policy.font_src    :self, :https
+  policy.img_src     :self, :https
+  policy.object_src  :none
+  policy.script_src  :self, :https
+  policy.style_src   :self, :https, :unsafe_inline
 
-#   # Specify URI for violation reports
-#   # policy.report_uri "/csp-violation-report-endpoint"
+  if Rails.env.development?
+    hosts_urls = %w(ws http).flat_map do |protocol|
+      hosts.map { |host| host_to_url(protocol, host, wepacker_port)}
+    end.select { |url| valid_url?(url)}
+
+
+    policy.connect_src :self, :data, *hosts_urls
+  else
+    policy.connect_src :self, :data
+  end
+
+  # Specify URI for violation reports
+  # policy.report_uri "/csp-violation-report-endpoint"
 end
 
 # If you are using UJS then enable automatic nonce generation
 # Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
 
 # Set the nonce only to specific directives
-# Rails.application.config.content_security_policy_nonce_directives = %w(script-src)
+# Rails.application.config.content_security_policy_nonce_directives = %w(style-src)
 
 # Report CSP violations to a specified URI
 # For further information see the following documentation:
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
-# Rails.application.config.content_security_policy_report_only = true
+Rails.application.config.content_security_policy_report_only = true
