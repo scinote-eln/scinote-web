f79bb9fa5f6e4e13bc9258688089ec69ed6a5ecd
sboursen-scinote <soufiane@scinote.net>
2023-04-05 14:51:12 +0200

    Add small refactor [SCI-8193]

--- app/serializers/protocol_serializer.rb
+++ app/serializers/protocol_serializer.rb
@@ -25,7 +25,7 @@ class ProtocolSerializer < ActiveModel::Serializer
   end
 
   def published_on_formatted
-    return unless object.published_on.respond_to?(:strftime)
+    return unless object.published_on.present?
 
     I18n.l(object.published_on, format: :full)
   end
