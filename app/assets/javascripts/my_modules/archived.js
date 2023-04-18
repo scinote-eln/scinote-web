2fc7ad9429f27d504cbfde55b74549f7e78de049
sboursen-scinote <soufiane@scinote.net>
2023-04-18 15:47:58 +0200

    Implemet the Move task functionality for archived tasks cards view [SCI-8253]

--- app/assets/javascripts/my_modules/archived.js
+++ app/assets/javascripts/my_modules/archived.js
@@ -18,17 +18,61 @@
   $('#module-archive').on('click', '.task-selector', function() {
     let taskId = $(this).closest('.task-selector-container').data('task-id');
     let index = $.inArray(taskId, selectedTasks);
-    let resoteTasksButton = $('.restore-button-container');
+    const restoreTasksButton = $('.restore-button-container');
+    const moveTasksButton = $('.move-button-container')
 
     // If checkbox is checked and row ID is not in list of selected folder IDs
     if (this.checked && index === -1) {
       selectedTasks.push(taskId);
-      resoteTasksButton.collapse('show');
+      restoreTasksButton.collapse('show');
+      moveTasksButton.collapse('show');
       // Otherwise, if checkbox is not checked and ID is in list of selected IDs
     } else if (!this.checked && index !== -1) {
       selectedTasks.splice(index, 1);
     }
     // Hide button
-    if (selectedTasks.length === 0) resoteTasksButton.collapse('hide');
+    if (selectedTasks.length === 0) {
+      restoreTasksButton.collapse('hide');
+      moveTasksButton.collapse('hide');
+    }
   });
+
+  function initMoveButton() {
+    $('.move-button-container').on('click', 'button', function(e) {
+      const cardsContainer = $('#cards-container');
+      $.get(cardsContainer.data('move-modules-modal-url'), (modalData) => {
+        if ($('#modal-move-modules').length > 0) {
+          console.log('replaced');
+          $('#modal-move-modules').replaceWith(modalData.html);
+        } else {
+          $('#module-archive').append(modalData.html);
+        }
+
+        $('#modal-move-modules').on('shown.bs.modal', function() {
+          $(this).find('.selectpicker').selectpicker().focus();
+        });
+
+        $('#modal-move-modules').on('click', 'button[data-action="confirm"]', () => {
+          console.log('clicked!')
+          const moveParams = {
+            to_experiment_id: $('#modal-move-modules').find('.selectpicker').val(),
+            my_module_ids: selectedTasks
+          };
+          $.post(cardsContainer.data('move-modules-url'), moveParams, (data) => {
+            HelperModule.flashAlertMsg(data.message, 'success');
+            window.location.reload();
+          }).error((data) => {
+            HelperModule.flashAlertMsg(data.responseJSON.message, 'danger');
+          });
+          $('#modal-move-modules').modal('hide');
+        });
+
+        $('#modal-move-modules').modal('show');
+        });
+    });
+  }
+
+
+
+  initMoveButton();
 }());
