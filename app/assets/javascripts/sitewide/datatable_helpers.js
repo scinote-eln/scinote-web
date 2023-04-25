e9cacc3db1403d60c3c712997fdd1363af3eaa9b
G-Chubinidze <112488503+G-Chubinidze@users.noreply.github.com>
2023-04-25 16:21:41 +0400

    Open Inventory - Double action fix [SCI-7506] (#5200)
    Co-authored-by: Giga Chubinidze <gchubinidze@unisens.ge>

--- app/assets/javascripts/sitewide/datatable_helpers.js
+++ app/assets/javascripts/sitewide/datatable_helpers.js
@@ -109,7 +109,7 @@ DataTableCheckboxes.prototype.clearSelection = function() {
 DataTableCheckboxes.prototype.initCheckboxes = function() {
   this.tableWrapper.on('click', '.table tbody tr', (e) => {
     var checkbox = $(e.currentTarget).find(this.config.checkboxSelector);
-    if (checkbox.attr('disabled')) return;
+    if (checkbox.attr('disabled') || $(e.target).is('a')) return;
     checkbox.prop('checked', !checkbox.prop('checked'));
     this.selectRow(e.currentTarget);
   }).on('click', this.config.checkboxSelector, (e) => {
