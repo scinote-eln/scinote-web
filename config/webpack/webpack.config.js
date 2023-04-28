2319a26238114ab9c3f30d023cab3f5a94f1845d
Martin Artnik <martin@scinote.net>
2023-04-28 10:15:20 +0200

    Add missing entry point to webpack.config.js [SCI-8296]

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -32,7 +32,8 @@ const entryList = {
   vue_repository_filter: './app/javascript/packs/vue/repository_filter.js',
   vue_repository_print_modal: './app/javascript/packs/vue/repository_print_modal.js',
   vue_navigation_top_menu: './app/javascript/packs/vue/navigation/top_menu.js',
-  vue_navigation_navigator: './app/javascript/packs/vue/navigation/navigator.js'
+  vue_navigation_navigator: './app/javascript/packs/vue/navigation/navigator.js',
+  vue_components_action_toolbar: './app/javascript/packs/vue/action_toolbar.js'
 }
 
 // Engine pack loading based on https://github.com/rails/webpacker/issues/348#issuecomment-635480949
