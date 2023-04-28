2305fa91a37b1f3c1d365039a763bfc84257b75a
Anton <anton@scinote.net>
2023-04-28 10:53:40 +0200

    Fix colors for tailwind

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   # config.assets.js_compressor = Uglifier.new(harmony: true)
-  config.assets.css_compressor = nil
+  # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
