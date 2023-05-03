df7a098628dd53fd733c31ba1d0d8dbbd05d6ed0
sboursen-scinote <soufiane@scinote.net>
2023-05-03 09:18:42 +0200

    Fix hound [SCI-8119][SCI-8119]

--- config/initializers/security_policy.rb
+++ config/initializers/security_policy.rb
@@ -19,7 +19,7 @@ Rails.application.config.content_security_policy do |policy|
 end
 
 # If you are using UJS then enable automatic nonce generation
-Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
+Rails.application.config.content_security_policy_nonce_generator = ->_request { SecureRandom.base64(16) }
 
 # Set the nonce only to specific directives
 Rails.application.config.content_security_policy_nonce_directives = %w(script-src)
