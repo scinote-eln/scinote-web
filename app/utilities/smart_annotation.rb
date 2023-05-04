a337b98ed5e5ca2fec8aa09b32f91e285989f67a
Alex Kriuchykhin <oleksii@scinote.net>
2023-05-04 16:59:32 +0200

    Fix inclusion of InputSanitizeHelper in SmartAnnotation utility (#5365)

--- app/utilities/smart_annotation.rb
+++ app/utilities/smart_annotation.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class SmartAnnotation
-  include ActionView::Helpers::InputSanitizeHelper
+  include InputSanitizeHelper
   include ActionView::Helpers::TextHelper
 
   attr_writer :current_user, :current_team, :query
