1ade65a8e3bd14ac107f35d9164e3dea23a20637
Oleksii Kriuchykhin <oleksii@scinote.net>
2023-06-06 13:48:06 +0200

    Use correct permissions in repository row assigning filters [SCI-8616]

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -18,7 +18,8 @@ class ProjectsController < ApplicationController
                                      sidebar experiments_cards view_type actions_dropdown create_tag)
   before_action :load_current_folder, only: %i(index cards new show)
   before_action :check_view_permissions, except: %i(index cards new create edit update archive_group restore_group
-                                                    users_filter actions_dropdown project_filter actions_toolbar)
+                                                    users_filter actions_dropdown inventory_assigning_project_filter
+                                                    actions_toolbar)
   before_action :check_create_permissions, only: %i(new create)
   before_action :check_manage_permissions, only: :edit
   before_action :load_exp_sort_var, only: :show
@@ -105,14 +106,14 @@ class ProjectsController < ApplicationController
     }
   end
 
-  def project_filter
+  def inventory_assigning_project_filter
     readable_experiments = Experiment.readable_by_user(current_user)
-    managable_active_my_modules = MyModule.managable_by_user(current_user).active
+    assignable_my_modules = MyModule.repository_row_assignable_by_user(current_user)
 
     projects = Project.readable_by_user(current_user)
                       .joins(experiments: :my_modules)
                       .where(experiments: { id: readable_experiments })
-                      .where(my_modules: { id: managable_active_my_modules })
+                      .where(my_modules: { id: assignable_my_modules })
                       .search(current_user, false, params[:query], 1, current_team)
                       .distinct
                       .pluck(:id, :name)
