18214f749be067d73ec3c337aaef1a949d474b38
Martin Artnik <martin@scinote.net>
2023-06-06 10:28:24 +0200

    Allow blob image source [SCI-8617]

--- config/initializers/security_policy.rb
+++ config/initializers/security_policy.rb
@@ -8,7 +8,7 @@ Rails.application.config.content_security_policy do |policy|
   policy.default_src :self, :https
   policy.base_uri    :self
   policy.font_src    :self, :https, :data
-  policy.img_src     :self, :https, :data
+  policy.img_src     :self, :https, :data, :blob
   policy.object_src  :none
   policy.script_src  :self, :https, :unsafe_eval
   policy.style_src   :self, :https, :unsafe_inline, :data
