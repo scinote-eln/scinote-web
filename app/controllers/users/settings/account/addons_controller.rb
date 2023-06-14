88df4b2a1169a940a2a8f48069f71c52793e4e28
Giga Chubinidze <giga@scinote.net>
2023-06-14 05:13:33 +0400

    Settings page - breadcrumbs [SCI-8511]

--- app/controllers/users/settings/account/addons_controller.rb
+++ app/controllers/users/settings/account/addons_controller.rb
@@ -1,11 +1,24 @@
+# frozen_string_literal: true
+
 module Users
   module Settings
     module Account
       class AddonsController < ApplicationController
+        before_action :set_breadcrumbs_items, only: %i(index show)
         layout 'fluid'
 
         def index
           @label_printer_any = LabelPrinter.any?
+        end
+
+        private
+
+        def set_breadcrumbs_items
+          @breadcrumbs_items = []
+          @breadcrumbs_items.push({
+                                    label: t('breadcrumbs.addons'),
+                                    url: addons_path
+                                  })
         end
       end
     end
