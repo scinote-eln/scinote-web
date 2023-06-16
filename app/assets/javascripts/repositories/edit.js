2df9367b01e2b09629309dbf6a75e570e92df50c
Alex Kriuchykhin <oleksii@scinote.net>
2023-06-16 10:19:54 +0200

    Remove old JS code, unneeded JS include nonce [SCI-8670] (#5607)

--- app/assets/javascripts/repositories/edit.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  'use strict';
-
-  $('.delete-repo-option').initSubmitModal('#delete-repo-modal', 'repository');
-  $('.rename-repo-option').initSubmitModal('#rename-repo-modal', 'repository');
-  $('.share-repo-option').initSubmitModal('.share-repo-modal', 'repository');
-  $('.copy-repo-option').initSubmitModal('#copy-repo-modal', 'repository');
-})();
