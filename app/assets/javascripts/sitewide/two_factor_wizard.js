a5090efacfcfa7a47eea58c186905aca54589470
sboursen-scinote <soufiane@scinote.net>
2023-07-07 06:33:18 +0200

    Fix 2FA form submit [SCI-8779]
    Co-authored-by: Sboursen <dev.sboursen@gmail.com>

--- app/assets/javascripts/sitewide/two_factor_wizard.js
+++ app/assets/javascripts/sitewide/two_factor_wizard.js
@@ -17,15 +17,22 @@ $(document).on('turbolinks:load', function() {
     });
   });
 
-  $('#twoFactorAuthenticationModal .2fa-enable-form').on('ajax:error', function(e, data) {
-    $(this).find('.submit-code-field').addClass('error').attr('data-error-text', data.responseJSON.error);
-  }).on('ajax:success', function(e, data) {
-    var blob = new Blob([data.recovery_codes.join('\r\n')], { type: 'text/plain;charset=utf-8' });
-    $('#twoFactorAuthenticationModal').find('.recovery-codes').html(data.recovery_codes.join('<br>'));
-    $('#twoFactorAuthenticationModal').find('[href="#2fa-step-4"]').tab('show');
-    $('.download-recovery-codes').attr('href', window.URL.createObjectURL(blob));
-    $('#twoFactorAuthenticationModal').one('hide.bs.modal', function() {
-      window.location.reload();
+  $('#twoFactorAuthenticationModal .2fa-enable-form').on('submit', function(e) {
+    e.preventDefault();
+
+    const form = $(this);
+    const formData = $(this).serialize();
+    const formAction = $(this).attr('action');
+    $.post(formAction, formData).error(function(data) {
+      form.find('.submit-code-field').addClass('error').attr('data-error-text', data.responseJSON.error);
+    }).done(function(data) {
+      const blob = new Blob([data.recovery_codes.join('\r\n')], { type: 'text/plain;charset=utf-8' });
+      $('#twoFactorAuthenticationModal').find('.recovery-codes').html(data.recovery_codes.join('<br>'));
+      $('#twoFactorAuthenticationModal').find('[href="#2fa-step-4"]').tab('show');
+      $('.download-recovery-codes').attr('href', window.URL.createObjectURL(blob));
+      $('#twoFactorAuthenticationModal').one('hide.bs.modal', function() {
+        window.location.reload();
+      });
     });
   });
 
