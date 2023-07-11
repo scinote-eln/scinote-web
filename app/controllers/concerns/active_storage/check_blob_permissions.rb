defe4fdc30ab70e0f026c94598c12143a3c48f46
Oleksii Kriuchykhin <oleksii@scinote.net>
2023-07-11 13:32:09 +0200

    Fix displaying of tinyMCE images in search results [SCI-8823]

--- app/controllers/concerns/active_storage/check_blob_permissions.rb
+++ app/controllers/concerns/active_storage/check_blob_permissions.rb
@@ -55,16 +55,17 @@ module ActiveStorage
 
     def check_tinymce_asset_read_permissions(asset)
       return render_403 unless asset
-      return true if asset.object.nil? && asset.team == current_team
+
+      current_user.permission_team = asset.team || current_team
 
       case asset.object_type
       when 'MyModule'
-        render_403 unless can_read_experiment?(asset.object.experiment)
+        render_403 unless can_read_my_module?(asset.object)
       when 'Protocol'
         render_403 unless can_read_protocol_in_module?(asset.object) ||
                           can_read_protocol_in_repository?(asset.object)
       when 'ResultText'
-        render_403 unless can_read_experiment?(asset.object.result.my_module.experiment)
+        render_403 unless can_read_my_module?(asset.object.result.my_module)
       when 'StepText'
         render_403 unless can_read_protocol_in_module?(asset.object.step.protocol) ||
                           can_read_protocol_in_repository?(asset.object.step.protocol)
