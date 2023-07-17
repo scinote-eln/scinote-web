48b84cc0b9079bc5b4149fb11e18f9882341b9b1
Andrej <andrej.jugovic7@gmail.com>
2023-07-17 11:47:03 +0200

    Add position to step elements API [SCI-8834]

--- app/serializers/api/v1/step_text_serializer.rb
+++ app/serializers/api/v1/step_text_serializer.rb
@@ -4,12 +4,16 @@ module Api
   module V1
     class StepTextSerializer < ActiveModel::Serializer
       type :step_texts
-      attributes :id, :text
+      attributes :id, :text, :position
 
       include TimestampableModel
 
       def contents
         object.text&.force_encoding(Encoding::UTF_8)
+      end
+
+      def position
+        object.step_orderable_element.position
       end
     end
   end
