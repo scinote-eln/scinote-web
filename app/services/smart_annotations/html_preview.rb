6300c1bef8174803fe31cdc9a4f50d3c405aa80c
ivanscinote <138504771+ivanscinote@users.noreply.github.com>
2023-07-17 16:20:49 +0200

    Fix smart annotations multiline display [SCI-8816] (#5790)

--- app/services/smart_annotations/html_preview.rb
+++ app/services/smart_annotations/html_preview.rb
@@ -26,9 +26,8 @@ module SmartAnnotations
           return "<span class='sa-type'>Exp</span>#{object.name} #{I18n.t('atwho.res.archived')}"
         end
 
-        "<a class='sa-link' href='#{ROUTES.my_modules_experiment_path(object)}'>
-          <span class='sa-type'>Exp</span>#{object.name}
-        </a>"
+        "<a class='sa-link' href='#{ROUTES.my_modules_experiment_path(object)}'>" \
+          "<span class='sa-type'>Exp</span>#{object.name}</a>"
       end
 
       def generate_tsk_snippet(_, object)
