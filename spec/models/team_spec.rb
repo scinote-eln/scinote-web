d191e4bbb6e7d0a18b47a6fbecc25d4b5dd2b162
sboursen-scinote <soufiane@scinote.net>
2023-07-25 21:12:54 +0200

    Implements tests for Shareable_links related Models [SCI-8755]

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -21,12 +21,12 @@ describe Team, type: :model do
     it { should have_db_column :last_modified_by_id }
     it { should have_db_column :description }
     it { should have_db_column :space_taken }
+    it { should have_db_column :shareable_links_enabled }
   end
 
   describe 'Relations' do
     it { should belong_to(:created_by).class_name('User').optional }
     it { should belong_to(:last_modified_by).class_name('User').optional }
-    it { should have_many :user_teams }
     it { should have_many :users }
     it { should have_many :projects }
     it { should have_many :protocols }
@@ -36,6 +36,7 @@ describe Team, type: :model do
     it { should have_many :reports }
     it { should have_many(:team_shared_objects).dependent(:destroy) }
     it { should have_many :shared_repositories }
+    it { should have_many(:shareable_links).dependent(:destroy) }
   end
 
   describe 'Validations' do
@@ -47,6 +48,44 @@ describe Team, type: :model do
     it do
       should validate_length_of(:description)
         .is_at_most(Constants::TEXT_MAX_LENGTH)
+    end
+  end
+
+  describe 'Callbacks' do
+    it 'does not triggers destroy_all on shareable_links before save when shareable_links_enabled? is false' do
+      allow(team).to receive(:shareable_links_enabled?).and_return(true)
+      expect(team.shareable_links).not_to receive(:destroy_all)
+      team.save
+    end
+
+    context 'when shareable_links_enabled is true' do
+      it 'does not destroy shareable_links before saving' do
+        team.shareable_links_enabled = true
+        create_list(:shareable_link, 3, team: team)
+        team.save
+
+        expect(team.shareable_links.count).to eq(3)
+      end
+    end
+
+    context 'when shareable_links_enabled is false' do
+      it 'triggers destroy_all on shareable_links before save when shareable_links_enabled? is false' do
+        allow(team).to receive(:shareable_links_enabled?).and_return(false)
+        expect(team.shareable_links).to receive(:destroy_all)
+        team.save
+      end
+
+      it 'destroys all shareable_links before saving' do
+        team.shareable_links_enabled = true
+        create_list(:shareable_link, 3, team: team)
+        team.save
+        team.shareable_links_enabled = false
+        create_list(:shareable_link, 3, team: team)
+
+        team.save
+
+        expect(team.shareable_links.count).to eq(0)
+      end
     end
   end
 end
