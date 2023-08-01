a0372d14bf0e8b41cd371ff46d173052d1d2bddc
wandji20 <wbertrand10@gmail.com>
2023-08-01 18:59:58 +0100

    Improve base controller timestamp filter method [SCI-8738]

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -248,11 +248,11 @@ module Api
       def timestamps_filter(records)
         from = params.dig(:created_at, :from).present? ? Date.parse(params.dig(:created_at, :from)) : nil
         to = params.dig(:created_at, :to).present? ? Date.parse(params.dig(:created_at, :to)) : nil
-        return records.where(created_at: (from..to)) if from && to
+        records = records.where(created_at: (from..to)) if from || to
 
         from = params.dig(:updated_at, :from).present? ? Date.parse(params.dig(:updated_at, :from)) : nil
         to = params.dig(:updated_at, :to).present? ? Date.parse(params.dig(:updated_at, :to)) : nil
-        return records.where(updated_at: (from..to)) if from && to
+        records = records.where(updated_at: (from..to)) if from || to
 
         records
       end
