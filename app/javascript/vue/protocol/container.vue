7c07befd3e308ceed7eb99b5622589033d5556bb
wandji20 <wbertrand10@gmail.com>
2023-08-11 17:47:02 +0100

    Fix CSS issues in protocols, and quick start(new task) [SCI-9018]

--- app/javascript/vue/protocol/container.vue
+++ app/javascript/vue/protocol/container.vue
@@ -36,7 +36,7 @@
       </div>
     </div>
     <div id="protocol-content" class="protocol-content collapse in" aria-expanded="true">
-      <div>
+      <div class="ml-[2.375rem] mb-8">
         <div class="protocol-name" v-if="!inRepository">
           <InlineEdit
             v-if="urls.update_protocol_name_url"
@@ -67,7 +67,7 @@
             </div>
           </div>
           <div id="protocol-description-container" :class=" inRepository ? 'protocol-description collapse in' : ''" >
-            <div v-if="urls.update_protocol_description_url">
+            <div class="ml-[0.25rem]" v-if="urls.update_protocol_description_url">
               <Tinymce
                 :value="protocol.attributes.description"
                 :value_html="protocol.attributes.description_view"
