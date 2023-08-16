25e6cbdbd8ba2b1236652fdd2f59c9fe97046265
wandji20 <wbertrand10@gmail.com>
2023-08-16 17:08:25 +0100

    Move inline JavaScript inclusion for new and edit result table to result_tables.js [SCI-9057]

--- app/assets/javascripts/results/result_tables/new.js
+++
@@ -1,7 +0,0 @@
-/* global Results */
-
-(function() {
-  $('.new-result-tables-buttons').on('click', '.save-result', (event) => {
-    Results.processResult(event, Results.ResultTypeEnum.TABLE);
-  });
-}());
