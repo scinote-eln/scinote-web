9e2291dc30be08aa8e4e62bd1b0770e47bb7da2e
Oleksii Kriuchykhin <oleksii@scinote.net>
2023-08-17 16:51:46 +0200

    Implement fetching of endpoint URLs from OpenID config URL in Azure AD strategy [SCI-9041]

--- lib/omniauth/strategies/custom_azure_active_directory.rb
+++ lib/omniauth/strategies/custom_azure_active_directory.rb
@@ -7,6 +7,22 @@ module OmniAuth
 
       option :name, 'customazureactivedirectory'
 
+      def client
+        omni_client = super
+        begin
+          app_conf =
+            Rails.cache.fetch("ad_app_conf_#{options[:client_id]}", expires_in: 1.day) do
+              JSON.parse(Net::HTTP.get(URI(options[:conf_url])))
+            end
+          omni_client.options[:authorize_url] = app_conf['authorization_endpoint']
+          omni_client.options[:token_url] = app_conf['token_endpoint']
+        rescue StandardError => e
+          Rails.logger.error('Failed to load OAuth2 configuration from the remote server! Using defaults.')
+          Rails.logger.error(e.message)
+        end
+        omni_client
+      end
+
       def raw_info
         if @raw_info.nil?
           id_token_data   = ::JWT.decode(access_token.params['id_token'], nil, false).first rescue {}
