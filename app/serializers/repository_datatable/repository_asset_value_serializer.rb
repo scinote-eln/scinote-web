5312ddbff680fb5a724fad6e49d0d0b59a8a1cde
Martin Artnik <martin@scinote.net>
2023-08-21 15:59:17 +0200

    Fix issues with repository columns [SCI-8994][SCI-8846]

--- app/serializers/repository_datatable/repository_asset_value_serializer.rb
+++ app/serializers/repository_datatable/repository_asset_value_serializer.rb
@@ -4,6 +4,7 @@ module RepositoryDatatable
   class RepositoryAssetValueSerializer < RepositoryBaseValueSerializer
     include Rails.application.routes.url_helpers
     include InputSanitizeHelper
+    include FileIconsHelper
 
     def value
       asset = value_object.asset
@@ -12,7 +13,7 @@ module RepositoryDatatable
         url: rails_blob_path(asset.file, disposition: 'attachment'),
         preview_url: asset_file_preview_path(asset),
         file_name: escape_input(asset.file_name),
-        icon_html: FileIconsHelper.sn_icon_for(asset)
+        icon_html: sn_icon_for(asset)
       }
     rescue StandardError => e
       Rails.logger.error e.message
