f964a7461da373c16f128cf5568a79bab42d20c7
Anton <anton@scinote.net>
2023-08-23 14:02:11 +0200

    Move status block to secondary navigation [SCI-9027]

--- app/assets/javascripts/my_modules/status_flow.js
+++ app/assets/javascripts/my_modules/status_flow.js
@@ -13,4 +13,52 @@
       $modalBody.html(result.html);
     });
   });
+
+  function checkStatusState() {
+    $.getJSON($('.status-flow-dropdown').data('status-check-url'), (statusData) => {
+      if (statusData.status_changing) {
+        setTimeout(() => { checkStatusState(); }, STATUS_POLLING_INTERVAL);
+      } else {
+        location.reload();
+      }
+    });
+  }
+
+  function applyTaskStatusChangeCallBack() {
+    if ($('.status-flow-dropdown').data('status-changing')) {
+      setTimeout(() => { checkStatusState(); }, STATUS_POLLING_INTERVAL);
+    }
+    $('.task-sharing-and-flows').on('click', '#dropdownTaskFlowList > li[data-state-id]', function() {
+      var list = $('#dropdownTaskFlowList');
+      var item = $(this);
+      animateSpinner();
+      $.ajax({
+        url: list.data('link-url'),
+        beforeSend: function(e, ajaxSettings) {
+          if (item.data('beforeSend') instanceof Function) {
+            return item.data('beforeSend')(item, ajaxSettings)
+          }
+          return true
+        },
+        type: 'PATCH',
+        data: { my_module: { status_id: item.data('state-id') } },
+        success: function(result) {
+          animateSpinner(null, false);
+          location.reload();
+        },
+        error: function(e) {
+          animateSpinner(null, false);
+          if (e.status === 403) {
+            HelperModule.flashAlertMsg(I18n.t('my_module_statuses.update_status.error.no_permission'), 'danger');
+          } else if (e.status === 422) {
+            HelperModule.flashAlertMsg(e.responseJSON.errors, 'danger');
+          } else {
+            HelperModule.flashAlertMsg('error', 'danger');
+          }
+        }
+      });
+    });
+  }
+
+  applyTaskStatusChangeCallBack();
 }());
