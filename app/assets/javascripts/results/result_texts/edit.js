165b7febcfc26f8c5f251e4f3aa8e8250fecd0a5
Alex Kriuchykhin <oleksii@scinote.net>
2023-08-28 14:53:09 +0200

    Fix JS errors on text results creation/editing [SCI-9140] (#6086)

--- app/assets/javascripts/results/result_texts/edit.js
+++
@@ -1,7 +0,0 @@
-/* global Results */
-
-(function() {
-  $('.edit-result-texts-buttons').on('click', '.save-result', (event) => {
-    Results.processResult(event, Results.ResultTypeEnum.TEXT);
-  });
-}());
