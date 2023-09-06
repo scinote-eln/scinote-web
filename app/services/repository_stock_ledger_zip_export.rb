51715cd5a4545acf82de2c8d9998c95dd73e9917
wandji20 <wbertrand10@gmail.com>
2023-09-06 09:27:03 +0100

    Update consumption report generation query [SCI-9023]

--- app/services/repository_stock_ledger_zip_export.rb
+++ app/services/repository_stock_ledger_zip_export.rb
@@ -46,65 +46,72 @@ module RepositoryStockLedgerZipExport
 
   def self.generate_data(row_ids)
     data = []
-    repository_rows =
-      RepositoryRow.where(id: row_ids)
-                   .joins(repository_stock_value: [{ repository_ledger_records: :user }])
-                   .joins('LEFT OUTER JOIN repositories ON repository_ledger_records.reference_id = repositories.id')
-                   .joins('LEFT OUTER JOIN my_module_repository_rows ON
-                     repository_ledger_records.reference_id = my_module_repository_rows.id')
-                   .joins('LEFT OUTER JOIN my_modules ON my_modules.id = my_module_repository_rows.my_module_id')
-                   .joins('LEFT OUTER JOIN experiments ON experiments.id = my_modules.experiment_id')
-                   .joins('LEFT OUTER JOIN projects ON projects.id = experiments.project_id')
-                   .joins('LEFT OUTER JOIN teams ON teams.id = projects.team_id')
-                   .order('repository_rows.created_at, record_created_at')
-                   .select('repository_rows.*, repository_ledger_records.created_at AS record_created_at,
-                    repository_ledger_records.reference_type AS record_reference_type,
-                    repository_ledger_records.amount AS record_amount,
-                    repository_ledger_records.balance AS record_balance,
-                    repository_ledger_records.unit AS record_unit,
-                    users.full_name AS full_name, users.settings AS user_settings,
-                    my_modules.id AS module_code, my_modules.name AS module_name,
-                    projects.name AS project_name, teams.name AS team_name,
-                    experiments.name AS experiment_name')
+    repository_ledger_records =
+      RepositoryLedgerRecord.joins([{ repository_stock_value: { repository_cell: :repository_row } }, :user])
+                            .where("repository_rows.id IN (#{row_ids.split.join(',')})")
+                            .joins('LEFT OUTER JOIN repositories ON
+                              repository_ledger_records.reference_id = repositories.id')
+                            .joins('LEFT OUTER JOIN my_module_repository_rows ON
+                              repository_ledger_records.reference_id = my_module_repository_rows.id')
+                            .joins('LEFT OUTER JOIN my_modules ON
+                              my_modules.id = my_module_repository_rows.my_module_id')
+                            .joins('LEFT OUTER JOIN experiments ON experiments.id = my_modules.experiment_id')
+                            .joins('LEFT OUTER JOIN projects ON projects.id = experiments.project_id')
+                            .joins('LEFT OUTER JOIN teams ON teams.id = projects.team_id')
+                            .order('row_created_at, repository_ledger_records.created_at')
+                            .select('repository_ledger_records.*,
+                              repository_rows.created_at AS row_created_at,
+                              repository_rows.id AS repository_id,
+                              repository_rows.name AS repository_name,
+                              users.full_name AS full_name, users.settings AS user_settings,
+                              my_modules.id AS module_id, my_modules.name AS module_name,
+                              projects.name AS project_name, teams.name AS team_name,
+                              experiments.name AS experiment_name')
 
     previous_balance = nil
     previous_row_id = nil
     # rubocop:disable Metrics/BlockLength
-    repository_rows.find_each do |row|
-      action_type =
-        previous_row_id == row.id && previous_balance && row.record_balance < previous_balance ? 'reduced' : 'added'
-      consumption_type = row.record_reference_type == 'MyModuleRepositoryRow' ? 'Task' : 'Inventory'
+    repository_ledger_records.each do |record|
+      action_type = if previous_row_id == record.repository_id\
+                        && previous_balance\
+                        && record.balance < previous_balance
+                      'reduced'
+                    else
+                      'added'
+                    end
+
+      consumption_type = record.reference_type == 'MyModuleRepositoryRow' ? 'Task' : 'Inventory'
 
       if action_type == 'reduced'
-        consumed_amount = row.record_amount.abs
-        consumed_amount_unit = row.record_unit
+        consumed_amount = record.amount.abs
+        consumed_amount_unit = record.unit
       else
-        added_amount = row.record_amount
-        added_amount_unit = row.record_unit
+        added_amount = record.amount
+        added_amount_unit = record.unit
       end
 
       row_data = [
         consumption_type,
-        row.name,
-        row.code,
+        record.repository_name,
+        "IT#{record.repository_id}",
         consumed_amount,
         consumed_amount_unit,
         added_amount,
         added_amount_unit,
-        row.full_name,
-        row.record_created_at.strftime(JSON.parse(row.user_settings)['date_format']),
-        row.team_name,
-        row.project_name,
-        row.experiment_name,
-        row.module_name,
-        "TA#{row.module_code}",
-        row.record_balance,
-        row.record_unit
+        record.full_name,
+        record.created_at.strftime(JSON.parse(record.user_settings)['date_format']),
+        record.team_name,
+        record.project_name,
+        record.experiment_name,
+        record.module_name,
+        "TA#{record.module_id}",
+        record.unit,
+        record.balance
       ]
 
       data << row_data
-      previous_balance = row.record_balance
-      previous_row_id = row.id
+      previous_balance = record.balance
+      previous_row_id = record.repository_id
     end
     # rubocop:enable Metrics/BlockLength
     data
