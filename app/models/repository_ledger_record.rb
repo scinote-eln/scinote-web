e071242359643bf07a43f5a3303528fcfeae8c77
wandji20 <wbertrand10@gmail.com>
2023-09-07 12:57:46 +0100

    Improve repository_ledger_record and repository_row relation [SCI-9023]

--- app/models/repository_ledger_record.rb
+++ app/models/repository_ledger_record.rb
@@ -6,4 +6,5 @@ class RepositoryLedgerRecord < ApplicationRecord
   belongs_to :repository_stock_value
   belongs_to :reference, polymorphic: true
   belongs_to :user
+  has_one :repository_row, through: :repository_stock_value
 end
