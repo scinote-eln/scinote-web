eb35c7960fd95c53e9c59796de536722917654d1
wandji20 <wbertrand10@gmail.com>
2023-09-08 15:04:55 +0100

    Add OVE yarn package and load assets from node_modules [SCI-9231]

--- app/javascript/vue/ove/OpenVectorEditor.vue
+++ app/javascript/vue/ove/OpenVectorEditor.vue
@@ -56,6 +56,7 @@
         readOnly: this.readOnly,
         showMenuBar: true,
         alwaysAllowSave: true,
+        menuFilter: this.menuFilter,
         ToolBarProps: {
           toolList: [
             'saveTool',
@@ -132,6 +133,27 @@
         if (parent !== window) {
           parent.document.getElementById('iFrameModal').dispatchEvent(new Event('hide'));
         }
+      },
+      menuFilter(menus) {
+       return menus.map(menu => {
+        if(menu.text !== 'Help') return menu;
+
+        const menuOverride = {
+            text: "Help",
+            submenu: [
+              { isMenuSearch: true },
+              "--",
+              { text: "About", onClick: () => { return } },
+              {
+                cmd: "versionNumber",
+                shouldDismissPopover: true,
+                onClick: () => { return }
+              },
+              "hotkeyDialog"
+            ]
+          };
+          return menuOverride;
+       });
       }
     }
   }
