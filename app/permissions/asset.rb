346a84a68aeedf7cff35ef5019cdaf230b7ecc07
wandji20 <wbertrand10@gmail.com>
2023-09-13 14:46:36 +0100

    Decouple asset module lockstate from asset permission helper [SCI-9110]

--- app/permissions/asset.rb
+++ app/permissions/asset.rb
@@ -18,7 +18,7 @@ Canaid::Permissions.register_for(Asset) do
   can :manage_asset do |user, asset|
     object = asset.step || asset.result || asset.repository_cell
 
-    has_permission = case object
+    case object
     when Step
       can_manage_step?(user, object)
     when Result
@@ -29,12 +29,6 @@ Canaid::Permissions.register_for(Asset) do
       else
         can_manage_repository_assets?(user, object.repository_column.repository)
       end
-    end
-
-    if asset.blob&.metadata&.dig('asset_type') == 'gene_sequence'
-      has_permission && !asset.my_module_locked?
-    else
-      has_permission
     end
   end
 end
