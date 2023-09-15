9f672ecfe29967694c2f7cfbc0cf1616c0493283
Andrej <andrej.jugovic7@gmail.com>
2023-09-15 16:07:10 +0200

    Move OVE trial period handling [SCI-9301]

--- app/services/open_vector_editor_service.rb
+++ app/services/open_vector_editor_service.rb
@@ -3,21 +3,7 @@
 class OpenVectorEditorService
   class << self
     def enabled?
-      ove_enabled_until = ENV.fetch('SCINOTE_OVE_ENABLED_UNTIL', nil)
-
-      return false if ove_enabled_until.blank?
-
-      DateTime.now.utc.to_date < DateTime.strptime(ove_enabled_until, '%d-%m-%Y').utc.to_date
-    end
-
-    def evaluation_period_left
-      return 0 unless enabled?
-
-      ove_enabled_until = ENV.fetch('SCINOTE_OVE_ENABLED_UNTIL', nil)
-
-      return 0 if ove_enabled_until.blank?
-
-      (DateTime.strptime(ove_enabled_until, '%d-%m-%Y').utc.to_date - DateTime.now.utc.to_date).to_i
+      false
     end
   end
 end
