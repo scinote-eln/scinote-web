1bf2bc615633a36e478c65aeea0a9a41bce87017
Gregor Lasnibat <gregor@scinote.net>
2023-09-19 14:38:17 +0200

    Refactoring RepositoryItemSidebar and custom columns [SCI-9153]
    Fixed regular card (without cusotm columns) styles [SCI-9153]
    
    Updated API to return column name, working on subcomponents [SCI-9153]
    
    TooltipPreview on dynamic file component working [SCI-9153]
    
    Finished new item card refactor [SCI-9153]
    
    PR fix 01 [SCI-9153]
    
    PR fixes 02 [SCI-9153]

---
+++ app/javascript/vue/repository_item_sidebar/repository_values/RepositoryAssetValue.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="repository-asset-value-wrapper" class="flex flex-col min-min-h-[46px] h-auto gap-[6px]">
+    <div class="font-inter text-sm font-semibold leading-5">
+      {{ colName }}
+    </div>
+    <div v-if="file_name" @mouseover="tooltipShowing = true" @mouseout="tooltipShowing = false"
+      class="w-fit cursor-pointer text-sn-science-blue">
+      {{ file_name }}
+      <tooltip-preview v-if="tooltipShowing" :url="url" :file_name="file_name" :preview_url="preview_url"
+        :icon_html="icon_html"></tooltip-preview>
+    </div>
+    <div v-else class="text-sn-dark-grey font-inter text-sm font-normal leading-5">
+      {{ i18n.t('repositories.item_card.repository_asset_value.no_asset') }}
+    </div>
+  </div>
+</template>
+
+<script>
+import TooltipPreview from './TooltipPreview.vue'
+
+export default {
+  name: 'RepositoryAssetvalue',
+  components: {
+    "tooltip-preview": TooltipPreview
+  },
+  data() {
+    return {
+      tooltipShowing: false,
+      id: null,
+      url: null,
+      preview_url: null,
+      file_name: null,
+      icon_html: null
+    }
+  },
+  props: {
+    data_type: String,
+    colId: Number,
+    colName: String,
+    colVal: Object
+  },
+  created() {
+    this.id = this.colVal.id
+    this.url = this.colVal.url
+    this.preview_url = this.colVal.preview_url
+    this.file_name = this.colVal.file_name
+    this.icon_html = this.colVal.icon_html
+  }
+}
+</script>
