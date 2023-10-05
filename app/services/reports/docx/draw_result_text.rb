d3c2a7e0ad2b5809f4834a627b936bf107a1da39
wandji20 <wbertrand10@gmail.com>
2023-10-05 12:25:08 +0100

    Fix issue with timestamps for reports [SCI-9420]

--- app/services/reports/docx/draw_result_text.rb
+++ app/services/reports/docx/draw_result_text.rb
@@ -2,14 +2,17 @@
 
 module Reports::Docx::DrawResultText
   def draw_result_text(element)
-    result = element.result
     result_text = element.orderable
+    timestamp = element.created_at
     color = @color
-    @docx.p result_text.name.presence || '', italic: true
+    @docx.p do
+      text result_text.name.presence || '', italic: true
+      text ' '
+      text I18n.t('projects.reports.elements.result_text.user_time',
+                  timestamp: I18n.l(timestamp, format: :full)), color: color[:gray]
+    end
     html = custom_auto_link(result_text.text, team: @report_team)
     Reports::HtmlToWordConverter.new(@docx, { scinote_url: @scinote_url,
                                               link_style: @link_style }).html_to_word_converter(html)
-
-    draw_result_comments(result) if @settings.dig('task', 'result_comments')
   end
 end
