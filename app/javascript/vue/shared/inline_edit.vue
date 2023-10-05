2c3d5c4fd50e2052049ddd20dc352bace298b4ac
Giga Chubinidze <giga@scinote.net>
2023-10-05 13:01:34 +0400

    changed styling to tw class / optimized inline editing height

--- app/javascript/vue/shared/inline_edit.vue
+++ app/javascript/vue/shared/inline_edit.vue
@@ -239,6 +239,7 @@
         if (this.editing && !this.singleLine) {
           this.$nextTick(() => {
             if (!this.$refs.input) return;
+            this.$refs.input.style.height = this.$refs.input.scrollHeight / 2  + 'px';
 
             this.$refs.input.style.height = this.$refs.input.scrollHeight  + 'px';
           });
