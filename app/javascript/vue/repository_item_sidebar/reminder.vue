0f56ea990b3b46c4d005f44dac95449a002b5de1
wandji20 <wbertrand10@gmail.com>
2023-10-18 18:15:51 +0100

    Use default browser tooltip for shoing tooltips in items card [SCI-9537]

--- app/javascript/vue/repository_item_sidebar/reminder.vue
+++ app/javascript/vue/repository_item_sidebar/reminder.vue
@@ -1,5 +1,5 @@
 <template v-if="value.reminder === true">
-  <div class="inline-block float-right cursor-pointer relative" data-placement="top" data-toggle="tooltip" :title="value.text"
+  <div class="inline-block float-right cursor-pointer relative" :title="reminderTitle"
      tabindex='-1'>
     <i class="sn-icon sn-icon-notifications row-reminders-icon"></i>
     <span :class="`inline-block absolute rounded-full w-2 h-2 right-1 top-0.5 ${reminderColor}`"></span>
@@ -11,14 +11,20 @@
     name: 'Reminder',
     props: {
       valueType: null,
-      value: null,
+      value: null
     },
     computed: {
       reminderColor() {
         if (this.value.reminder && (this.value.stock_amount > 0 || this.value.days_left > 0)) {
-          return 'bg-sn-alert-brittlebush'
+          return 'bg-sn-alert-brittlebush';
         }
         return 'bg-sn-alert-passion';
+      },
+      reminderTitle() {
+        let title = this.value.reminder_text
+        if (this.value.reminder_message) title = `${title}\n${this.value.reminder_message}`;
+  
+        return title;
       }
     }
   }
