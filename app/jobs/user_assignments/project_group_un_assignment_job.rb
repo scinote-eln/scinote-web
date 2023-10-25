059af4b892c7ee8fbc33f6abb9478449a83f46ed
Giga Chubinidze <giga@scinote.net>
2023-10-25 10:08:27 +0400

    changed manual assignment

--- app/jobs/user_assignments/project_group_un_assignment_job.rb
+++ app/jobs/user_assignments/project_group_un_assignment_job.rb
@@ -11,6 +11,7 @@ module UserAssignments
 
         remove_users_from_my_modules(project, users)
         remove_users_from_experiments(project, users)
+        remove_users_from_project(project, users)
         user_assignments.destroy_all
       end
     end
@@ -31,6 +32,28 @@ module UserAssignments
       UserAssignment.where(assignable_type: 'MyModule', assignable_id: my_modules, user: users).delete_all
       my_modules.update_all(updated_at: Time.current)
       # rubocop:enable Rails/SkipsModelValidations
+    end
+
+    def remove_users_from_project(project, users)
+      users.each do |user|
+        user_assignment = project.user_assignments.find_by(user: user)
+
+        if user_assignment.last_with_permission?(ProjectPermissions::USERS_MANAGE)
+          raise ActiveRecord::RecordInvalid
+        end
+
+        propagate_job(project, user_assignment, destroy: true)
+      end
+    end
+
+    def propagate_job(project, user_assignment, destroy: false)
+      UserAssignments::PropagateAssignmentJob.perform_later(
+        project,
+        user_assignment.user.id,
+        user_assignment.user_role,
+        user_assignment.assigned_by_id,
+        destroy: destroy
+      )
     end
   end
 end
