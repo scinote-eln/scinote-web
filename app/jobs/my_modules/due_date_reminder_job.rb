5f35b4510c9d08541142d1fe4de6a22277fca9c1
Giga Chubinidze <giga@scinote.net>
2023-11-21 10:59:16 +0400

    added MyModules module

--- app/jobs/my_modules/due_date_reminder_job.rb
+++ app/jobs/my_modules/due_date_reminder_job.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
-class DueDateReminderJob < ApplicationJob
-  def perform
-    my_modules = MyModule.approaching_due_dates
+module MyModules
+  class DueDateReminderJob < ApplicationJob
+    def perform
+      my_modules = MyModule.approaching_due_dates
 
-    my_modules.each do |task|
-      TaskDueDateNotification.send_notifications({ my_module_id: task.id })
+      my_modules.each do |task|
+        TaskDueDateNotification.send_notifications({ my_module_id: task.id })
+      end
     end
   end
 end
