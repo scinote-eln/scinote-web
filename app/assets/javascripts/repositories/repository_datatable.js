0b2c8546c03b024e16bfddf5c614172b1f47fc34
Martin Artnik <martin@scinote.net>
2023-11-22 13:49:29 +0100

    Add proper e2e ids to inventories and inventory items [SCI-9624]

--- app/assets/javascripts/repositories/repository_datatable.js
+++ app/assets/javascripts/repositories/repository_datatable.js
@@ -667,7 +667,7 @@ var RepositoryDatatable = (function(global) {
         visible: true,
         render: function(data, type, row) {
           return "<a href='" + row.recordInfoUrl + "'"
-                 + "class='record-info-link' data-e2e='e2e-TL-invInventory-Item-1'>" + data + '</a>';
+                 + "class='record-info-link' data-e2e='e2e-TL-invInventory-Item-" + row.id + "'>" + data + '</a>';
         }
       }, {
         // Added on column
