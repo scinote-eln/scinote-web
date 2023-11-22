09310f654625f79eac7415468429120dc3f6191a
Soufiane <soufiane@scinote.net>
2023-11-22 11:05:52 +0100

    Fix scrolling edge case for the ScrollSpy component [SCI-9690] (#6661)
    - Clicking on navigator item should highlight the item when the window is zoomed out or zoomed in (not 100%).
    - Clicking on navigator item should scroll to section when window is zoomed in.

--- app/javascript/vue/repository_item_sidebar/repository_values/ScrollSpy.vue
+++ app/javascript/vue/repository_item_sidebar/repository_values/ScrollSpy.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="flex gap-3">
     <div id="navigation-text"
-      class="flex flex-col py-2 px-0 gap-3 self-stretch w-[130px] h-[130px] justify-center items-center">
+         v-if="thresholds.length"
+         class="flex flex-col py-2 px-0 gap-3 self-stretch w-[130px] h-[130px] justify-center items-center">
       <div v-for="(navigationItem, index) in itemsToCreate" :key="navigationItem.textId"
         @click="navigateToSection(navigationItem)"
         class="text-sn-grey nav-text-item flex flex-col w-[130px] h-[130px] justify-between text-right hover:cursor-pointer"
-        :class="{ 'text-sn-science-blue': (arrOfThresholds[index]?.from <= scrollPosition && scrollPosition <= arrOfThresholds[index]?.to) }">
+        :class="{ 'text-sn-science-blue': navigationItemsStatus[index] }">
         {{ i18n.t(`repositories.highlight_component.${navigationItem.labelAlias}`) }}
       </div>
     </div>
@@ -13,7 +14,7 @@
     <div id="highlight-container" class="w-[1px] h-[130px] flex flex-col justify-evenly bg-sn-light-grey">
       <div v-for="(navigationItem, index) in itemsToCreate" :key="navigationItem.id"
         class="w-[5px] h-[28px] rounded-[11px]"
-        :class="{ 'bg-sn-science-blue relative left-[-2px]': (arrOfThresholds[index]?.from <= scrollPosition && scrollPosition <= arrOfThresholds[index]?.to) }">
+        :class="{ 'bg-sn-science-blue relative left-[-2px]': navigationItemsStatus[index] }">
       </div>
     </div>
   </div>
@@ -33,22 +34,35 @@ export default {
       sections: [],
       sectionsWithHeight: [],
       allSectionsCumulativeHeight: null,
-      arrOfThresholds: [],
+      thresholds: [],
+      navigationItemsStatus: [], // highlighted or not
       scrollPosition: null,
       centerOfScrollThumb: null,
     };
   },
 
   mounted() {
+    console.log('mounted');
+    window.addEventListener('resize', this.handleResize);
     this.initializeComponent();
     this.$nextTick(() => {
       this.calculateAllSectionsCumulativeHeight()
       this.calculateSectionsHeight();
       this.constructThresholds()
+      this.handleScroll()
+
+      if (!this.initialSectionId) {
+        this.navigateToSection(this.itemsToCreate[0])
+      }
+      else {
+        const itemToNavigateTo = this.itemsToCreate.find((item) => item.sectionId === this.initialSectionId)
+        this.navigateToSection(itemToNavigateTo)
+      }
     });
   },
 
   beforeDestroy() {
+    window.removeEventListener('resize', this.handleResize);
     this.removeScrollListener();
   },
 
@@ -58,6 +72,8 @@ export default {
       const scrollSpyContentEl = document.getElementById('scrollSpyContent')
       this.bodyContainerEl = bodyWrapperEl
       this.sections = Array.from(scrollSpyContentEl.querySelectorAll('section[id]'));
+      this.navigationItemsStatus = Array(this.sections.length).fill(false);
+      this.navigationItemsStatus[0] = true;
       this.addScrollListener();
     },
 
@@ -120,26 +136,26 @@ export default {
           const to = Math.round(deltaTravel * this.sectionsWithHeight[i].percentHeight / 100) + prevThreshold
           const id = this.sectionsWithHeight[i].sectionId
           prevThreshold = to + 1
-          const thresholdObj = {
+          const threshold = {
             id,
             index: i,
             from,
             to
           }
-          this.arrOfThresholds[i] = thresholdObj
+          this.thresholds[i] = threshold
         }
         // last section
         else if (i === this.sectionsWithHeight.length - 1) {
           const from = prevThreshold
           const to = scrollableArea.scrollHeight
           const id = this.sectionsWithHeight[i].sectionId
-          const thresholdObj = {
+          const threshold = {
             id,
             index: i,
             from,
             to
           }
-          this.arrOfThresholds[i] = thresholdObj
+          this.thresholds[i] = threshold
         }
         else {
           // other sections
@@ -147,13 +163,13 @@ export default {
           const to = Math.round(deltaTravel * this.sectionsWithHeight[i].percentHeight / 100) + prevThreshold - 1
           const id = this.sectionsWithHeight[i].sectionId
           prevThreshold = to + 1
-          const thresholdObj = {
+          const threshold = {
             id,
             index: i,
             from,
             to
           }
-          this.arrOfThresholds[i] = thresholdObj
+          this.thresholds[i] = threshold
         }
       }
     },
@@ -162,25 +178,27 @@ export default {
     handleScroll() {
       const scrollableArea = this.bodyContainerEl;
       const scrollThumbsDistanceFromTop = scrollableArea.scrollTop + this.centerOfScrollThumb;
-      this.scrollPosition = scrollThumbsDistanceFromTop
+      this.scrollPosition = scrollThumbsDistanceFromTop;
+      this.updateNavigationItemsStatusOnScroll();
     },
 
     // Navigating (scrolling into view) via click
     navigateToSection(navigationItem) {
       if (!this.bodyContainerEl) return;
+      this.removeScrollListener();
 
       const scrollableArea = this.bodyContainerEl;
-      const foundObj = this.arrOfThresholds.find((obj) => obj.id === navigationItem.sectionId)
+      const foundThreshold = this.thresholds.find((obj) => obj.id === navigationItem.sectionId)
       const domElToScrollTo = document.getElementById(navigationItem.label)
 
-      if (foundObj.index === 0) {
+      if (foundThreshold.index === 0) {
         // scroll to top
         this.bodyContainerEl.scrollTo({
           top: 0,
           behavior: "auto"
         });
       }
-      else if (foundObj.index === this.arrOfThresholds.length - 1) {
+      else if (foundThreshold.index === this.thresholds.length - 1) {
         // scroll to bottom
         this.bodyContainerEl.scrollTo({
           top: 99999,
@@ -189,9 +207,12 @@ export default {
       }
       else {
         // scroll to the start of a section's threshold, adjusted for the center thumb value (true center)
-        scrollableArea.scrollTop = foundObj.from - this.centerOfScrollThumb
+        scrollableArea.scrollTop = foundThreshold.from - this.centerOfScrollThumb
       }
       this.flashTitleColor(domElToScrollTo);
+
+      this.updateNavigationItemsStatusOnClick(this.itemsToCreate.indexOf(navigationItem) || 0);
+      setTimeout(() => this.addScrollListener(), 1500);
     },
 
     flashTitleColor(domEl) {
@@ -199,6 +220,34 @@ export default {
 
       domEl.classList.add('text-sn-science-blue');
       setTimeout(() => domEl.classList.remove('text-sn-science-blue'), 300);
+    },
+
+    handleResize() {
+      this.$nextTick(() => {
+        this.calculateAllSectionsCumulativeHeight()
+        this.calculateSectionsHeight();
+        this.constructThresholds()
+      });
+    },
+
+    updateNavigationItemsStatusOnScroll() {
+      this.thresholds.forEach((threshold, index) => {
+        this.navigationItemsStatus[index] = false;
+
+        if (threshold?.from <= this.scrollPosition && this.scrollPosition <= threshold?.to) {
+          this.navigationItemsStatus[index] = true;
+        }
+      });
+    },
+
+    updateNavigationItemsStatusOnClick(itemIndex) {
+      this.thresholds.forEach((_, index) => {
+        this.navigationItemsStatus[index] = false;
+
+        if (index === itemIndex) {
+          this.navigationItemsStatus[index] = true;
+        }
+      });
     },
   }
 }
