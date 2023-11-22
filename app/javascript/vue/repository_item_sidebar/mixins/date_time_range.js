0e8bd2b06fb65879eae8f7437519cfb5c08015a8
sboursen-scinote <soufiane@scinote.net>
2023-11-22 15:04:36 +0100

    Switch to using Vue3 date-picker in item card [SCI-9741]

--- app/javascript/vue/repository_item_sidebar/mixins/date_time_range.js
+++ app/javascript/vue/repository_item_sidebar/mixins/date_time_range.js
@@ -11,46 +11,61 @@ export default {
       return 'border-sn-light-grey hover:border-sn-sleepy-grey';
     },
     isEditable() {
-      return this.isEditing && this.editingField === this.dateType
-    }
+      return this.isEditing && this.editingField === this.dateType;
+    },
   },
   methods: {
     enableEdit() {
-      this.isEditing = true
-      this.$emit('setEditingField', this.dateType)
+      this.isEditing = true;
+      this.$emit('setEditingField', this.dateType);
     },
     saveChange() {
-      if (!this.isEditing ||
-          this.isSaving ||
-          !this.params ||
-          (this.params && !Object.keys(this.params).includes(this.colId?.toString()))) {
-        Object.assign(this.$data, { isEditing: false, isSaving: false, errorMessage: null });
+      if (
+        !this.isEditing
+        || this.isSaving
+        || !this.params
+        || (this.params && !Object.keys(this.params).includes(this.colId?.toString()))
+      ) {
+        Object.assign(this.$data, {
+          isEditing: false,
+          isSaving: false,
+          errorMessage: null,
+        });
         return;
-      };
+      }
 
       // Don't submit unless values changed
       switch (true) {
         case ['date', 'dateTime', 'time'].includes(this.dateType):
-          if(this.equalDates(this.params[this.colId], this.initDate)) {
+          if (this.equalDates(this.params[this.colId], this.initDate)) {
             Object.assign(this.$data, { isEditing: false, isSaving: false });
             return;
           }
-          if(this.dateType === 'time' && this.equalTimes(this.params[this.colId], this.initDate)) {
+          if (
+            this.dateType === 'time'
+            && this.equalTimes(this.params[this.colId], this.initDate)
+          ) {
             Object.assign(this.$data, { isEditing: false, isSaving: false });
             return;
           }
+          break;
         case ['dateRange', 'dateTimeRange', 'timeRange'].includes(this.dateType):
-          if (this.equalDates(this.timeFrom?.datetime, this.initStartDate) &&
-              this.equalDates(this.timeTo?.datetime, this.initEndDate)) {
+          if (
+            this.equalDates(this.timeFrom?.datetime, this.initStartDate)
+            && this.equalDates(this.timeTo?.datetime, this.initEndDate)
+          ) {
             Object.assign(this.$data, { isEditing: false, isSaving: false });
             return;
           }
-          if (this.dateType === 'timeRange' &&
-              this.equalTimes(this.timeFrom?.datetime, this.initStartDate) &&
-              this.equalTimes(this.timeTo?.datetime, this.initEndDate)) {
+          if (
+            this.dateType === 'timeRange'
+            && this.equalTimes(this.timeFrom?.datetime, this.initStartDate)
+            && this.equalTimes(this.timeTo?.datetime, this.initEndDate)
+          ) {
             Object.assign(this.$data, { isEditing: false, isSaving: false });
             return;
           }
+          break;
         default:
           break;
       }
@@ -65,28 +80,34 @@ export default {
           const cellValue = result?.value;
           switch (true) {
             case ['date', 'dateTime', 'time'].includes(this.dateType):
-              this.values = cellValue
-              this.initDate = cellValue?.datetime
+              this.values = cellValue;
+              this.initDate = cellValue?.datetime;
+              break;
             case ['dateRange', 'dateTimeRange', 'timeRange'].includes(this.dateType):
               this.initStartDate = cellValue?.start_time?.datetime;
               this.initEndDate = cellValue?.end_time?.datetime;
+              break;
             default:
               break;
           }
-          Object.assign($this.$data, { isEditing: false, isSaving: false, values: result?.value });
+          Object.assign($this.$data, {
+            isEditing: false,
+            isSaving: false,
+            values: result?.value,
+          });
           if ($('.dataTable')[0]) $('.dataTable').DataTable().ajax.reload(null, false);
-        }
+        },
       });
     },
     setParams() {
-      let defaultParams = this.params ? this.params : { [this.colId]: {} };
+      const defaultParams = this.params ? this.params : { [this.colId]: {} };
       switch (true) {
         case ['date', 'dateTime', 'time'].includes(this.dateType):
           defaultParams[this.colId] = this.values?.datetime;
           break;
         case ['dateRange', 'dateTimeRange', 'timeRange'].includes(this.dateType):
-          defaultParams[this.colId]['start_time'] = this.timeFrom?.datetime;
-          defaultParams[this.colId]['end_time'] = this.timeTo?.datetime;
+          defaultParams[this.colId].start_time = this.timeFrom?.datetime;
+          defaultParams[this.colId].end_time = this.timeTo?.datetime;
           break;
         default:
           break;
@@ -94,21 +115,23 @@ export default {
       this.params = defaultParams;
     },
     formatDateTime(date, field = null) {
-      this.values = this.values ? this.values : { [this.colId]: {} }
-      let params = this.params && this.params[this.colId] ? this.params : { [this.colId]: {} };
-      let timeFrom = this.timeFrom || {};
-      let timeTo = this.timeTo || {};
+      this.values = this.values ? this.values : { [this.colId]: {} };
+      const params = this.params && this.params[this.colId]
+        ? this.params
+        : { [this.colId]: {} };
+      const timeFrom = this.timeFrom || {};
+      const timeTo = this.timeTo || {};
       switch (true) {
         case ['date', 'dateTime', 'time'].includes(this.dateType):
           params[this.colId] = date;
-          this.values['datetime'] = date;
+          this.values.datetime = date;
           break;
         case ['dateRange', 'dateTimeRange', 'timeRange'].includes(this.dateType):
           if (field === 'start_time') {
-            timeFrom['datetime'] = date;
+            timeFrom.datetime = date;
           }
-          if (field === 'end_time'){
-            timeTo['datetime'] = date;
+          if (field === 'end_time') {
+            timeTo.datetime = date;
           }
           params[this.colId][field] = date;
           break;
@@ -121,22 +144,30 @@ export default {
     },
     dateValue(date) {
       const typesThatCantBeEmpty = ['dateRange', 'dateTimeRange'];
-      if (date && (date.currentTarget === null) && typesThatCantBeEmpty.includes(this.dateType)) {
+      if (
+        date
+        && date.currentTarget === null
+        && typesThatCantBeEmpty.includes(this.dateType)
+      ) {
         this.errorMessage = I18n.t('repositories.item_card.date_time.errors.select_valid_value');
-        return;
+        return null;
       }
-      if(date) return new Date(date)
-      return new Date()
+      if (date) return new Date(date);
+      return null;
     },
-    hasMonthText(){
-      $('body').data('datetime-picker-format')?.match(/MMM/)
+    hasMonthText() {
+      $('body')
+        .data('datetime-picker-format')
+        ?.match(/MMM/);
     },
-    equalDates(date1, date2){
+    equalDates(date1, date2) {
       return new Date(date1).getTime() === new Date(date2).getTime();
     },
-    equalTimes(date1, date2){
-      return new Date(date1).getHours() === new Date(date2).getHours() &&
-              new Date(date1).getMinutes() == new Date(date2).getMinutes()
+    equalTimes(date1, date2) {
+      return (
+        new Date(date1).getHours() === new Date(date2).getHours()
+        && new Date(date1).getMinutes() === new Date(date2).getMinutes()
+      );
     },
     validateAndSave() {
       this.errorMessage = null;
@@ -145,28 +176,38 @@ export default {
           // To do
           break;
         case ['dateRange', 'dateTimeRange', 'timeRange'].includes(this.dateType):
-          if(this.params && this.params[this.colId]) {
-            if((!this.params[this.colId].start_time && this.params[this.colId].end_time) ||
-              (this.params[this.colId].start_time && !this.params[this.colId].end_time)) {
-              this.errorMessage = I18n.t('repositories.item_card.date_time.errors.not_valid_range');
-              return;
-            }
-            if (this.params[this.colId].start_time && this.params[this.colId].end_time &&
-                this.params[this.colId].start_time.getTime() && this.params[this.colId].end_time.getTime()) {
-              if(this.params[this.colId].start_time.getTime() > this.params[this.colId].end_time.getTime()) {
-                this.errorMessage = I18n.t('repositories.item_card.date_time.errors.not_valid_range');
-                return
-              }
-            }
-            if (!this.params[this.colId].start_time && !this.params[this.colId].end_time) {
-              this.params = { [this.colId]: null }
-            }
+          if (!this.params?.[this.colId]) return;
+
+          // start_time and end_time must be both present or both absent
+          if (
+            !!this.params[this.colId]?.start_time?.getTime?.()
+            ^ !!this.params[this.colId]?.end_time?.getTime?.()
+          ) {
+            this.errorMessage = I18n.t('repositories.item_card.date_time.errors.not_valid_range');
+            return;
+          }
+
+          // start_time should always be lesser then end_time
+          if (
+            this.params[this.colId]?.start_time?.getTime?.()
+            && this.params[this.colId].start_time.getTime() > this.params[this.colId].end_time.getTime()
+          ) {
+            this.errorMessage = I18n.t('repositories.item_card.date_time.errors.not_valid_range');
+            return;
+          }
+
+          // start_time and end_time both are absent
+          if (
+            !this.params[this.colId]?.start_time
+            && !this.params[this.colId]?.end_time
+          ) {
+            this.params = { [this.colId]: null };
           }
           break;
         default:
           break;
       }
       this.saveChange();
-    }
+    },
   },
 };
