9e9f8ab402b239d405fe7edffcb6df370e5045eb
Alex Kriuchykhin <oleksii@scinote.net>
2023-11-24 14:21:11 +0100

    Add external_id field to repository list and checklist items [SCI-9454] (#6702)

---
+++ db/migrate/20231107163821_add_external_id_to_repository_column_items.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require File.expand_path('app/helpers/database_helper')
+
+class AddExternalIdToRepositoryColumnItems < ActiveRecord::Migration[6.1]
+  include DatabaseHelper
+
+  def up
+    add_column :repository_list_items, :external_id, :string, null: true
+    add_column :repository_checklist_items, :external_id, :string, null: true
+    add_index :repository_list_items,
+              :external_id,
+              unique: true,
+              name: 'unique_index_repository_list_items_on_external_id'
+    add_index :repository_checklist_items,
+              :external_id,
+              unique: true,
+              name: 'unique_index_repository_checklist_items_on_external_id'
+    add_gin_index_without_tags(:repository_list_items, :external_id)
+    add_gin_index_without_tags(:repository_checklist_items, :external_id)
+  end
+
+  def down
+    remove_index :repository_rows, name: 'index_repository_checklist_items_on_external_id'
+    remove_index :repository_rows, name: 'index_repository_list_items_on_external_id'
+    remove_index :repository_checklist_items,
+                 :external_id,
+                 name: 'unique_index_repository_checklist_items_on_external_id'
+    remove_index :repository_list_items,
+                 :external_id,
+                 name: 'unique_index_repository_list_items_on_external_id'
+    remove_column :repository_checklist_items, :external_id, :string, null: true
+    remove_column :repository_list_items, :external_id, :string, null: true
+  end
+end
