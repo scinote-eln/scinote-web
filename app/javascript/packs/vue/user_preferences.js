2dce55541d8480e3b1e0cb469647b55af3508ce7
Martin Artnik <martin@scinote.net>
2023-11-24 09:48:21 +0100

    Update user preferences with to Vue3

--- app/javascript/packs/vue/user_preferences.js
+++ app/javascript/packs/vue/user_preferences.js
@@ -1,13 +1,10 @@
-import TurbolinksAdapter from 'vue-turbolinks';
-import Vue from 'vue/dist/vue.esm';
-import PerfectScrollbar from 'vue2-perfect-scrollbar';
+import PerfectScrollbar from 'vue3-perfect-scrollbar';
+import { createApp } from 'vue/dist/vue.esm-bundler.js';
 import UserPreferences from '../../vue/user_preferences/container.vue';
+import { mountWithTurbolinks } from './helpers/turbolinks.js';
 
-Vue.use(TurbolinksAdapter);
-Vue.use(PerfectScrollbar);
-Vue.prototype.i18n = window.I18n;
-
-new Vue({
-  el: '#user_preferences',
-  components: { UserPreferences },
-});
+const app = createApp({});
+app.component('UserPreferences', UserPreferences);
+app.use(PerfectScrollbar);
+app.config.globalProperties.i18n = window.I18n;
+mountWithTurbolinks(app, '#user_preferences');
