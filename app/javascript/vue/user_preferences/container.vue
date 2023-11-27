b85284c54854d2edc1edfa6109099974965dd000
Giga Chubinidze <giga@scinote.net>
2023-11-27 17:14:50 +0400

    fix after conflict fix

--- app/javascript/vue/user_preferences/container.vue
+++ app/javascript/vue/user_preferences/container.vue
@@ -51,8 +51,8 @@
               <td class="p-2.5 text-base w-32">{{ i18n.t('notifications.email') }}</td>
             </tr>
           </div>
-          <template v-for="(_notifications, subGroup, i) in notificationsGroups[group]" :key="subGroup" v-if="subGroup !== 'always_on'">
-            <tr
+          <template v-for="(_notifications, subGroup, i) in notificationsGroups[group]" :key="subGroup">
+            <tr v-if="subGroup !== 'always_on'"
                 class="text-base border-transparent border-b-sn-super-light-grey border-solid"
                 :class="{'border-t-sn-super-light-grey': i == 0}"
             >
