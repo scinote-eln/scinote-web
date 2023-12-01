a5f711aa460dd0d01b25503b88115d3eefc41813
Ivan Kljun <ivan@scinote.net>
2023-12-01 15:00:30 +0100

    Add job for RepositoryDateValue, fix translation [SCI-5961]

--- app/notifications/repository_item_date_notification.rb
+++ app/notifications/repository_item_date_notification.rb
@@ -2,9 +2,12 @@
 
 class RepositoryItemDateNotification < BaseNotification
   def message
+    unit = human_readable_unit(column.metadata['reminder_unit'], column.metadata['reminder_value'])
     I18n.t(
       'notifications.notification.item_date_reminder_html',
-      repository_row_name: subject.name
+      repository_row_name: subject.name,
+      value: column.metadata['reminder_value'],
+      units: unit
     )
   end
 
@@ -18,7 +21,31 @@ class RepositoryItemDateNotification < BaseNotification
     RepositoryRow.find(params[:repository_row_id])
   end
 
+  def column
+    RepositoryColumn.find(params[:repository_column_id])
+  end
+
   after_deliver do
-    RepositoryDateTimeValue.find(params[:date_time_value_id]).update(notification_sent: true)
+    if params[:repository_date_time_value_id]
+      RepositoryDateTimeValue.find(params[:repository_date_time_value_id]).update(notification_sent: true)
+    elsif params[:repository_date_value_id]
+      RepositoryDateVal7ue.find(params[:repository_date_value_id]).update(notification_sent: true)
+    end
+  end
+
+  private
+
+  def human_readable_unit(seconds, value)
+    units_hash = {
+      '2419200' => 'month',
+      '604800' => 'week',
+      '86400' => 'day'
+    }
+
+    base_unit = units_hash.fetch(seconds) do
+      raise ArgumentError, "Unrecognized time unit for seconds value: #{seconds}"
+    end
+
+    value.to_i > 1 ? base_unit.pluralize : base_unit
   end
 end
