788f01aeae3a892d8ffa9c97cebcbf297e68028f
Martin Artnik <martin@scinote.net>
2023-12-13 13:30:23 +0100

    Fix due date reminder spam for locked tasks [SCI-9885]

--- app/jobs/my_modules/due_date_reminder_job.rb
+++ app/jobs/my_modules/due_date_reminder_job.rb
@@ -3,7 +3,7 @@
 module MyModules
   class DueDateReminderJob < ApplicationJob
     def perform
-      my_modules = MyModule.approaching_due_dates
+      my_modules = MyModule.uncomplete.approaching_due_dates
 
       my_modules.each do |task|
         TaskDueDateNotification.send_notifications({ my_module_id: task.id })
