6cddf46e1de53c9db410a09ce135c70fb0b994b1
Oleksii Kriuchykhin <oleksii@scinote.net>
2023-12-18 14:38:51 +0100

    Fix imports of protocols with tables from Protocols.io [SCI-9723]

--- app/services/protocol_importers/import_protocol_service.rb
+++ app/services/protocol_importers/import_protocol_service.rb
@@ -14,6 +14,7 @@ module ProtocolImporters
       @errors = {}
     end
 
+    # rubocop:disable Metrics/BlockLength
     def call
       return self unless valid?
 
@@ -26,7 +27,7 @@ module ProtocolImporters
           step_params.symbolize_keys!
 
           # Create step with nested attributes for tables
-          step = @protocol.steps.create!(step_params.slice(:name, :position, :tables_attributes)
+          step = @protocol.steps.create!(step_params.slice(:name, :position)
                                   .merge(user: @user, completed: false)
                                   .merge(last_modified_by_id: @user.id))
 
@@ -36,8 +37,26 @@ module ProtocolImporters
             position: 0,
             orderable: step_text
           )
-
           TinyMceAsset.update_images(step_text, '[]', @user)
+
+          # Add tables
+          if step_params[:tables_attributes].present?
+            step_params[:tables_attributes].each do |table_attributes|
+              table_attributes.symbolize_keys!
+              table = step.tables.create!(
+                name: table_attributes[:name],
+                contents: JSON.parse(table_attributes[:contents].encode('UTF-8', 'UTF-8')).to_json,
+                metadata: JSON.parse(table_attributes[:metadata].presence || '{}'),
+                created_by: @user,
+                last_modified_by: @user,
+                team: @team
+              )
+              step.step_orderable_elements.create!(
+                position: step.step_orderable_elements.size,
+                orderable: table.step_table
+              )
+            end
+          end
 
           # 'Manually' create assets here. "Accept nested attributes" won't work for assets
           step.assets << AttachmentsBuilder.generate(step_params.deep_symbolize_keys, user: @user, team: @team)
@@ -45,12 +64,17 @@ module ProtocolImporters
         end
       rescue ActiveRecord::RecordInvalid => e
         @errors[:protocol] = e.record.errors
+        raise ActiveRecord::Rollback
       rescue StandardError => e
+        Rails.logger.error(e.message)
+        Rails.logger.error(e.backtrace.join("\n"))
         @errors[:protocol] = e.message
+        raise ActiveRecord::Rollback
       end
 
       self
     end
+    # rubocop:enable Metrics/BlockLength
 
     def succeed?
       @errors.none?
