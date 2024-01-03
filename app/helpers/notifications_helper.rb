5e97cf9554e2170108fd4c92e1f4815d11853e4e
Giga Chubinidze <giga@scinote.net>
2024-01-04 03:31:46 +0400

    added subject instead of message

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -12,26 +12,24 @@ module NotificationsHelper
                      unassigned_user: target_user.name,
                      team: team.name,
                      unassigned_by_user: user.name)
-      message = team.name
-
       if role
         title = I18n.t('notifications.assign_user_to_team',
                        assigned_user: target_user.name,
                        role: role,
                        team: team.name,
                        assigned_by_user: user.name)
-        message = ActionController::Base.helpers.sanitize(
-          "<a href=#{projects_path(team: team)} target='_blank'>#{team.name}</a>"
-        )
-
       end
     end
 
-    GeneralNotification.send_notifications({
-                                             type: role ? :invite_user_to_team : :remove_user_from_team,
-                                             title: sanitize_input(title),
-                                             message: message,
-                                             user: target_user
-                                           })
+    GeneralNotification.send_notifications(
+      {
+        type: role ? :invite_user_to_team : :remove_user_from_team,
+        title: sanitize_input(title),
+        subject_id: team.id,
+        subject_class: team.class.name,
+        subject_name: team.respond_to?(:name) && team.name,
+        user: target_user
+      }
+    )
   end
 end
