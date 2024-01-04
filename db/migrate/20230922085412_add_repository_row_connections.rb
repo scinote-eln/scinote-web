0fd7c3e506c1148e00fc69895f27f78faf5c9564
Oleksii Kriuchykhin <oleksii@scinote.net>
2024-01-04 17:36:00 +0100

    test

--- db/migrate/20230922085412_add_repository_row_connections.rb
+++ db/migrate/20230922085412_add_repository_row_connections.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class AddRepositoryRowConnections < ActiveRecord::Migration[7.0]
-  def change
+  def up
     create_table :repository_row_connections do |t|
       t.references :parent, index: true, foreign_key: { to_table: :repository_rows }
       t.references :child, index: true, foreign_key: { to_table: :repository_rows }
@@ -11,11 +11,24 @@ class AddRepositoryRowConnections < ActiveRecord::Migration[7.0]
       t.timestamps
     end
 
-    add_index :repository_row_connections, %i(parent_id child_id), unique: true
-
     change_table :repository_rows, bulk: true do |t|
       t.integer :parent_connections_count
       t.integer :child_connections_count
     end
+
+    add_index :repository_row_connections,
+              'LEAST(parent_id, child_id), GREATEST(parent_id, child_id)',
+              name: 'index_repository_row_connections_on_connection_pair',
+              unique: true
+    add_check_constraint :repository_row_connections, 'parent_id != child_id',
+                         name: 'constraint_repository_row_connections_on_self_connection'
+  end
+
+  def down
+    remove_check_constraint :repository_row_connections,
+                            name: 'constraint_repository_row_connections_on_self_connection'
+    remove_index :repository_row_connections, 'index_repository_row_connections_on_connection_pair'
+    remove_columns :repository_rows, :parent_connections_count, :child_connections_count
+    drop_table :repository_row_connections
   end
 end
