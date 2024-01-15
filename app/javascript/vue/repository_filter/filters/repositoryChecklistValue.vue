db0c14974669e965a1ee4edf26c1b16d9f45fc04
mvrazic <144342580+mvrazic@users.noreply.github.com>
2024-01-15 16:34:58 +0100

    Update repositoryChecklistValue.vue

--- app/javascript/vue/repository_filter/filters/repositoryChecklistValue.vue
+++ app/javascript/vue/repository_filter/filters/repositoryChecklistValue.vue
@@ -6,7 +6,7 @@
         :options="this.operators"
         :selectedValue="this.operator"
         :selectorId="`OperatorSelector${this.filter.id}`"
-        :data-e2e="`e2e-DD-invInventoryFilterCO-option${this.filter.column.name.replace(/[\s-]+/g, '')}`"
+        :data-e2e="`e2e-DD-invInventoryFilterCO-option${this.filter.column.id}`"
         @dropdown:changed="updateOperator"
       />
     </div>
@@ -22,7 +22,7 @@
       :dataSelectMultipleAllSelected="this.i18n.t('repositories.show.repository_filter.filters.types.RepositoryChecklistValue.all_selected')"
       :selectorId="`ChecklistSelector${this.filter.id}`"
       :placeholder="this.i18n.t('repositories.show.repository_filter.filters.types.RepositoryChecklistValue.select_placeholder', {name: this.filter.column.name})"
-      :data-e2e="`e2e-DC-invInventoryFilterCO-input${this.filter.column.name.replace(/[\s-]+/g, '')}`"
+      :data-e2e="`e2e-DC-invInventoryFilterCO-input${this.filter.column.id}`"
       @dropdown:changed="updateValue"
     />
   </div>
