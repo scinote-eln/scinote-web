c23db0ecd8f80130478acb6cc942760bcd17c0f5
Oleksii Kriuchykhin <oleksii@scinote.net>
2024-01-18 14:44:11 +0100

    Add support of server side session store [SCI-9960]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_15_114821) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_18_094253) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gist"
   enable_extension "pg_trgm"
@@ -979,6 +979,15 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_15_114821) do
     t.index ["my_module_id"], name: "index_results_on_my_module_id"
     t.index ["restored_by_id"], name: "index_results_on_restored_by_id"
     t.index ["user_id"], name: "index_results_on_user_id"
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.string "session_id", null: false
+    t.jsonb "data"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
+    t.index ["updated_at"], name: "index_sessions_on_updated_at"
   end
 
   create_table "settings", force: :cascade do |t|
