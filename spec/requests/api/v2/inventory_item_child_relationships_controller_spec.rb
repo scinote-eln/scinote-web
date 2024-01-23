bdc96633b00a16ef40f3b22847fff4aca76a2ca7
Oleksii Kriuchykhin <oleksii@scinote.net>
2024-01-23 17:23:16 +0100

    Split inventory item relationships API V2 endpoint into separate endpoints for parents and children [SCI-10078]

--- spec/requests/api/v2/inventory_item_child_relationships_controller_spec.rb
+++ spec/requests/api/v2/inventory_item_child_relationships_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Api::V2::InventoryItemRelationshipsController', type: :request do
+RSpec.describe 'Api::V2::InventoryItemChildRelationshipsController', type: :request do
   before :all do
     @user = create(:user)
     @normal_user = create(:user, full_name: 'Normal User')
@@ -8,6 +8,8 @@ RSpec.describe 'Api::V2::InventoryItemRelationshipsController', type: :request d
     @inventory = create(:repository, team: @team, created_by: @user)
     @other_inventory = create(:repository, team: @team, created_by: @user)
     @inventory_item = create(:repository_row, repository: @inventory, created_by: @user)
+    @child_inventory_item = create(:repository_row, repository: @other_inventory, created_by: @user)
+    @child_connection = create(:repository_row_connection, parent: @inventory_item, child: @child_inventory_item, created_by: @user)
     @other_inventory_item = create(:repository_row, repository: @other_inventory, created_by: @user)
 
     @valid_headers = {
@@ -16,9 +18,56 @@ RSpec.describe 'Api::V2::InventoryItemRelationshipsController', type: :request d
                       }
   end
 
+  describe 'GET #index' do
+    it 'Response with correct inventory child relationship items' do
+      hash_body = nil
+      get api_v2_team_inventory_item_child_relationships_path(
+        team_id: @team.id,
+        inventory_id: @inventory.id,
+        item_id: @inventory_item.id,
+        include: :child
+      ), headers: @valid_headers
+      expect { hash_body = json }.not_to raise_exception
+      expect(hash_body[:data]).to match_array(
+        JSON.parse(
+          ActiveModelSerializers::SerializableResource
+            .new(@inventory_item.child_connections.order(:id),
+                 each_serializer: Api::V2::InventoryItemRelationshipSerializer,
+                 include: :child)
+            .to_json
+        )['data']
+      )
+      expect(hash_body).to include('included')
+    end
+  end
+
+  describe 'GET #show' do
+    it 'Response with correct inventory child relationship item' do
+      hash_body = nil
+      get api_v2_team_inventory_item_child_relationship_path(
+        team_id: @team.id,
+        inventory_id: @inventory.id,
+        item_id: @inventory_item.id,
+        id: @child_connection.id,
+        include: :child
+      ), headers: @valid_headers
+      expect { hash_body = json }.not_to raise_exception
+      expect(hash_body[:data]).to match_array(
+        JSON.parse(
+          ActiveModelSerializers::SerializableResource
+            .new(@child_connection,
+                 serializer: Api::V2::InventoryItemRelationshipSerializer,
+                 include: :child)
+            .to_json
+        )['data']
+      )
+      expect(hash_body).to include('included')
+    end
+  end
+
   describe 'POST #create' do
     let(:create_action) do
-      post api_v2_team_inventory_item_inventory_item_relationships_path(
+      post api_v2_team_inventory_item_child_relationships_path(
         team_id: @team.id,
         inventory_id: @inventory.id,
         item_id: @inventory_item.id
@@ -26,31 +75,13 @@ RSpec.describe 'Api::V2::InventoryItemRelationshipsController', type: :request d
     end
 
     context 'with valid parameters' do
-      context 'using parent_id' do
-        let(:request_body) do
-          {
-            data: {
-              type: 'inventory_item_relationships',
-              attributes: {
-                parent_id: @other_inventory_item.id
-              }
-            }
-          }
-        end
-  
-        it 'creates a new relationship' do
-          expect { create_action }.to change { RepositoryRowConnection.count }.by(1)
-          expect(response).to have_http_status(201)
-        end
-      end
-
       context 'using child_id' do
         let(:request_body) do
           {
             data: {
               type: 'inventory_item_relationships',
               attributes: {
-                parent_id: @other_inventory_item.id
+                child_id: @other_inventory_item.id
               }
             }
           }
@@ -63,26 +94,7 @@ RSpec.describe 'Api::V2::InventoryItemRelationshipsController', type: :request d
       end
     end
 
-    context 'with non valid parameters' do
-      context 'with mutually exclusive parameters' do
-        let(:request_body) do
-          {
-            data: {
-              type: 'inventory_item_relationships',
-              attributes: {
-                parent_id: @inventory_item.id,
-                child_id: @other_inventory_item.id
-              }
-            }
-          }
-        end
-  
-        it 'raises a mutually exclusive parameters error' do
-          create_action
-          expect(response).to have_http_status(400)
-        end
-      end
-  
+    context 'with non valid parameters' do 
       context 'with missing type' do
         let(:request_body) { { data: { type: '', attributes: { parent_id: @other_inventory_item.id } } } }
   
@@ -92,13 +104,13 @@ RSpec.describe 'Api::V2::InventoryItemRelationshipsController', type: :request d
         end
       end
   
-      context 'with missing item with parent_id or child_id' do
+      context 'with child_id for missing item' do
         let(:request_body) do
           {
             data: {
               type: 'inventory_item_relationships',
               attributes: {
-                parent_id: -1
+                child_id: -1
               }
             }
           }
@@ -112,7 +124,7 @@ RSpec.describe 'Api::V2::InventoryItemRelationshipsController', type: :request d
 
       context 'without manage permission' do
         let(:another_create_action) do
-          post api_v2_team_inventory_item_inventory_item_relationships_path(
+          post api_v2_team_inventory_item_child_relationships_path(
             team_id: @team.id,
             inventory_id: @inventory.id,
             item_id: @inventory_item.id
@@ -121,7 +133,7 @@ RSpec.describe 'Api::V2::InventoryItemRelationshipsController', type: :request d
             data: {
               type: 'inventory_item_relationships',
               attributes: {
-                parent_id: @other_inventory_item.id
+                child_id: @other_inventory_item.id
               }
             }
           }.to_json,
@@ -143,7 +155,7 @@ RSpec.describe 'Api::V2::InventoryItemRelationshipsController', type: :request d
     let(:relationship) { create(:repository_row_connection, parent: @inventory_item, child: @other_inventory_item, created_by: @user) }
 
     let(:destroy_action) do
-      delete api_v2_team_inventory_item_inventory_item_relationship_path(
+      delete api_v2_team_inventory_item_child_relationship_path(
         team_id: @team.id,
         inventory_id: @inventory.id,
         item_id: @inventory_item.id,
@@ -161,7 +173,7 @@ RSpec.describe 'Api::V2::InventoryItemRelationshipsController', type: :request d
 
     context 'without manage permission' do
       let(:another_destroy_action) do
-        delete api_v2_team_inventory_item_inventory_item_relationship_path(
+        delete api_v2_team_inventory_item_child_relationship_path(
           team_id: @team.id,
           inventory_id: @inventory.id,
           item_id: @inventory_item.id,
@@ -182,7 +194,7 @@ RSpec.describe 'Api::V2::InventoryItemRelationshipsController', type: :request d
 
     context 'with invalid ID' do
       it 'returns 404 not found' do
-        delete api_v2_team_inventory_item_inventory_item_relationship_path(
+        delete api_v2_team_inventory_item_child_relationship_path(
           team_id: @team.id,
           inventory_id: @inventory.id,
           item_id: @inventory_item.id,
