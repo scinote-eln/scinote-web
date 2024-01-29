5662dbb1c9618f46422c90ecb94d0b5d186f8a30
wandji20 <wbertrand10@gmail.com>
2024-01-29 15:34:15 +0100

    Force load event for previewing images on firefox browser [SCI-10058]

--- app/assets/javascripts/shared/file_preview.js
+++ app/assets/javascripts/shared/file_preview.js
@@ -1,7 +1,11 @@
 /* global ActiveStoragePreviews */
-
 (function() {
   $('.asset-image')
     .on('load', (event) => ActiveStoragePreviews.showPreview(event))
     .on('error', (event) => ActiveStoragePreviews.reCheckPreview(event));
+
+  // Force load event for image on firefox browser.
+  if (/firefox/i.test(navigator.userAgent)) {
+    $('.asset-image').trigger('load');
+  }
 }());
