8dcd00c433dd301bd2ae1da87968281316225136
Alex Kriuchykhin <oleksii@scinote.net>
2024-02-01 13:14:31 +0100

    Add support of server side session store [SCI-9960] (#6974)

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source 'http://rubygems.org'
 
 ruby '3.2.2'
 
+gem 'activerecord-session_store'
 gem 'bootsnap', require: false
 gem 'devise', '~> 4.8.1'
 gem 'devise_invitable'
---
+++ db/migrate/20240118094253_add_sessions_table.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class AddSessionsTable < ActiveRecord::Migration[7.0]
+  def change
+    create_table :sessions do |t|
+      t.string :session_id, null: false
+      t.jsonb :data
+      t.timestamps
+    end
+
+    add_index :sessions, :session_id, unique: true
+    add_index :sessions, :updated_at
+  end
+end
