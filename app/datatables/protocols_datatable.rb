af3384220dcc24c30d6d8111a4fbc12646377f32
Oleksii Kriuchykhin <okriuchykhin@biosistemika.com>
2024-02-07 11:19:33 +0100

    Temporary disable linked protocols counters on protocols table

--- app/datatables/protocols_datatable.rb
+++ app/datatables/protocols_datatable.rb
@@ -162,16 +162,6 @@ class ProtocolsDatatable < CustomDatatable
       .joins("LEFT OUTER JOIN protocols protocol_versions " \
              "ON protocol_versions.protocol_type = #{Protocol.protocol_types[:in_repository_published_version]} " \
              "AND protocol_versions.parent_id = protocols.parent_id")
-      .joins("LEFT OUTER JOIN protocols self_linked_task_protocols " \
-             "ON self_linked_task_protocols.protocol_type = #{Protocol.protocol_types[:linked]} " \
-             "AND self_linked_task_protocols.parent_id = protocols.id")
-      .joins("LEFT OUTER JOIN protocols parent_linked_task_protocols " \
-             "ON parent_linked_task_protocols.protocol_type = #{Protocol.protocol_types[:linked]} " \
-             "AND parent_linked_task_protocols.parent_id = protocols.parent_id")
-      .joins("LEFT OUTER JOIN protocols version_linked_task_protocols " \
-             "ON version_linked_task_protocols.protocol_type = #{Protocol.protocol_types[:linked]} " \
-             "AND version_linked_task_protocols.parent_id = protocol_versions.id " \
-             "AND version_linked_task_protocols.parent_id != protocols.id")
       .joins('LEFT OUTER JOIN "protocol_protocol_keywords" ' \
              'ON "protocol_protocol_keywords"."protocol_id" = "protocols"."id"')
       .joins('LEFT OUTER JOIN "protocol_keywords" ' \
@@ -190,10 +180,8 @@ class ProtocolsDatatable < CustomDatatable
       'STRING_AGG(DISTINCT("protocol_keywords"."name"), \', \') AS "protocol_keywords_str"',
       "CASE WHEN protocols.protocol_type = #{Protocol.protocol_types[:in_repository_draft]} " \
       "THEN 0 ELSE COUNT(DISTINCT(\"protocol_versions\".\"id\")) + 1 " \
-      "END AS nr_of_versions",
-      '(COUNT(DISTINCT("self_linked_task_protocols"."id")) + ' \
-      'COUNT(DISTINCT("parent_linked_task_protocols"."id")) + ' \
-      'COUNT(DISTINCT("version_linked_task_protocols"."id"))) AS nr_of_linked_tasks',
+      "END AS nr_of_versions ",
+      '0 AS nr_of_linked_tasks',
       'COUNT(DISTINCT("all_user_assignments"."id")) AS "nr_of_assigned_users"',
       'MAX("users"."full_name") AS "full_username_str"', # "Hack" to get single username
       'MAX("archived_users"."full_name") AS "archived_full_username_str"'
