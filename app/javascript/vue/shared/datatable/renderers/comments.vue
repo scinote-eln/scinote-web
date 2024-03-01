3067b835db626fc32442112b6365fb2e0466d3a3
sboursen-scinote <soufiane@scinote.net>
2024-03-01 09:48:25 +0100

    Fix unssen comments font [SCI-10262]

--- app/javascript/vue/shared/datatable/renderers/comments.vue
+++ app/javascript/vue/shared/datatable/renderers/comments.vue
@@ -11,7 +11,7 @@
       +
     </template>
     <span v-if="params.data.comments.count_unseen > 0"
-          class="unseen-comments inline-flex align-super text-xs font-semibold rounded-full bg-sn-science-blue text-sn-white w-3.5 h-3.5 items-center justify-center">
+          class="unseen-comments inline-flex align-super text-xs rounded-full bg-sn-science-blue text-sn-white w-3.5 h-3.5 items-center justify-center">
       {{params.data.comments.count_unseen }}
     </span>
   </a>
