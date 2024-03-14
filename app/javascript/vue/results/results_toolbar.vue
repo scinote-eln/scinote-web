44193fc4993c9488aa2a789df985e643cec23fb1
sboursen-scinote <soufiane@scinote.net>
2024-03-14 11:43:51 +0100

    Fix color of filter icon in task results [SCI-10437]

--- app/javascript/vue/results/results_toolbar.vue
+++ app/javascript/vue/results/results_toolbar.vue
@@ -33,7 +33,7 @@
         </li>
       </ul>
     </div>
-    <div class="result-toolbar__right flex items-center">
+    <div class="result-toolbar__right flex items-center [&_.sn-icon-filter]:!text-sn-blue">
       <button class="btn btn-secondary mr-3" @click="collapseResults" tabindex="0">
         {{ i18n.t('my_modules.results.collapse_label') }}
       </button>
