f749ebaf0faa608669e946892749f155895f74b7
ajugo <andrej.jugovic7@gmail.com>
2024-03-18 10:30:03 +0100

    Revert "Replace task flow buttons partial after successful change of task status [SCI-10198]"

--- app/assets/javascripts/my_modules/status_flow.js
+++ app/assets/javascripts/my_modules/status_flow.js
@@ -44,7 +44,7 @@
         data: { my_module: { status_id: item.data('state-id') } },
         success: function(result) {
           animateSpinner(null, false);
-          $('.task-sharing-and-flows .status-flow-container').replaceWith(result.html);
+          location.reload();
         },
         error: function(e) {
           animateSpinner(null, false);
