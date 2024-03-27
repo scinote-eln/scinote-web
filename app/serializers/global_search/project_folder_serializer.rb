51d3e4a1b4a54f48185ce9fa9104a0dae002f4d1
wandji20 <wbertrand10@gmail.com>
2024-03-27 09:24:27 +0100

    Add project folders group to global search page [SCI-10470]

---
+++ app/serializers/global_search/project_folder_serializer.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+module GlobalSearch
+  class ProjectFolderSerializer < ActiveModel::Serializer
+    include Rails.application.routes.url_helpers
+
+    attributes :id, :name, :code, :created_at, :team, :parent_folder, :archived, :url
+
+    def team
+      {
+        name: object.team.name,
+        url: projects_path(project_folder_id: object.id)
+      }
+    end
+
+    def created_at
+      I18n.l(object.created_at, format: :full_date)
+    end
+
+    def url
+      project_folder_path(object)
+    end
+
+    def parent_folder
+      if object.parent_folder_id?
+        {
+          name: object.parent_folder.name,
+          url: project_folder_path(object.parent_folder.id)
+        }
+      end
+    end
+  end
+end
