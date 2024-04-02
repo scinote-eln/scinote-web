ad7310834a1b44523016f501da69db7914f36627
sboursen-scinote <soufiane@scinote.net>
2024-04-02 18:03:59 +0200

    Fix experiment description position in table [SCI-10519]

--- app/javascript/vue/experiments/renderers/description.vue
+++ app/javascript/vue/experiments/renderers/description.vue
@@ -2,9 +2,10 @@
   <div class="group relative flex items-center group-hover:marker text-xs h-full w-full"
        :style="{ lineHeight: 'unset' }">
     <div class="flex gap-2"
+        :style="{ lineHeight: 'unset' }"
         :class="{
-                  'items-center': params.dtComponent.currentViewRender === 'table',
-                  'items-end': params.dtComponent.currentViewRender === 'cards'
+                  'items-center text-sm': params.dtComponent.currentViewRender === 'table',
+                  'items-end text-xs': params.dtComponent.currentViewRender === 'cards'
                 }">
       <span v-if="shouldTruncateText"
             class="cursor-pointer grow"
@@ -16,7 +17,14 @@
             v-html="params.data.sa_description">
       </span>
       <span v-else class="grow" v-html="params.data.sa_description"></span>
-      <span v-if="shouldTruncateText" @click.stop="showDescriptionModal" class="text-sn-blue cursor-pointer shrink-0 text-xs">{{ i18n.t('experiments.card.more') }}</span>
+      <span v-if="shouldTruncateText" @click.stop="showDescriptionModal" class="text-sn-blue cursor-pointer shrink-0 inline-block"
+            :style="{ lineHeight: 'unset' }"
+            :class="{
+                      'text-xs': params.dtComponent.currentViewRender === 'cards',
+                      'text-sm': params.dtComponent.currentViewRender === 'table'
+                    }">
+        {{ i18n.t('experiments.card.more') }}
+      </span>
     </div>
   </div>
 </template>
