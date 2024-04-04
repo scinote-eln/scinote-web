a0e9d04611583881eaac2e72e1fa6863d1ceb383
Giga Chubinidze <giga@scinote.net>
2024-04-04 05:29:02 +0400

    When exporting inventory items, do not convert smart annotation tags to text [SCI-10534]

--- app/services/repository_csv_export.rb
+++ app/services/repository_csv_export.rb
@@ -62,9 +62,7 @@ module RepositoryCsvExport
                          if cell.value_type == 'RepositoryAssetValue' && handle_file_name_func
                            handle_file_name_func.call(cell.value.asset)
                          else
-                           SmartAnnotations::TagToText.new(
-                             user, repository.team, cell.value.export_formatted
-                           ).text
+                            cell.value.export_formatted
                          end
                        end
           end
