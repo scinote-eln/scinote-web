e1ecc9f58498e1a8370f71e15b5b7a9781b5115c
Martin Artnik <martin@scinote.net>
2024-04-08 11:57:30 +0200

    Add missing import modal files

---
+++ app/javascript/vue/repositories/modals/import.vue
@@ -0,0 +1,35 @@
+<template>
+  <infoModal ref="modal" :startHidden="true" :infoParams="{}" :title="'UPDATE'" :helpText="'HELP ME'">
+    UPDAE
+  </infoModal>
+</template>
+
+<script>
+/* global HelperModule */
+
+import axios from '../../../packs/custom_axios.js';
+import InfoModal from '../../shared/info_modal.vue';
+
+export default {
+  name: 'ImportRepositoryModal',
+  components: {InfoModal},
+  props: {
+    repositoryUrl: String, required: true
+  },
+  data() {
+    return {
+      infoParams: {}
+    };
+  },
+  created() {
+    window.importRepositoryModalComponent = this;
+  },
+  mounted() {
+  },
+  methods: {
+    open() {
+      this.$refs.modal.open();
+    }
+  }
+};
+</script>
