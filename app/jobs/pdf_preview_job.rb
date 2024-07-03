33f6d6e87b9800ddf2d7e2204e855579ccf7740b
Martin Artnik <martin@scinote.net>
2024-07-03 14:25:23 +0200

    Timeout slow libreoffice preview generation [SCI-10809]

--- app/jobs/pdf_preview_job.rb
+++ app/jobs/pdf_preview_job.rb
@@ -16,39 +16,9 @@ class PdfPreviewJob < ApplicationJob
 
   def perform(asset_id)
     asset = Asset.find(asset_id)
-    blob = asset.file.blob
-    blob.open(tmpdir: tempdir) do |input|
-      work_dir = File.dirname(input.path)
-      preview_filename = "#{File.basename(input.path, '.*')}.pdf"
-      preview_file = File.join(work_dir, preview_filename)
-      Rails.logger.info "Starting preparing document preview for file #{blob.filename.sanitized}..."
 
-      ActiveRecord::Base.transaction do
-        success = system(
-          libreoffice_path, '--headless', '--invisible', '--convert-to', 'pdf', '--outdir', work_dir, input.path
-        )
-        unless success && File.file?(preview_file)
-          raise StandardError, "There was an error generating PDF preview, blob id: #{blob.id}"
-        end
-
-        ActiveRecord::Base.no_touching do
-          asset.file_pdf_preview.attach(io: File.open(preview_file), filename: "#{blob.filename.base}.pdf")
-          asset.update(pdf_preview_processing: false)
-        end
-        Rails.logger.info("Finished preparing PDF preview for file #{blob.filename.sanitized}.")
-      end
-    ensure
-      File.delete(preview_file) if File.file?(preview_file)
-    end
-  end
-
-  private
-
-  def tempdir
-    Rails.root.join('tmp')
-  end
-
-  def libreoffice_path
-    ENV['LIBREOFFICE_PATH'] || 'soffice'
+    PdfPreviewService.new(asset.file.blob, asset.file_pdf_preview).generate!
+  ensure
+    asset.update(pdf_preview_processing: false)
   end
 end
