b6af1031f5c5dcac7576699e1c81cbeb0d06966a
Martin Artnik <85488244+artoscinote@users.noreply.github.com>
2024-08-06 10:58:50 +0200

    Revert "Timeout slow libreoffice preview generation [SCI-10809] (#7678)"
    This reverts commit 1265f29a9ebdb23652e482453583e406f4387ca6.

--- app/jobs/reports/docx_preview_job.rb
+++ app/jobs/reports/docx_preview_job.rb
@@ -15,7 +15,39 @@ module Reports
 
     def perform(report_id)
       report = Report.find(report_id)
-      PdfPreviewService.new(report.docx_file.blob, report.docx_preview_file).generate!
+      blob = report.docx_file.blob
+      blob.open(tmpdir: tempdir) do |input|
+        work_dir = File.dirname(input.path)
+        preview_filename = "#{File.basename(input.path, '.*')}.pdf"
+        preview_file = File.join(work_dir, preview_filename)
+        Rails.logger.info "Starting preparing document preview for file #{blob.filename.sanitized}..."
+
+        ActiveRecord::Base.transaction do
+          success = system(
+            libreoffice_path, '--headless', '--invisible', '--convert-to', 'pdf', '--outdir', work_dir, input.path
+          )
+          unless success && File.file?(preview_file)
+            raise StandardError, "There was an error generating PDF preview, blob id: #{blob.id}"
+          end
+
+          ActiveRecord::Base.no_touching do
+            report.docx_preview_file.attach(io: File.open(preview_file), filename: "#{blob.filename.base}.pdf")
+          end
+          Rails.logger.info("Finished preparing PDF preview for file #{blob.filename.sanitized}.")
+        end
+      ensure
+        File.delete(preview_file) if File.file?(preview_file)
+      end
+    end
+
+    private
+
+    def tempdir
+      Rails.root.join('tmp')
+    end
+
+    def libreoffice_path
+      ENV['LIBREOFFICE_PATH'] || 'soffice'
     end
   end
 end
