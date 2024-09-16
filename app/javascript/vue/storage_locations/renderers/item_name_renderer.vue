f4e1498df15858f83531b134226712929d8578a7
Anton <anton@scinote.net>
2024-09-16 11:50:54 +0200

    Add repository item card to storage container [SCI-11048]

---
+++ app/javascript/vue/storage_locations/renderers/item_name_renderer.vue
@@ -0,0 +1,26 @@
+<template>
+  <span>
+    <span v-if="params.data.hidden" class="text-sn-grey-700">
+      <i class="sn-icon sn-icon-locked-task"></i>
+      {{ i18n.t('storage_locations.show.hidden') }}
+    </span>
+    <a
+      :href="params.data.row_url"
+      class="record-info-link"
+      :title="params.data.row_name"
+    >
+      {{ params.data.row_name }}
+    </a>
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'ItemNameRenderer',
+  props: {
+    params: {
+      required: true
+    }
+  }
+};
+</script>
