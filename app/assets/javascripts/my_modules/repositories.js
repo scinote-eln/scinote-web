24b9ff2386ac8b925952f51ef05b02872d88e6a0
Martin Artnik <85488244+artoscinote@users.noreply.github.com>
2024-10-08 10:19:32 +0200

    Revert "Don't unassign items when unsharing inventories [SCI-11108]"

--- app/assets/javascripts/my_modules/repositories.js
+++ app/assets/javascripts/my_modules/repositories.js
@@ -433,10 +433,10 @@ var MyModuleRepositories = (function() {
   }
 
   function initSimpleTable() {
-    $('#assigned-items-container').on('shown.bs.collapse', '.assigned-repository-container.readable-repository', function() {
+    $('#assigned-items-container').on('shown.bs.collapse', '.assigned-repository-container', function() {
       var repositoryContainer = $(this);
-      var repositoryTable = repositoryContainer.find('.repository-table');
-      var initializedTable = repositoryContainer.find('.dataTables_wrapper .repository-table');
+      var repositoryTable = repositoryContainer.find('.table');
+      var initializedTable = repositoryContainer.find('.dataTables_wrapper table');
 
       // do not try to re-initialized already initialized table
       if (initializedTable.length) {
