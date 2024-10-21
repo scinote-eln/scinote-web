21fb93110acffbc04619cbfb5f1a897a8961f096
Alex Kriuchykhin <oleksii@scinote.net>
2024-10-21 17:26:39 +0200

    Add option for enabling Aws::Rails::SesMailer for sending emails with AWS SDK [SCI-11207] (#7981)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
   }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = ENV['SMTP_USE_AWS_SES'] == 'true' ? :ses : :smtp
 
   config.action_mailer.smtp_settings = {
     address: Rails.application.secrets.mailer_address,
