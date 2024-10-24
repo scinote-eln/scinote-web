1939902eb1633e608ace5159d2e2959266d936d7
Alex Kriuchykhin <oleksii@scinote.net>
2024-10-24 13:22:21 +0200

    Fix NewRelic ignore calls for ActiveJobs [SCI-11212] (#7988)

--- app/jobs/notification_cleanup_job.rb
+++ app/jobs/notification_cleanup_job.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
 class NotificationCleanupJob < ApplicationJob
-  newrelic_ignore
-
   def perform
+    NewRelic::Agent.ignore_transaction
     Notification.where('created_at < ?', 3.months.ago).delete_all
   end
 end
