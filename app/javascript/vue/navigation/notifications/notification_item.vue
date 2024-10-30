649a8abe66ac9ad6ea0f3acc6223f513f95b6cea
Anton <anton@scinote.net>
2024-10-30 11:44:30 +0100

    Fixes for notifications improvment [SCI-11235]

--- app/javascript/vue/navigation/notifications/notification_item.vue
+++ app/javascript/vue/navigation/notifications/notification_item.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="sci-navigation--notificaitons-flyout-notification">
+  <div class="sci-navigation--notificaitons-flyout-notification hover:bg-sn-super-light-grey !px-2 !-mx-2">
     <div class="flex item-center">
-      <div class="sci-navigation--notificaitons-flyout-notification-date">
-        {{ notification.attributes.created_at }}
-      </div>
+      <a :href="lastBreadcrumbUrl" @click="toggleRead(true); closeFlyout()" class="hover:no-underline text-black hover:text-black grow">
+        <div class="sci-navigation--notificaitons-flyout-notification-date">
+          {{ notification.attributes.created_at }}
+        </div>
+      </a>
       <div class="ml-auto cursor-pointer" @click="toggleRead()">
         <div v-if="!notification.attributes.checked" class="w-2.5 h-2.5 bg-sn-coral rounded-full cursor-pointer"></div>
-        <div v-else class="w-2.5 h-2.5 border-2 border-sn-grey rounded-full border-solid cursor-pointer"></div>
+        <div v-else class="w-2.5 h-2.5 border-2 border-sn-grey rounded-full border-solid cursor-pointer hover:border-sn-coral"></div>
       </div>
     </div>
-    <a :href="lastBreadcrumbUrl" @click="toggleRead(true)" class="hover:no-underline text-black hover:text-black">
+    <a :href="lastBreadcrumbUrl" @click="toggleRead(true); closeFlyout()" class="hover:no-underline text-black hover:text-black">
       <div class="sci-navigation--notificaitons-flyout-notification-title"
           v-html="notification.attributes.title"
           :data-seen="notification.attributes.checked"></div>
@@ -42,6 +44,9 @@ export default {
     }
   },
   methods: {
+    closeFlyout() {
+      this.$emit('close');
+    },
     toggleRead(check = false) {
       const params = {};
       if (!this.notification.attributes.checked || check) {
