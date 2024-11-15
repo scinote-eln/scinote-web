718fafdec8a20d1579ee8be2dedb4734cd92b7f2
Anton <anton@scinote.net>
2024-11-15 14:03:14 +0100

    Fix file versions for api flies [SCI-11288]

--- app/javascript/vue/shared/file_versions_modal.vue
+++ app/javascript/vue/shared/file_versions_modal.vue
@@ -32,7 +32,9 @@
                   </div>
                   <div class="flex text-xs text-sn-grey justify-start">
                     <div class="mr-3">{{ fileVersion.attributes.created_at }}</div>
-                    <div class="mr-3 text-nowrap text-ellipsis overflow-hidden max-w-52">{{ fileVersion.attributes.created_by.full_name }}</div>
+                    <div v-if="fileVersion.attributes.created_by" class="mr-3 text-nowrap text-ellipsis overflow-hidden max-w-52">
+                      {{ fileVersion.attributes.created_by.full_name }}
+                    </div>
                     <div>{{ i18n.t("assets.file_versions_modal.size") }}: {{ Math.round(fileVersion.attributes.byte_size/1024) }}KB</div>
                   </div>
                 </div>
