dd1c2f550571d7fb5faf8b81139679009b31c056
Alex Kriuchykhin <oleksii@scinote.net>
2024-11-21 15:10:15 +0100

    Add option to set custom AWS region for SES mail API [SCI-11207] (#8057)

---
+++ config/initializers/action_mailer.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+if ENV['SMTP_USE_AWS_SES'] == 'true'
+  options = {}
+  options[:region] = ENV['AWS_SES_REGION'] if ENV['AWS_SES_REGION'].present?
+  ActionMailer::Base.add_delivery_method :ses, Aws::Rails::SesMailer, **options
+end
