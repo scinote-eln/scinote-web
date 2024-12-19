ec6b586151b1c78f1ddbb55cddc0168b83ee95d6
Martin Artnik <martin@scinote.net>
2024-12-19 08:47:20 +0100

    Fix WOPI action url [SCI-11325]

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -299,7 +299,7 @@ class Asset < ApplicationRecord
 
       # Extract only the licenced user flag parameter
       is_licenced_user = ENV['WOPI_BUSINESS_USERS'] == 'true' && action_url.include?('IsLicensedUser=BUSINESS_USER')
-      action_url = action_url.split('?').first + "?IsLicencedUser=#{is_licenced_user ? 1 : 0}"
+      action_url = action_url.split('?').first + "?IsLicencedUser=#{is_licenced_user ? 1 : 0}&"
 
       rest_url = Rails.application.routes.url_helpers.wopi_rest_endpoint_url(
         host: ENV['WOPI_ENDPOINT_URL'],
