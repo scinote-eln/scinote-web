4359e300b82f13681a5e1105eac3b32a0c734a0e
Anton <anton@scinote.net>
2024-12-30 16:19:58 +0100

    Add archive and restore actions to forms table [SCI-11405]

--- app/services/lists/forms_service.rb
+++ app/services/lists/forms_service.rb
@@ -9,6 +9,11 @@ module Lists
 
     def fetch_records
       @records = Form.where(team: @team).readable_by_user(@user)
+
+      view_mode = @params[:view_mode] || 'active'
+
+      @records = @records.archived if view_mode == 'archived'
+      @records = @records.active if view_mode == 'active'
     end
 
     def filter_records; end
