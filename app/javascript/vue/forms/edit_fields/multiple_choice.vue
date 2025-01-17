b5624b60538c9677a1842a08dd0a7292ffcc1a48
Anton <anton@scinote.net>
2025-01-17 14:19:53 +0100

    Fix checklists label [SCI-11445]

--- app/javascript/vue/forms/edit_fields/multiple_choice.vue
+++ app/javascript/vue/forms/edit_fields/multiple_choice.vue
@@ -2,13 +2,13 @@
   <div class="flex flex-col gap-4">
     <hr class="my-4 w-full">
     <div>
-      <h5 class="mb-4">{{ i18n.t('forms.show.dropdown_options_label') }}</h5>
+      <h5 class="mb-4">{{ i18n.t('forms.show.checklist_options_label') }}</h5>
       <div class="sci-input-container-v2 h-40" :class="{'error': !validField}" :data-error="optionFieldErrors">
         <textarea
           class="sci-input"
           v-model="options"
           @change="$emit('updateField')"
-          :placeholder="i18n.t('forms.show.dropdown_options_placeholder_html')" />
+          :placeholder="i18n.t('forms.show.checklist_options_placeholder_html')" />
       </div>
     </div>
     <hr class="my-4 w-full">
