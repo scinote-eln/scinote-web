cbca201d60260c692352f6de80c1ab07596cd88e
Anton <anton@scinote.net>
2025-04-24 14:01:05 +0200

    Add new sorting for projects table [SCI-11835]

--- app/services/lists/projects_service.rb
+++ app/services/lists/projects_service.rb
@@ -1,5 +1,7 @@
 module Lists
   class ProjectsService < BaseService
+    include ActionView::Helpers::SanitizeHelper
+
     def initialize(team, user, folder, params)
       @team = team
       @user = user
@@ -61,9 +63,7 @@ module Lists
       search_query = @params[:search].presence || @filters[:query]
       records = records.where_attributes_like(['projects.name', Project::PREFIXED_ID_SQL, 'projects.description'], search_query) if search_query.present?
 
-      if @filters[:members].present?
-        records = records.joins(:user_assignments).where(user_assignments: { user_id: @filters[:members].values })
-      end
+      records = records.joins(:user_assignments).where(user_assignments: { user_id: @filters[:members].values }) if @filters[:members].present?
       if @filters[:created_at_from].present?
         records = records.where('projects.created_at > ?',
                                 @filters[:created_at_from])
@@ -73,9 +73,7 @@ module Lists
         records = records.where('projects.archived_on < ?',
                                 @filters[:archived_on_to])
       end
-      if @filters[:archived_on_from].present?
-        records = records.where('projects.archived_on > ?', @filters[:archived_on_from])
-      end
+      records = records.where('projects.archived_on > ?', @filters[:archived_on_from]) if @filters[:archived_on_from].present?
       records
     end
 
@@ -128,6 +126,26 @@ module Lists
         @records = @records.sort_by { |object| project_comments_count(object) }
       when 'comments_DESC'
         @records = @records.sort_by { |object| project_comments_count(object) }.reverse!
+      when 'start_on_ASC'
+        @records = @records.sort_by { |object| project_start_on(object) }
+      when 'start_on_DESC'
+        @records = @records.sort_by { |object| project_start_on(object) }.reverse!
+      when 'due_date_ASC'
+        @records = @records.sort_by { |object| project_due_date(object) }
+      when 'due_date_DESC'
+        @records = @records.sort_by { |object| project_due_date(object) }.reverse!
+      when 'status_ASC'
+        @records = @records.sort_by { |object| project_status(object, 'asc') }
+      when 'status_DESC'
+        @records = @records.sort_by { |object| project_status(object, 'desc') }.reverse!
+      when 'supervised_by_ASC'
+        @records = @records.sort_by { |object| project_supervised_by(object, 'asc') }
+      when 'supervised_by_DESC'
+        @records = @records.sort_by { |object| project_supervised_by(object, 'desc') }.reverse!
+      when 'description_ASC'
+        @records = @records.sort_by { |object| project_description(object, 'asc') }
+      when 'description_DESC'
+        @records = @records.sort_by { |object| project_description(object, 'desc') }.reverse!
       end
     end
 
@@ -141,6 +159,56 @@ module Lists
 
     def project_users_count(object)
       project?(object) ? object.users.count : -1
+    end
+
+    def project_start_on(object)
+      return Date.new(2100, 1, 1) unless project?(object)
+
+      object.start_on || Date.new(2100, 1, 1)
+    end
+
+    def project_due_date(object)
+      return Date.new(2100, 1, 1) unless project?(object)
+
+      object.due_date || Date.new(2100, 1, 1)
+    end
+
+    def project_status(object, direction)
+      return (direction == 'asc' ? 10 : -1) unless project?(object)
+
+      statuses = {
+        not_started: 0,
+        started: 1,
+        completed: 2
+      }
+
+      statuses[object.status.to_sym]
+    end
+
+    def project_supervised_by(object, direction)
+      no_value = direction == 'asc' ? 1 : 0
+      has_value = direction == 'asc' ? 0 : 1
+
+      return [no_value, ''] unless project?(object)
+
+      if object.supervised_by
+        [has_value, object.supervised_by.name]
+      else
+        [no_value, '']
+      end
+    end
+
+    def project_description(object, direction)
+      no_value = direction == 'asc' ? 1 : 0
+      has_value = direction == 'asc' ? 0 : 1
+
+      return [no_value, ''] unless project?(object)
+
+      if object.description.present?
+        [has_value, strip_tags(object.description)]
+      else
+        [no_value, '']
+      end
     end
 
     def project?(object)
