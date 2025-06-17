935b1f5cd8c796decb24bf8116b0a11ce83bd792
Anton <anton@scinote.net>
2025-06-17 11:46:58 +0200

    Add user groups table [SCI-11955]

--- app/controllers/users/settings/user_group_memberships_controller.rb
+++ app/controllers/users/settings/user_group_memberships_controller.rb
@@ -11,7 +11,21 @@ module Users
 
       def show; end
 
-      def create; end
+      def create
+        ActiveRecord::Base.transaction do
+          new_users = @team.users.where(id: params[:user_ids])
+
+          new_users.each do |user|
+            @user_group.user_group_memberships.create!(user: user, created_by: current_user)
+          end
+
+          render json: { message: :success }, status: :created
+        rescue ActiveRecord::RecordInvalid => e
+          Rails.logger.error e.message
+          head :unprocessable_entity
+          raise ActiveRecord::Rollback
+        end
+      end
 
       def destroy; end
 
@@ -25,12 +39,12 @@ module Users
         @user_group = @team.user_groups.find(params[:user_group_id])
       end
 
-      def load_user_group
+      def load_user_group_membership
         @user_group_membership = @user_group.user_group_memberships.find(params[:id])
       end
 
       def check_manage_permissions
-        render_403 unless can_manage_team(@team)
+        render_403 unless can_manage_team?(@team)
       end
     end
   end
