2b7de4871c63bdb27709fddf96c2e83f6e531fbc
Martin Artnik <85488244+artoscinote@users.noreply.github.com>
2025-06-23 15:36:15 +0200

    Fix label template migration [SCI-11903]

--- db/migrate/20250530134309_fix_default201dpi_label_templates.rb
+++ db/migrate/20250530134309_fix_default201dpi_label_templates.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
-
 class FixDefault201dpiLabelTemplates < ActiveRecord::Migration[7.0]
+  class TempLabelTemplate < ApplicationRecord
+    self.table_name = 'label_templates'
+  end
+  
   def up
-    LabelTemplate.where(
+    TempLabelTemplate.where(
       content: "^XA\n^MTT\n^MUD,200,200\n^PR2\n^MD30\n^LH0,8\n^PW180\n^CF0,15\n^FO0,5^FD{{ITEM_ID}}^FS\n^FO0,13^BQN,2,3^FDMA,{{ITEM_ID}}^FS\n^FO70,27^FB100,2,0,L^FD{{NAME}}^FS^FS\n^XZ\n"
     ).update!(
       content: "^XA\n^MTT\n^MUD,200,200\n^PR2\n^MD30\n^LH0,8\n^PW180\n^CF0,15\n^FO0,5^FD{{ITEM_ID}}^FS\n^FO0,13^BQN,2,3^FDMA,{{ITEM_ID}}^FS\n^FO70,27^FB100,4,0,L^FD{{NAME}}^FS^FS\n^XZ\n"
@@ -10,7 +13,7 @@ class FixDefault201dpiLabelTemplates < ActiveRecord::Migration[7.0]
   end
 
   def down
-    LabelTemplate.where(
+    TempLabelTemplate.where(
       content: "^XA\n^MTT\n^MUD,200,200\n^PR2\n^MD30\n^LH0,8\n^PW180\n^CF0,15\n^FO0,5^FD{{ITEM_ID}}^FS\n^FO0,13^BQN,2,3^FDMA,{{ITEM_ID}}^FS\n^FO70,27^FB100,4,0,L^FD{{NAME}}^FS^FS\n^XZ\n"
     ).update!(
       content: "^XA\n^MTT\n^MUD,200,200\n^PR2\n^MD30\n^LH0,8\n^PW180\n^CF0,15\n^FO0,5^FD{{ITEM_ID}}^FS\n^FO0,13^BQN,2,3^FDMA,{{ITEM_ID}}^FS\n^FO70,27^FB100,2,0,L^FD{{NAME}}^FS^FS\n^XZ\n"
