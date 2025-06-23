d1461085e9357e35f076872a4382f396d02b66c9
Martin Artnik <martin@scinote.net>
2025-06-23 16:38:44 +0200

    Fix label template migration [SCI-11903]

--- db/migrate/20250530134309_fix_default201dpi_label_templates.rb
+++ db/migrate/20250530134309_fix_default201dpi_label_templates.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
+
 class FixDefault201dpiLabelTemplates < ActiveRecord::Migration[7.0]
   class TempLabelTemplate < ApplicationRecord
     self.table_name = 'label_templates'
   end
-  
+
   def up
     TempLabelTemplate.where(
       content: "^XA\n^MTT\n^MUD,200,200\n^PR2\n^MD30\n^LH0,8\n^PW180\n^CF0,15\n^FO0,5^FD{{ITEM_ID}}^FS\n^FO0,13^BQN,2,3^FDMA,{{ITEM_ID}}^FS\n^FO70,27^FB100,2,0,L^FD{{NAME}}^FS^FS\n^XZ\n"
-    ).update!(
+    ).update_all(
       content: "^XA\n^MTT\n^MUD,200,200\n^PR2\n^MD30\n^LH0,8\n^PW180\n^CF0,15\n^FO0,5^FD{{ITEM_ID}}^FS\n^FO0,13^BQN,2,3^FDMA,{{ITEM_ID}}^FS\n^FO70,27^FB100,4,0,L^FD{{NAME}}^FS^FS\n^XZ\n"
     )
   end
@@ -15,7 +16,7 @@ class FixDefault201dpiLabelTemplates < ActiveRecord::Migration[7.0]
   def down
     TempLabelTemplate.where(
       content: "^XA\n^MTT\n^MUD,200,200\n^PR2\n^MD30\n^LH0,8\n^PW180\n^CF0,15\n^FO0,5^FD{{ITEM_ID}}^FS\n^FO0,13^BQN,2,3^FDMA,{{ITEM_ID}}^FS\n^FO70,27^FB100,4,0,L^FD{{NAME}}^FS^FS\n^XZ\n"
-    ).update!(
+    ).update_all(
       content: "^XA\n^MTT\n^MUD,200,200\n^PR2\n^MD30\n^LH0,8\n^PW180\n^CF0,15\n^FO0,5^FD{{ITEM_ID}}^FS\n^FO0,13^BQN,2,3^FDMA,{{ITEM_ID}}^FS\n^FO70,27^FB100,2,0,L^FD{{NAME}}^FS^FS\n^XZ\n"
     )
   end
