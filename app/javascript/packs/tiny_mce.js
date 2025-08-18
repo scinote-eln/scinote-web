a932e4231d5746e96f964d370979e1500b844865
Andrej <andrej@scinote.net>
2025-08-18 14:02:13 +0200

    Add nested lists for tinyMce fields [SCI-12192]

--- app/javascript/packs/tiny_mce.js
+++ app/javascript/packs/tiny_mce.js
@@ -42,7 +42,21 @@ import contentUiCss from '!!raw-loader!tinymce/skins/ui/tinymce-5/content.min.cs
 
 const contentPStyle = `p { margin: 0; padding: 0;}`;
 const contentBodyStyle = `body { font-family: "SN Inter", "Open Sans", Arial, Helvetica, sans-serif }`;
-const contentStyle = [contentCss, contentUiCss, contentBodyStyle, contentPStyle].map((s) => s.toString()).join('\n');
+const contentListStyle = `
+  ol:not([style*="list-style-type"]) {
+    counter-reset: item;
+  }
+
+  ol:not([style*="list-style-type"]) li {
+    display: block;
+  }
+
+  ol:not([style*="list-style-type"]) li:before {
+    content: counters(item, ".") ". ";
+    counter-increment: item;
+  }
+`;
+const contentStyle = [contentCss, contentUiCss, contentBodyStyle, contentPStyle, contentListStyle].map((s) => s.toString()).join('\n');
 
 // Optional pre-initialization method
 if (typeof(window.preTinyMceInit) === 'function') {
