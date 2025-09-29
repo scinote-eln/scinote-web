96052aee71d37ab9953d2a6eee8cf6a4dcc1f877
Andrej <andrej@scinote.net>
2025-09-29 09:51:54 +0200

    Add remove reminders for everyone [SCI-12318]

--- app/controllers/hidden_repository_cell_reminders_controller.rb
+++ app/controllers/hidden_repository_cell_reminders_controller.rb
@@ -4,10 +4,23 @@ class HiddenRepositoryCellRemindersController < ApplicationController
   before_action :load_repository
   before_action :load_repository_row
   before_action :check_read_permissions
+  before_action :check_manage_permissions, only: :create_all
 
   def create
     hidden_repository_cell_reminder =
       current_user.hidden_repository_cell_reminders.create!(repository_cell_id: params[:repository_cell_id])
+
+    render json: hidden_repository_cell_reminder, status: :ok
+  end
+
+  def create_all
+    repository_cell_id = params[:repository_cell_id]
+    hidden_repository_cell_reminder = current_user.hidden_repository_cell_reminders.create!(repository_cell_id: repository_cell_id)
+    @repository_row.repository.users.where.not(id: current_user.id).each do |user|
+      next if user.hidden_repository_cell_reminders.find_by(repository_cell_id: repository_cell_id)
+
+      user.hidden_repository_cell_reminders.create!(repository_cell_id: repository_cell_id)
+    end
 
     render json: hidden_repository_cell_reminder, status: :ok
   end
@@ -26,5 +39,9 @@ class HiddenRepositoryCellRemindersController < ApplicationController
 
   def check_read_permissions
     render_403 unless can_read_repository?(@repository)
+  end
+
+  def check_manage_permissions
+    render_403 unless can_manage_repository_rows?(@repository)
   end
 end
