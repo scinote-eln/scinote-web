8d23e20c9fb70641c77d6957e86573da96c240e2
Martin Artnik <martin@scinote.net>
2025-11-28 11:58:38 +0100

    Enable direct uploads via API [SCI-12168]

--- app/controllers/api/v1/assets_controller.rb
+++ app/controllers/api/v1/assets_controller.rb
@@ -3,9 +3,10 @@
 module Api
   module V1
     class AssetsController < BaseController
+      include ::Api::V1::BlobCreation
+
       before_action :load_team, :load_project, :load_experiment, :load_task, :load_protocol, :load_step
       before_action :load_asset, only: :show
-      before_action :check_upload_type, only: :create
 
       def index
         attachments =
@@ -22,20 +23,7 @@ module Api
       def create
         raise PermissionError.new(Asset, :create) unless can_manage_protocol_in_module?(@protocol)
 
-        if @form_multipart_upload
-          asset = @step.assets.new(asset_params.merge({ team_id: @team.id }))
-        else
-          blob = ActiveStorage::Blob.create_and_upload!(
-            io: StringIO.new(Base64.decode64(asset_params[:file_data])),
-            filename: asset_params[:file_name],
-            content_type: asset_params[:file_type],
-            metadata: { created_by_id: current_user.id }
-          )
-          asset = @step.assets.new(file: blob, team: @team)
-        end
-
-        asset.save!(context: :on_api_upload)
-        asset.post_process_file
+        asset = attach_blob!(@step)
 
         render jsonapi: asset,
                serializer: AssetSerializer,
@@ -47,20 +35,15 @@ module Api
       def asset_params
         raise TypeError unless params.require(:data).require(:type) == 'attachments'
 
-        return params.require(:data).require(:attributes).permit(:file) if @form_multipart_upload
+        return params.require(:data).require(:attributes).permit(:file) if params.dig(:data, :attributes, :file)
 
-        attr_list = %i(file_data file_type file_name)
-        params.require(:data).require(:attributes).require(attr_list)
+        attr_list = %i(file_data file_type file_name signed_blob_id)
         params.require(:data).require(:attributes).permit(attr_list)
       end
 
       def load_asset
         @asset = @step.assets.find(params.require(:id))
         raise PermissionError.new(Asset, :read) unless can_read_protocol_in_module?(@asset.step.protocol)
-      end
-
-      def check_upload_type
-        @form_multipart_upload = true if params.dig(:data, :attributes, :file)
       end
     end
   end
