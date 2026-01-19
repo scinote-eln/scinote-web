a415482fe2fcf68fd35a70e368314b6d570ebb4e
Andrej <andrej@scinote.net>
2026-01-19 14:50:08 +0100

    Add skipping steps to worskpace automations [SCI-12748]

--- app/services/automation_observers/all_steps_completion_observer.rb
+++ app/services/automation_observers/all_steps_completion_observer.rb
@@ -4,12 +4,12 @@ module AutomationObservers
   class AllStepsCompletionObserver < BaseObserver
     def self.on_update(step, user)
       return unless Current.team.settings.dig('team_automation_settings', 'tasks', 'task_status_completed', 'on_all_steps_completion')
-      return unless step.saved_change_to_completed? && step.completed
+      return unless (step.saved_change_to_completed? && step.completed) || (step.saved_change_to_skipped_at? && step.skipped_at)
       return unless step.protocol.in_module?
 
       my_module = step.protocol.my_module
 
-      return unless my_module.my_module_status.previous_status == my_module.my_module_status_flow.initial_status && my_module.steps.where(completed: false).none?
+      return unless my_module.my_module_status.previous_status == my_module.my_module_status_flow.initial_status && my_module.steps.where(completed: false, skipped_at: nil).none?
 
       previous_status_id = my_module.my_module_status.id
       my_module.update!(my_module_status: my_module.my_module_status.next_status, last_modified_by: user)
