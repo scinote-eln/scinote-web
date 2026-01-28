ebc431bd844155798563f229a30d2de89f8113dc
Andrej <andrej@scinote.net>
2026-01-28 10:47:13 +0100

    Fix column sorting and searching for assigned repository table [SCI-12770]

--- app/models/repository_asset_value.rb
+++ app/models/repository_asset_value.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class RepositoryAssetValue < ApplicationRecord
+  include SearchableModel
+
   belongs_to :created_by,
              foreign_key: :created_by_id,
              class_name: 'User',
