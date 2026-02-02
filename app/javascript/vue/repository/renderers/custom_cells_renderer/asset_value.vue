306a5eb1ebf65654cf52216704e96783773d708b
Anton <anton@scinote.net>
2026-02-02 15:44:07 +0100

    Add file previews to assigned tables [SCI-12798]

--- app/javascript/vue/repository/renderers/custom_cells_renderer/asset_value.vue
+++ app/javascript/vue/repository/renderers/custom_cells_renderer/asset_value.vue
@@ -1,15 +1,33 @@
 <template>
   <div v-if="params.value">
-    {{ params.value.value.file_name }}
+    <a  :href="params.value.value.url"
+        class="file-preview-link file-name"
+        :id="`modal_link${params.value.value.id}`"
+        data-no-turbolink="true"
+        :data-id="params.value.value.id"
+        :data-preview-url="previewUrl"
+    >
+      {{ params.value.value.file_name }}
+    </a>
   </div>
 </template>
 
 <script>
+
+import {
+  asset_file_preview_path
+} from '../../../../routes.js';
+
 export default {
   name: 'AssetValue',
   props: {
     params: {
       required: true
+    }
+  },
+  computed: {
+    previewUrl() {
+      return asset_file_preview_path(this.params.value.value.id, { preview: true });
     }
   }
 };
