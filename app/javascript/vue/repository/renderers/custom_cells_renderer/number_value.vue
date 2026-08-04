adcc498f6d28cef7ec8f7534fdbe2659a475ae73
Anton <anton@scinote.net>
2026-08-04 12:21:43 +0200

    Add edit mode for number cell [SCI-13389]

--- app/javascript/vue/repository/renderers/custom_cells_renderer/number_value.vue
+++ app/javascript/vue/repository/renderers/custom_cells_renderer/number_value.vue
@@ -1,6 +1,19 @@
 <template>
-  <div v-if="params.value">
-    {{ params.value.value }}
+  <div v-if="params.data.permissions.manage" class="relative">
+    <input
+      ref="input"
+      v-model="newValue"
+      type="text"
+      :placeholder="i18n.t('repositories.table.number.enter_number')"
+      @keydown.enter="saveValue"
+      @keydown.esc="cancelEdit"
+      @blur="saveValue"
+      @input="validateFormat"
+      class="sci-table-input-v2 !border-transparent !bg-transparent placeholder:text-sn-grey"
+    />
+  </div>
+  <div v-else>
+    {{ this.newValue }}
   </div>
 </template>
 
@@ -10,6 +23,41 @@ export default {
   props: {
     params: {
       required: true
+    }
+  },
+  created() {
+    this.newValue = this.params?.value?.value;
+  },
+  data() {
+    return {
+      newValue: null,
+    };
+  },
+  methods: {
+    validateFormat() {
+      const decimals = this.params?.colDef?.cellRendererParams?.metadata?.decimals || 0;
+      const regexp = decimals === 0 ? /[^-0-9]/g : /[^-0-9.]/g;
+      const decimalsRegex = new RegExp(`^-?\\d*(\\.\\d{0,${decimals}})?`);
+      let value = this.newValue;
+      value = value.replace(regexp, '');
+      value = value.match(decimalsRegex)[0];
+      this.newValue = value;
+    },
+    saveValue() {
+      if (this.newValue !== this.params?.value?.value) {
+        this.params.dtComponent.$emit(
+          'updateCell',
+          this.params.data,
+          this.params.colDef,
+          this.newValue
+        );
+      }
+    },
+    cancelEdit() {
+      this.newValue = this.params?.value?.value;
+      this.$nextTick(() => {
+        this.$refs.input.blur();
+      });
     }
   }
 };
