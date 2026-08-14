1e718405562334ecf571e6aad1b7a0119f865aca
matej-scinote <matej@scinote.net>
2026-08-14 11:54:31 +0200

    Protocol locking UI/UX improvement:wqs [SCI-13430] (#9641)

--- app/javascript/vue/protocol/container.vue
+++ app/javascript/vue/protocol/container.vue
@@ -279,6 +279,7 @@
                 </template>
               </div>
             </div>
+            <div v-if="inRepository && steps.length > 0" class="sci-divider"></div>
             <div :class="{
                 'tw-hidden': loadingOverlay
               }"
