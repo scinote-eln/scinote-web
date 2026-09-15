dc73ca2f322c712364ecfe17c2c150c47ba52226
Andrej <andrej@scinote.net>
2026-09-15 13:54:04 +0200

    Add activities for analytical reports [SCI-13513]

--- app/jobs/experiments/generate_report_job.rb
+++ app/jobs/experiments/generate_report_job.rb
@@ -7,9 +7,24 @@ module Experiments
 
       ExperimentReports::GenerateReportService.new(analytical_report.reference, analytical_report, analytical_report.created_by).call(my_module_ids)
       analytical_report&.update(generating_status: :done)
+      log_activity(analytical_report)
     rescue StandardError => e
       analytical_report&.destroy
       raise e
+    end
+
+    private
+
+    def log_activity(analytical_report)
+      experiment = analytical_report.reference
+      Activities::CreateActivityService
+        .call(activity_type: :experiment_analytical_report_generated,
+              owner: analytical_report.created_by,
+              team: experiment.team,
+              project: experiment.project,
+              subject: experiment,
+              message_items: { analytical_report: analytical_report.id,
+                               experiment: experiment.id })
     end
   end
 end
--- app/jobs/my_modules/generate_report_job.rb
+++ app/jobs/my_modules/generate_report_job.rb
@@ -22,11 +22,28 @@ module MyModules
         MyModuleReports::MergePdfsWithWatermarkService.new(user, analytical_report, asset_ids, header_text, footer_text, add_numarization, add_blank_page).generate!
       end
       analytical_report&.update(generating_status: :done)
+
+      log_activity(analytical_report)
     rescue StandardError => e
       analytical_report&.destroy
       raise e
     ensure
       I18n.backend.date_format = nil
+    end
+
+    private
+
+    def log_activity(analytical_report)
+      my_module = analytical_report.reference
+      Activities::CreateActivityService
+        .call(activity_type: :task_analytical_report_generated,
+              owner: analytical_report.created_by,
+              team: my_module.team,
+              project: my_module.project,
+              subject: my_module,
+              message_items: { analytical_report: analytical_report.id,
+                               report_template: analytical_report.report_template.id,
+                               my_module: my_module.id })
     end
   end
 end
